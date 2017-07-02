83de1d02e44312c4e3a6cce683410d3d6490685e
LeandroTorresSicilia <jltorressicilia@gmail.com>
2017-07-02 13:48:03 -0400

    adding ButtonGroup (#25)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ export Badge from './src/components/Badge';
 export Breadcrumbs from './src/components/Breadcrumbs';
 export Breadcrumb from './src/components/Breadcrumb';
 export Button from './src/components/Button';
+export ButtonGroup from './src/components/ButtonGroup';
 export ButtonIcon from './src/components/ButtonIcon';
 export Icon from './src/components/Icon';
 export IconSvg from './src/components/IconSvg';
+export ProgressBar from './src/components/ProgressBar';
