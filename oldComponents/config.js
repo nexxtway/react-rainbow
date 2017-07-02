dbe6f582b737164cbf0b7ba3ca7c555b294b5271
Reinier Guerra <reiniergs@gmail.com>
2017-07-01 17:22:50 -0700

    make components consumables and more clean up (#21)

--- oldComponents/config.js
+++
@@ -1,9 +0,0 @@
-const defaultPathToAssets = '/assets';
-const configMap = {
-    pathToAssets: defaultPathToAssets
-};
-
-export default {
-    get:  key => configMap[key],
-    set: (key, value) => configMap[key] = value
-}
--- oldComponents/libs/uniqueId.js
+++
@@ -1,6 +0,0 @@
-let lastId = 0;
-
-export default function uniqueId (prefix = 'id') {
-    lastId += 1;
-    return `${prefix}${lastId}`;
-};
