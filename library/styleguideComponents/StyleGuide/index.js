b84fc489b1175b5d7798c784de0e158452673caf
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2018-08-20 14:14:20 -0400

    fix styles in layout and left menu (#98)

--- library/styleguideComponents/StyleGuide/index.js
+++ library/styleguideComponents/StyleGuide/index.js
@@ -13,11 +13,11 @@ export default function StyleGuide(props) {
 
     return (
         <div className="slds-grid slds-wrap slds-react-styleguide-container">
-            <aside className="slds-size_1-of-1 slds-medium-size_1-of-4 slds-large-size_1-of-6 slds-order--2 slds-medium-order--1 react-slds-sidebar">
+            <aside className="react-slds-sidebar slds-is-fixed">
                 <Logo title={title} version={version} />
                 {toc}
             </aside>
-            <main className="slds-size_1-of-1 slds-medium-size_3-of-4 slds-large-size_5-of-6 slds-order--1 slds-medium-order--2 slds-p-top_large slds-p-horizontal_xx-large slds-border_left react-slds-main-content">
+            <main className="react-slds-main-content">
                 {children}
             </main>
         </div>
