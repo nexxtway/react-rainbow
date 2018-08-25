48ff15467438cb2168b4a88aa8b55de07f1aa0bb
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2018-08-24 23:49:51 -0400

    feat: added index export file (#131)

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import Avatar from './components/Avatar/';
+import Badge from './components/Badge/';
+import Breadcrumb from './components/Breadcrumb/';
+import Breadcrumbs from './components/Breadcrumbs/';
+import Button from './components/Button/';
+import ButtonGroup from './components/ButtonGroup/';
+import ButtonIcon from './components/ButtonIcon/';
+import ButtonMenu from './components/ButtonMenu/';
+import Card from './components/Card/';
+import Icon from './components/Icon/';
+import Input from './components/Input/';
+import MenuItem from './components/MenuItem/';
+import ProgressBar from './components/ProgressBar/';
+import Spinner from './components/Spinner/';
+import VerticalItem from './components/VerticalItem/';
+import VerticalNavigation from './components/VerticalNavigation/';
+import VerticalSection from './components/VerticalSection/';
+import VerticalSectionOverflow from './components/VerticalSectionOverflow/';
+
+export {
+    Avatar,
+    Badge,
+    Breadcrumb,
+    Breadcrumbs,
+    Button,
+    ButtonGroup,
+    ButtonIcon,
+    ButtonMenu,
+    Card,
+    Icon,
+    Input,
+    MenuItem,
+    ProgressBar,
+    Spinner,
+    VerticalItem,
+    VerticalNavigation,
+    VerticalSection,
+    VerticalSectionOverflow,
+};
