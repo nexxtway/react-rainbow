9b404192e0ad7d2a5c4840963975a80d8ebe44e7
AlejandroYanes <alejandro.yanes94@gmail.com>
2018-09-01 17:44:22 -0400

    feat: implement rainbow spinner (#31)

--- src/components/Spinner/__test__/spinner.spec.js
+++ src/components/Spinner/__test__/spinner.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import renderer from 'react-test-renderer';
 import Spinner from './../index';
 
 describe('<Spinner/>', () => {
@@ -17,40 +16,40 @@ describe('<Spinner/>', () => {
         expect(component.children().length).toBe(1);
     });
     it('should have the right class names when ehiter the variant and size are not passed', () => {
-        const component = renderer.create(
+        const component = mount(
             <Spinner />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_medium').exists()).toBe(true);
     });
-    it('should have the right class names when a custom container class name is passed, variant is brand and size is large', () => {
-        const component = renderer.create(
-            <Spinner variant="brand" size="large" containerClassName="custom-container-classname" />,
+    it('should have the right class names when a custom class name is passed, variant is brand and size is large', () => {
+        const component = mount(
+            <Spinner variant="brand" size="large" className="custom-container-classname" />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_brand.rainbow-spinner_large.custom-container-classname').exists()).toBe(true);
     });
     it('should have the right class names when variant is inverse and size is medium', () => {
-        const component = renderer.create(
+        const component = mount(
             <Spinner variant="inverse" size="medium" />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_inverse.rainbow-spinner_medium').exists()).toBe(true);
     });
     it('should have the right class names when size is small', () => {
-        const component = renderer.create(
+        const component = mount(
             <Spinner size="small" />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_small').exists()).toBe(true);
     });
     it('should have the right class names when size is x-small', () => {
-        const component = renderer.create(
+        const component = mount(
             <Spinner size="x-small" />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_x-small').exists()).toBe(true);
     });
     it('should have the right class names when size is xx-small', () => {
-        const component = renderer.create(
+        const component = mount(
             <Spinner size="xx-small" />,
         );
-        expect(component).toMatchSnapshot();
+        expect(component.find('div.rainbow-spinner.rainbow-spinner_xx-small').exists()).toBe(true);
     });
     it('should pass assistiveText to the prop text of AssistiveText component', () => {
         const component = mount(
