4670dd958764cbed806ca823fe3050fc87318834
leyanessantiago <le.yanessantiago@gmail.com>
2018-09-02 01:51:56 -0400

    style: delete spinner class.

--- src/components/Card/cardBody.js
+++ src/components/Card/cardBody.js
@@ -6,7 +6,7 @@ import './styles.css';
 export default function CardBoddy({ isLoading, children }) {
     if (isLoading) {
         return (
-            <Spinner className="rainbow-card__spinner" isVisible={isLoading} size="small" />
+            <Spinner isVisible={isLoading} size="small" />
         );
     }
     return children;
