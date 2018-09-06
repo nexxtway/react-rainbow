3c84ca6b911a034ebc6e491e17534db108836238
TahimiLeonBravo <tahimileon@gmail.com>
2018-09-06 19:33:03 -0400

    remove: proptype folder

--- src/propTypes/iconNamePropType.js
+++
@@ -1,7 +0,0 @@
-import createChainableTypeChecker from './createChainableTypeChecker';
-import iconNamePropTypeValidator from './iconNamePropTypeValidator';
-
-export default {
-    simple: createChainableTypeChecker(iconNamePropTypeValidator),
-    oneOf: iconPrefixes => createChainableTypeChecker(iconNamePropTypeValidator, iconPrefixes),
-};
