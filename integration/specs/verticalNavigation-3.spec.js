e1bc19396ebcbb89c0634d5e9ee1c8b631fa4539
cmcartaya <cmcartaya1sm@gmail.com>
2018-09-08 18:23:13 -0400

    test: fix integration test

--- integration/specs/verticalNavigation-3.spec.js
+++ integration/specs/verticalNavigation-3.spec.js
@@ -1,5 +1,5 @@
 describe('VerticalNavigation with href example', () => {
-    const AVATAR_ITEM = '#rsg-root > div > main > section > section > div > div > article > div > article > div > div > div > div.slds-large-size_1-of-3.slds-medium-size_1-of-2.slds-small-size_1-of-1.slds-color__background_gray-1.slds-p-vertical_medium.slds-border_right > nav > div > ul > li:nth-child(1) > a';
+    const AVATAR_ITEM = 'div.rainbow-background-color_white.rainbow-p-top_small.rainbow-p-vertical_medium > nav > div > ul > li.rainbow-nav-vertical__item > a';
 
     it('should go to "avatar page" when click on the "Avatar" item', () => {
         browser.url('/#!/VerticalNavigation/3');
