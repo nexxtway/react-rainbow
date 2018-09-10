933a8c2cec9be3e26ca9e4b67b1e9f9d9b47d9f6
cmcartaya <cmcartaya1sm@gmail.com>
2018-09-10 15:28:51 -0400

    refactor: fix class, test and delete icon component in input

--- src/components/Input/__test__/requiredAsterisk.spec.js
+++ src/components/Input/__test__/requiredAsterisk.spec.js
@@ -3,17 +3,11 @@ import { mount } from 'enzyme';
 import RequiredAsterisk from './../requiredAsterisk';
 
 describe('<InputRequiredAsterisk/>', () => {
-    it('should not render the abbr element when is not required', () => {
-        const component = mount(
-            <RequiredAsterisk required={false} />,
-        );
-        expect(component.find('abbr[className="rainbow-required"]').exists()).toBe(false);
-    });
     it('should render the abbr element when is required', () => {
         const component = mount(
             <RequiredAsterisk required />,
         );
-        const asterisk = component.find('abbr[className="rainbow-required"]');
+        const asterisk = component.find('abbr[className="rainbow-input-required"]');
         expect(asterisk.exists()).toBe(true);
         expect(asterisk.text()).toBe('* ');
     });
@@ -21,7 +15,7 @@ describe('<InputRequiredAsterisk/>', () => {
         const component = mount(
             <RequiredAsterisk required />,
         );
-        const asterisk = component.find('abbr[className="rainbow-required"]');
+        const asterisk = component.find('abbr[className="rainbow-input-required"]');
         expect(asterisk.prop('title')).toBe('required');
     });
 });
