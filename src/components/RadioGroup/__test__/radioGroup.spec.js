45b9df940fca02e70aa3add797736427d3124a5c
cmcartaya <cmcartaya1sm@gmail.com>
2018-09-10 22:34:24 -0400

    refactor: refactoring class in components 2

--- src/components/RadioGroup/__test__/radioGroup.spec.js
+++ src/components/RadioGroup/__test__/radioGroup.spec.js
@@ -7,12 +7,12 @@ describe('<RadioGroup />', () => {
         const component = mount(
             <RadioGroup error="input error" />,
         );
-        expect(component.find('fieldset').prop('className')).toBe('rainbow-radio-group-form-element rainbow-radio-group-has-error');
+        expect(component.find('fieldset').prop('className')).toBe('rainbow-radio-group-container rainbow-radio-group-has-error');
     });
     it('should have the right class names when a custom class is passed', () => {
         const component = mount(
             <RadioGroup className="my-custom-class" />,
         );
-        expect(component.find('fieldset').prop('className')).toBe('rainbow-radio-group-form-element my-custom-class');
+        expect(component.find('fieldset').prop('className')).toBe('rainbow-radio-group-container my-custom-class');
     });
 });
