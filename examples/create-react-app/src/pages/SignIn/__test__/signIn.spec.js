125c834c1fb224ad85ffb638b4f8fbf0df17ea47
cmcartaya <cmcartaya1sm@gmail.com>
2018-09-17 02:11:45 -0400

    feat: add page example Sign In

---
+++ examples/create-react-app/src/pages/SignIn/__test__/signIn.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from 'react-rainbow-components/components/Card';
+import Button from 'react-rainbow-components/components/Button';
+import Input from 'react-rainbow-components/components/Input';
+import SignInExample from './../';
+
+
+describe('<SignInExample/>', () => {
+    it('should render the SignIn', () => {
+        const component = shallow(
+            <SignInExample />,
+        );
+        expect(component.find(Card).exists()).toBe(true);
+        expect(component.find(Button).exists()).toBe(true);
+        expect(component.find(Input).exists()).toBe(true);
+    });
+});
---
+++ examples/create-react-app/src/pages/SignIn/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Card from 'react-rainbow-components/components/Card';
+import Button from 'react-rainbow-components/components/Button';
+import Input from 'react-rainbow-components/components/Input';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
+
+export default function SignInExample() {
+    return (
+        <div className="rainbow-align-content_center rainbow-flex_column">
+            <img
+                className="rainbow-p-vertical_x-large"
+                src="assets/images/rainbow-logo.svg"
+                alt="rainbow-logo" />
+            <Card className="rainbow-p-around_x-large rainbow-sign-in_card-container">
+                <h1 className="rainbow-font-size-heading_medium rainbow-color_brand rainbow-sign-in_title">
+                    Sign in
+                </h1>
+                <Input
+                    className="rainbow-m-bottom_large"
+                    label="Email Address"
+                    required
+                    placeholder="Enter your email address"
+                    icon={
+                        <FontAwesomeIcon icon={faEnvelope} size="lg" className="rainbow-color_gray-3" />
+                    } />
+                <Input
+                    className="rainbow-m-bottom_large"
+                    label="Password"
+                    placeholder="Enter your password"
+                    required
+                    icon={
+                        <FontAwesomeIcon icon={faLock} size="lg" className="rainbow-color_gray-3" />
+                    } />
+
+                <Button
+                    className="rainbow-m-bottom_medium rainbow-sign-in_button-sign-in"
+                    label="Sign in"
+                    variant="brand" />
+                <a className="rainbow-font-size-heading_small rainbow-color_brand rainbow-align-content_center">
+                    forgot your password?
+                </a>
+            </Card>
+        </div>
+    );
+}
