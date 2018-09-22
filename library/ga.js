2d14de72cd5abc4bd0f96be543ab792b53498084
LeandroTorresSicilia <jltorressicilia@gmail.com>
2018-09-22 12:53:35 -0400

    fix: fixed pathname (#213)

--- library/ga.js
+++ library/ga.js
@@ -3,4 +3,4 @@ import ReactGA from 'react-ga';
 const trackId = 'UA-126214096-1';
 ReactGA.initialize(trackId);
 
-ReactGA.pageview(window.location.pathname);
+ReactGA.pageview(window.location.hash);
