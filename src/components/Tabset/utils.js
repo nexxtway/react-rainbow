895658430eea7f0ad65703b7bc6fccea50590c5c
ayanes <alejandro.yanes94@gmail.com>
2018-09-22 15:46:40 -0400

    feat: implement tabset full-width behavior and ability to add children dynamically

---
+++ src/components/Tabset/utils.js
@@ -0,0 +1,21 @@
+export function getChildTabNodes(ref) {
+    if (ref) {
+        return ref.querySelectorAll('a[role="tab"]');
+    }
+    return [];
+}
+
+function getRefIndex(nodes, ref) {
+    return nodes.indexOf(ref);
+}
+
+function sortChildren(childrenRefs, nodes) {
+    const [...newChildrenRefs] = childrenRefs;
+    newChildrenRefs.sort((refA, refB) => getRefIndex(nodes, refA) - getRefIndex(nodes, refB));
+    return newChildrenRefs;
+}
+
+export function insertChildOrderly(childrenRefs, childRef, nodes) {
+    const newChildrenRefs = childrenRefs.concat([childRef]);
+    return sortChildren(newChildrenRefs, nodes);
+}
