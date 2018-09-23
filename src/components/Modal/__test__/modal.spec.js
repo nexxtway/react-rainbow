35bd27e0accdae1d0ee82a0bd211f62b46e5bf85
leyanessantiago <le.yanessantiago@gmail.com>
2018-09-23 13:10:12 -0400

    test: Add test to modal.

---
+++ src/components/Modal/__test__/modal.spec.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Modal from './../';
+
+describe('<Modal/>', () => {
+    it('should render the children passed', () => {
+        const component = mount(
+            <Modal>
+                <p data-id="modal-children">modal content</p>
+            </Modal>,
+        );
+        expect(component.find('p[data-id="modal-children"]').text()).toBe('modal content');
+    });
+    it('should have the right class names in the backdrop element', () => {
+        const component = mount(
+            <Modal>
+                <p />
+            </Modal>,
+        );
+        expect(component.find('div[className="rainbow-modal_backdrop"]').exists()).toBe(true);
+    });
+    it('should have the right class names in the backdrop element when the modal is opened', () => {
+        const component = mount(
+            <Modal isOpen>
+                <p />
+            </Modal>,
+        );
+        expect(component.find('div[className="rainbow-modal_backdrop rainbow-modal_backdrop--open"]').exists()).toBe(true);
+    });
+    it('should have the right class names in the section element', () => {
+        const component = mount(
+            <Modal>
+                <p />
+            </Modal>,
+        );
+        expect(component.find('section[role="dialog"]').prop('className')).toBe('rainbow-modal rainbow-modal--slide-down');
+    });
+    it('should have the right class names in the section element when the modal is opened', () => {
+        const component = mount(
+            <Modal isOpen>
+                <p />
+            </Modal>,
+        );
+        expect(component.find('section[role="dialog"]').prop('className')).toBe('rainbow-modal rainbow-modal--fade-in');
+    });
+    it('should have the right class names in the section element when the modal is opened and the size is medium', () => {
+        const component = mount(
+            <Modal isOpen size="medium">
+                <p />
+            </Modal>,
+        );
+        expect(component.find('section[role="dialog"]').prop('className')).toBe('rainbow-modal rainbow-modal--fade-in rainbow-modal--medium');
+    });
+    it('should have the right class names in the section element when the modal is opened and the size is large', () => {
+        const component = mount(
+            <Modal isOpen size="large">
+                <p />
+            </Modal>,
+        );
+        expect(component.find('section[role="dialog"]').prop('className')).toBe('rainbow-modal rainbow-modal--fade-in rainbow-modal--large');
+    });
+});
---
+++ src/components/Modal/pageObject/index.js
@@ -0,0 +1,59 @@
+/**
+ * Modal page object class.
+ * @class
+ * @tutorial buttonMenu
+ */
+class PageModal {
+    /**
+     * Create a new Modal page object.
+     * @constructor
+     * @param {string} rootElement - The selector of the Modal root element.
+     */
+    constructor(rootElement) {
+        this.rootElement = rootElement;
+    }
+
+    /**
+     * Open the modal element.
+     * @method
+     */
+    openModal() {
+        $('#modal-container button[data-id="button-icon-element"]').click();
+    }
+
+    /**
+     * Clicks the closeButton element.
+     * @method
+     */
+    clickCloseButton() {
+        $(this.rootElement).$('.rainbow-modal_close').click();
+    }
+
+    /**
+     * Clicks the backdrop element.
+     * @method
+     */
+    clickOutside() {
+        $(this.rootElement).leftClick(1, 1);
+    }
+
+    /**
+     * Returns true when the modal is open, false otherwise.
+     * @method
+     * @returns {bool}
+     */
+    isOpen() {
+        return $(this.rootElement).$('[role="dialog"]').isVisible();
+    }
+
+    /**
+     * Returns true when the closeButton has focus.
+     * @method
+     * @returns {bool}
+     */
+    hasFocusCloseButton() {
+        return $(this.rootElement).$('.rainbow-modal_close').hasFocus();
+    }
+}
+
+module.exports = PageModal;
