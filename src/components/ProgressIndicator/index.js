d07cfefc2e2c7bf050bd2da160eb3b1c057270ff
leyanessantiago <37941027+leyanessantiago@users.noreply.github.com>
2018-10-13 18:59:19 -0400

    feat: implementing progressIndicator component. (#294)

---
+++ src/components/ProgressIndicator/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { Provider } from './context';
+import './styles.css';
+
+export default class ProgressIndicator extends Component {
+    constructor(props) {
+        super(props);
+        this.stepChildren = [];
+        this.registerStep = this.registerStep.bind(this);
+        this.setChildrenState = this.setChildrenState.bind(this);
+    }
+
+    getContainerClassNames() {
+        const { className } = this.props;
+        return classnames('rainbow-progress-indicator', className);
+    }
+
+    setChildrenState(step) {
+        const { currentStepName } = this.props;
+        const activeStepIndex = this.stepChildren.findIndex(item => item.name === currentStepName);
+        const currentChildIndex = this.stepChildren.findIndex(item => item.name === step.name);
+
+        if (currentChildIndex === activeStepIndex) {
+            step.onSetStepState('Active');
+        } else if (activeStepIndex === -1 || currentChildIndex < activeStepIndex) {
+            step.onSetStepState('Completed');
+        } else if (currentChildIndex > activeStepIndex) {
+            step.onSetStepState('Inactive');
+        }
+    }
+
+    registerStep(step) {
+        const newChildrenRefs = this.stepChildren.concat([step]);
+        this.stepChildren = newChildrenRefs;
+        this.setChildrenState(step);
+    }
+
+    render() {
+        const {
+            style,
+            children,
+            currentStepName,
+            onClick,
+        } = this.props;
+        const context = {
+            currentStepName,
+            privateRegisterStep: this.registerStep,
+            privateOnClick: onClick,
+            setChildrenState: this.setChildrenState,
+        };
+
+        return (
+            <div className={this.getContainerClassNames()} style={style}>
+                <ol className="rainbow-progress-indicator_list">
+                    <Provider value={context}>
+                        { children }
+                    </Provider>
+                </ol>
+                <div className="rainbow-progress-indicator_bar" />
+            </div>
+        );
+    }
+}
+
+ProgressIndicator.propTypes = {
+    /** Specifies which step is active */
+    currentStepName: PropTypes.string,
+    /** A CSS class for the outer element, in addition to the component's base classes. */
+    className: PropTypes.string,
+    /** An object with custom style applied to the outer element. */
+    style: PropTypes.object,
+    /** The action triggered when the element is clicked. */
+    onClick: PropTypes.func,
+    /**
+     * This prop that should not be visible in the documentation.
+     * @ignore
+     */
+    children: PropTypes.node,
+};
+
+ProgressIndicator.defaultProps = {
+    currentStepName: '',
+    className: undefined,
+    style: undefined,
+    onClick: () => {},
+    children: null,
+};
---
+++ src/components/ProgressStep/index.js
@@ -0,0 +1,131 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import AssistiveText from '../AssistiveText';
+import ButtonIcon from '../ButtonIcon';
+import { Consumer } from '../ProgressIndicator/context';
+import DoneIcon from './doneIcon';
+import ErrorIcon from './errorIcon';
+import './styles.css';
+
+class StepItem extends Component {
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.setStepState = this.setStepState.bind(this);
+        this.state = {
+            stepState: 'Inactive',
+        };
+    }
+
+    componentDidMount() {
+        const { privateRegisterStep, name } = this.props;
+        return setTimeout(() => privateRegisterStep({
+            name,
+            onSetStepState: this.setStepState,
+        }), 0);
+    }
+
+    componentDidUpdate({ currentStepName: prevCurrentStepName }) {
+        const { currentStepName, name } = this.props;
+        if (prevCurrentStepName !== currentStepName) {
+            this.props.setChildrenState({
+                name,
+                onSetStepState: this.setStepState,
+            });
+        }
+    }
+
+    setStepState(stepState) {
+        const { hasError } = this.props;
+        if (hasError) {
+            return this.setState({ stepState: 'Error' });
+        }
+        return this.setState({ stepState });
+    }
+
+    getContainerClassNames() {
+        const { className } = this.props;
+        return classnames('rainbow-progress-step', className);
+    }
+
+    getButtonClassNames() {
+        const { stepState } = this.state;
+        return classnames('rainbow-progress-step_marker', {
+            'rainbow-progress-step--is-completed': stepState === 'Completed',
+            'rainbow-progress-step--is-active': stepState === 'Active',
+            'rainbow-progress-step--error': stepState === 'Error',
+        });
+    }
+
+    getAssistiveText() {
+        const { label } = this.props;
+        const { stepState } = this.state;
+
+        if (stepState !== 'Incative') {
+            return `${label} - ${stepState}`;
+        }
+        return label;
+    }
+
+    getIcon() {
+        const { stepState } = this.state;
+        if (stepState === 'Error') {
+            return <ErrorIcon />;
+        } else if (stepState === 'Completed') {
+            return <DoneIcon />;
+        }
+        return null;
+    }
+
+    handleOnClick(event) {
+        const { privateOnClick, name } = this.props;
+        return privateOnClick(event, name);
+    }
+
+    render() {
+        const { label } = this.props;
+
+        return (
+            <li className={this.getContainerClassNames()}>
+                <ButtonIcon
+                    icon={this.getIcon()}
+                    className={this.getButtonClassNames()}
+                    onClick={this.handleOnClick}>
+                    <AssistiveText text={this.getAssistiveText()} />
+                </ButtonIcon>
+                <span className="rainbow-progress-step_label">{label}</span>
+            </li>
+        );
+    }
+}
+
+export default function ProgressStep(props) {
+    return (
+        <Consumer>
+            {context => <StepItem {...props} {...context} />}
+        </Consumer>
+    );
+}
+
+ProgressStep.propTypes = {
+    /** The name is used to determine which ProgressStep is active. */
+    name: PropTypes.string,
+    /** Text label for the ProgressStep. */
+    label: PropTypes.node,
+    /** Specifies if the ProgressStep has an error */
+    hasError: PropTypes.bool,
+    /** A CSS class for the outer element, in addition to the component's base classes. */
+    className: PropTypes.string,
+    /** An object with custom style applied to the outer element. */
+    style: PropTypes.object,
+};
+
+ProgressStep.defaultProps = {
+    name: '',
+    label: '',
+    hasError: false,
+    className: undefined,
+    style: undefined,
+};
