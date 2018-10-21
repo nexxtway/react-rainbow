1ec0168331985707ac2eea4cf853885509b8e148
AlejandroYanes <alejandro.yanes94@gmail.com>
2018-10-21 02:42:38 -0400

    test: added recaptcha tests (#318)

---
+++ src/components/ReCaptcha/__tests__/component.spec.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ReCaptchaComponent from '../component';
+
+window.grecaptcha = {
+    ready: jest.fn(),
+};
+
+describe('<ReCaptchaComponent />', () => {
+    it('it should call the ready function', () => {
+        mount(
+            <ReCaptchaComponent
+                value="site-Key"
+                theme="light"
+                size="normal"
+                tabIndex={0}
+                onChange={() => {}} />,
+        );
+        expect(window.grecaptcha.ready).toHaveBeenCalledTimes(1);
+    });
+    it('it should call the render function with the right params', () => {
+        window.grecaptcha = {
+            ready: callback => callback(),
+            render: jest.fn(),
+        };
+
+        const component = mount(
+            <ReCaptchaComponent
+                value="site-key"
+                theme="light"
+                size="normal"
+                tabIndex={0}
+                onChange={() => {}} />,
+        );
+        expect(component.find('div#recaptcha-container').exists()).toBe(true);
+        expect(window.grecaptcha.render).toHaveBeenCalledWith('recaptcha-container', {
+            sitekey: 'site-key',
+            theme: 'light',
+            size: 'normal',
+            tabIndex: 0,
+            callback: expect.any(Function),
+            'expired-callback': expect.any(Function),
+            'error-callback': expect.any(Function),
+        });
+    });
+});
---
+++ src/components/ReCaptcha/__tests__/get-url.spec.js
@@ -0,0 +1,12 @@
+import getUrl from '../get-url';
+
+describe('getUrl function', () => {
+    it('should return the recaptcha url without the lang param', () => {
+        const url = getUrl();
+        expect(url).toBe('https://www.google.com/recaptcha/api.js?render=explicit');
+    });
+    it('should return the recaptcha url with the lang param', () => {
+        const url = getUrl('es');
+        expect(url).toBe('https://www.google.com/recaptcha/api.js?render=explicit&hl=es');
+    });
+});
