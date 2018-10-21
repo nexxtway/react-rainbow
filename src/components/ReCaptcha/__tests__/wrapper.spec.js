d1e9416bd80ac3656cfe61348c081351a07120c8
ayanes <alejandro.yanes94@gmail.com>
2018-10-21 01:52:02 -0400

    test: added recaptcha tests

---
+++ src/components/ReCaptcha/__tests__/wrapper.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ReCaptchaWrapper from '../wrapper';
+
+window.grecaptcha = {
+    ready: jest.fn(),
+};
+
+describe('<ReCaptchaWrapper />', () => {
+    it('should not render when isScriptLoaded and isScriptLoadSucceed are false', () => {
+        const component = mount(<ReCaptchaWrapper />);
+        component.setProps({
+            isScriptLoaded: false,
+            isScriptLoadSucceed: false,
+        });
+        expect(component.children()).toEqual({});
+    });
+    it('should not render when isScriptLoaded is true and isScriptLoadSucceed is false', () => {
+        const component = mount(<ReCaptchaWrapper />);
+        component.setProps({
+            isScriptLoaded: true,
+            isScriptLoadSucceed: false,
+        });
+        expect(component.children()).toEqual({});
+    });
+    it('should render the ReCaptcha loader with the right props', () => {
+        const component = mount(
+            <ReCaptchaWrapper
+                onChange={() => {}}
+                value="site-key"
+                theme="dark"
+                size="normal"
+                error="error"
+                tabIndex={0}
+                isScriptLoaded
+                isScriptLoadSucceed />,
+        );
+        const loader = component.find('ReCaptchaComponent');
+
+        expect(loader.exists()).toBe(true);
+        expect(loader.props()).toEqual(
+            {
+                onChange: expect.any(Function),
+                value: 'site-key',
+                theme: 'dark',
+                size: 'normal',
+                error: 'error',
+                tabIndex: 0,
+                className: undefined,
+                style: undefined,
+            },
+        );
+    });
+});
