4f77fc212ce6538a09ceaba8a53d6bde3a65ad43
ayanes <alejandro.yanes94@gmail.com>
2018-10-26 17:36:09 -0400

    fix: change components markup to follow accessibility rules

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import ButtonIcon from '../ButtonIcon';
 import LeftArrow from './leftArrow';
 import RightArrow from './rightArrow';
+import NavigationButton from './navigationButton';
 import PageButtons from './pageButtons';
 import './styles.css';
 
 export default function Pagination(props) {
     const { pages, activePage, onChange, className, style } = props;
 
-    const getContainerClassNames = () => classnames('rainbow-pagination_container', className);
+    const getContainerClassNames = () => classnames('rainbow-pagination', className);
 
     const isFirstItemSelected = activePage === 1;
     const isLastItemSelected = activePage === pages;
 
     return (
-        <div className={getContainerClassNames()} style={style}>
-            <ButtonIcon
-                variant="border-filled"
-                className="rainbow-pagination_button rainbow-pagination_button-icon"
-                icon={<LeftArrow />}
-                onClick={event => onChange(event, activePage - 1)}
-                disabled={isFirstItemSelected} />
-            <PageButtons onChange={onChange} pages={pages} activePage={activePage} />
-            <ButtonIcon
-                variant="border-filled"
-                className="rainbow-pagination_button rainbow-pagination_button-icon"
-                icon={<RightArrow />}
-                onClick={event => onChange(event, activePage + 1)}
-                disabled={isLastItemSelected} />
-        </div>
+        <nav className={getContainerClassNames()} style={style} aria-label="pagination">
+            <ul className="rainbow-pagination_container">
+                <NavigationButton
+                    icon={<LeftArrow />}
+                    onClick={event => onChange(event, activePage - 1)}
+                    disabled={isFirstItemSelected} />
+                <PageButtons onChange={onChange} pages={pages} activePage={activePage} />
+                <NavigationButton
+                    icon={<RightArrow />}
+                    onClick={event => onChange(event, activePage + 1)}
+                    disabled={isLastItemSelected} />
+            </ul>
+        </nav>
     );
 }
 
--- src/components/Pagination/pageButtons.js
+++ src/components/Pagination/pageButtons.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import ButtonGroup from '../ButtonGroup';
-import Button from '../Button';
 import getFirstItem from './get-first-item';
 
 export default function PageButtons(props) {
     const { pages, activePage, onChange } = props;
 
-    const getButtonClassName = page => classnames('rainbow-pagination_button', {
-        'rainbow-pagination_active-button': activePage === page,
-    });
+    const getButtonClassName = (firstItem, index, buttonsToRender) => (
+        classnames(
+            'rainbow-pagination_button',
+            {
+                'rainbow-pagination_button--first': index === 0,
+                'rainbow-pagination_button--last': index === buttonsToRender - 1,
+                'rainbow-pagination_button--active': activePage === firstItem + index,
+            },
+        )
+    );
+
+    const getAriaCurrent = (page) => {
+        if (page === activePage) {
+            return 'page';
+        }
+        return undefined;
+    };
 
     const renderButtons = () => {
         const firstItem = pages > 4 ? getFirstItem(pages, activePage) : 1;
         const buttonsToRender = pages > 4 ? 5 : pages;
-        return Array(buttonsToRender).fill(0).map((item, index) => (
-            <Button
-                key={`page-button-${firstItem + index}`}
-                label={firstItem + index}
-                className={getButtonClassName(firstItem + index)}
-                onClick={event => onChange(event, firstItem + index)} />
-        ));
+        return Array(buttonsToRender).fill(0).map((item, index) => {
+            const page = firstItem + index;
+            return (
+                <li key={`page-button-${page}`} className={getButtonClassName(firstItem, index, buttonsToRender)}>
+                    <a
+                        onClick={event => onChange(event, page)}
+                        aria-current={getAriaCurrent(page)}
+                        aria-label={`Goto Page ${page}`}
+                        role="presentation">
+                    <span>
+                        {page}
+                    </span>
+                    </a>
+                </li>
+            );
+        });
     };
 
-    return (
-        <ButtonGroup className="rainbow-pagination_pages">
-            {renderButtons()}
-        </ButtonGroup>
-    );
+    return renderButtons();
 }
 
 PageButtons.propTypes = {
