7be3b2d19a17802509ec5c415c3a2ce27feefd2f
AlejandroYanes <alejandro.yanes94@gmail.com>
2018-10-27 15:53:36 -0400

    test: add pagination tests (#354)

---
+++ src/components/Pagination/__test__/pageButtons.spec.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import PageButtons from '../pageButtons';
+
+describe('<PageButtons />', () => {
+    it('should render 5 children if pages is grater than 4', () => {
+        const component = mount(<PageButtons pages={7} onChange={() => {}} activePage={3} />);
+        const buttons = component.find('li');
+        expect(buttons.length).toBe(5);
+    });
+    it('should render as many children as pages if pages is less than 4', () => {
+        const component = mount(<PageButtons pages={4} onChange={() => {}} activePage={3} />);
+        const buttons = component.find('li');
+        expect(buttons.length).toBe(4);
+    });
+    it('should set the right className to the active page button', () => {
+        const component = mount(<PageButtons pages={7} onChange={() => {}} activePage={3} />);
+        const pageButtons = component.find('li.rainbow-pagination_button');
+        expect(pageButtons.get(2).props.className).toBe('rainbow-pagination_button rainbow-pagination_button--active');
+    });
+    it('should set aria-current to "page" to the active anchor element', () => {
+        const component = mount(<PageButtons pages={7} onChange={() => {}} activePage={3} />);
+        const pageButtons = component.find('a');
+        expect(pageButtons.get(2).props['aria-current']).toBe('page');
+    });
+    it('should set aria-current to undefined to the anchor element if is not active', () => {
+        const component = mount(<PageButtons pages={7} onChange={() => {}} activePage={3} />);
+        const pageButtons = component.find('a');
+        expect(pageButtons.get(1).props['aria-current']).toBe(undefined);
+    });
+    it('should set aria-label to the anchor elements', () => {
+        const component = mount(<PageButtons pages={7} onChange={() => {}} activePage={3} />);
+        const pageButtons = component.find('a');
+        expect(pageButtons.get(1).props['aria-label']).toBe('Goto Page 2');
+    });
+    it('should call onChange if a page button is clicked', () => {
+        const onChangeMockFn = jest.fn();
+        const component = mount(<PageButtons pages={1} onChange={onChangeMockFn} activePage={1} />);
+        const anchor = component.find('a');
+        anchor.simulate('click');
+        expect(onChangeMockFn).toHaveBeenCalledWith(expect.any(Object), 1);
+    });
+});
