48ad595293ebd63e9cbcae31eb85ed29e4a7c16b
ayanes <alejandro.yanes94@gmail.com>
2018-10-28 15:54:40 -0400

    fix: added test to check nav-buttons when disabled

--- src/components/Pagination/__test__/navigationButton.spec.js
+++ src/components/Pagination/__test__/navigationButton.spec.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { mount } from 'enzyme';
 import NavigationButton from '../navigationButton';
 
-const onCkickMockFn = jest.fn();
 
 describe('<NavigationButton />', () => {
     it('should call the onClick function when clicked', () => {
+        const onCkickMockFn = jest.fn();
         const component = mount(<NavigationButton onClick={onCkickMockFn} icon={<span />} />);
         const anchor = component.find('a');
         anchor.simulate('click');
         expect(onCkickMockFn).toHaveBeenCalledTimes(1);
     });
+    it('should not call the onClick function when clicked if disabled', () => {
+        const onCkickMockFn = jest.fn();
+        const component = mount(
+            <NavigationButton onClick={onCkickMockFn} icon={<span />} disabled />,
+        );
+        const anchor = component.find('a');
+        anchor.simulate('click');
+        expect(onCkickMockFn).toHaveBeenCalledTimes(0);
+    });
     it('should set the aria-disabled to the anchor element if disabled', () => {
+        const onCkickMockFn = jest.fn();
         const component = mount(
             <NavigationButton onClick={onCkickMockFn} icon={<span />} disabled />,
         );
