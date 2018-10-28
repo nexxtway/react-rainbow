e20f026c0e74e52f79785575b7a89fd7db52c4d6
TahimiLeonBravo <tahimileon@gmail.com>
2018-10-27 21:43:54 -0500

    docs: add description to the components

--- src/components/ReCaptcha/index.js
+++ src/components/ReCaptcha/index.js
@@ -5,6 +5,9 @@ import withReduxForm from '../../libs/hocs/withReduxForm';
 import getUrl from './get-url';
 import ReCaptchaWrapper from './wrapper';
 
+/**
+* The reCAPTCHA component is used to protects your website from spam and abuse.
+*/
 function ReCaptcha(props) {
     const { lang, ...rest } = props;
 
