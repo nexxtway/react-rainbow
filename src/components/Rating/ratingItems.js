4ef643b5d56274b3e11b15376b4e3bf5392d3572
leyanessantiago <37941027+leyanessantiago@users.noreply.github.com>
2018-10-28 18:18:09 -0400

    feat: implementing the Rating component. (#366)

---
+++ src/components/Rating/ratingItems.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Star from './star';
+
+export default function RatingItems(props) {
+    const {
+        onChange,
+        value,
+        name,
+    } = props;
+
+    return Array(5).fill(0).map((item, index) => {
+        const key = `star-${index}`;
+        const filled = index < value;
+        return (
+            <Star
+                key={key}
+                name={name}
+                onChange={onChange}
+                value={index + 1}
+                filled={filled} />
+        );
+    });
+}
+
+RatingItems.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+};
+
+RatingItems.defaultProps = {
+    value: undefined,
+    onChange: () => {},
+    name: undefined,
+};
