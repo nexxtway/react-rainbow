47b5f868db0ff1e1501c71a1aa50972ca40326a3
Reinier Guerra <reiniergs@gmail.com>
2018-10-30 00:23:44 -0700

    change repo to 90milesbridge

--- library/styleguideComponents/SectionHeading/GithubStarsWrapper/__test__/fetchStars.spec.js
+++ library/styleguideComponents/SectionHeading/GithubStarsWrapper/__test__/fetchStars.spec.js
@@ -10,7 +10,7 @@ beforeEach(() => {
 describe('fetch stars method', () => {
     it('should call fetch with the right data', () => {
         fetchGithubStars();
-        expect(fetch).toHaveBeenCalledWith('https://api.github.com/repos/reiniergs/react-rainbow-components/stargazers');
+        expect(fetch).toHaveBeenCalledWith('https://api.github.com/repos/90milesbridge/react-rainbow-components/stargazers');
     });
     it('should return the number of stars for the repository', () => {
         fetch.mockReturnValue(Promise.resolve({
