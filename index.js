bf1a1ba2e4f780c3eb5d1ba0d42ee16d9b3637e8
AlejandroYanes <alejandro.yanes94@gmail.com>
2018-11-28 15:25:17 -0500

    feat: add new components to global index export (#414)

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
+import Accordion from './Accordion';
+import AccordionSection from './AccordionSection';
 import Avatar from './Avatar';
+import AvatarMenu from './AvatarMenu';
 import Badge from './Badge';
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
+import ButtonIcon from './ButtonIcon';
 import ButtonMenu from './ButtonMenu';
 import Card from './Card';
+import CarouselCard from './CarouselCard';
+import CarouselImage from './CarouselImage';
+import Chart from './Chart';
+import CheckboxGroup from './CheckboxGroup';
+import CheckboxToggle from './CheckboxToggle';
+import Dataset from './Dataset';
+import GMap from './GMap';
 import Input from './Input';
+import MapMarker from './MapMarker';
+import MenuDivider from './MenuDivider';
 import MenuItem from './MenuItem';
+import Modal from './Modal';
+import Notification from './Notification';
+import Pagination from './Pagination';
+import PrimitiveMenu from './PrimitiveMenu';
 import ProgressBar from './ProgressBar';
+import ProgressIndicator from './ProgressIndicator';
+import ProgressStep from './ProgressStep';
+import RadioGroup from './RadioGroup';
+import Rating from './Rating';
+import ReCaptcha from './ReCaptcha';
+import Select from './Select';
+import Slider from './Slider';
 import Spinner from './Spinner';
+import Tab from './Tab';
+import Tabset from './Tabset';
+import Textarea from './Textarea';
 import VerticalItem from './VerticalItem';
 import VerticalNavigation from './VerticalNavigation';
 import VerticalSection from './VerticalSection';
 import VerticalSectionOverflow from './VerticalSectionOverflow';
 
 export {
+    Accordion,
+    AccordionSection,
     Avatar,
+    AvatarMenu,
     Badge,
     Button,
     ButtonGroup,
+    ButtonIcon,
     ButtonMenu,
     Card,
+    CarouselCard,
+    CarouselImage,
+    Chart,
+    CheckboxGroup,
+    CheckboxToggle,
+    Dataset,
+    GMap,
     Input,
+    MapMarker,
+    MenuDivider,
     MenuItem,
+    Modal,
+    Notification,
+    Pagination,
+    PrimitiveMenu,
     ProgressBar,
+    ProgressIndicator,
+    ProgressStep,
+    RadioGroup,
+    Rating,
+    ReCaptcha,
+    Select,
+    Slider,
     Spinner,
+    Tab,
+    Tabset,
+    Textarea,
     VerticalItem,
     VerticalNavigation,
     VerticalSection,
