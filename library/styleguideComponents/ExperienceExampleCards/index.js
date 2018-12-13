ef7c056d406a71b8c575d7defa918803b2919601
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2018-12-13 16:36:56 -0600

    Fix experience example card padding (#445)

--- library/styleguideComponents/ExperienceExampleCards/index.js
+++ library/styleguideComponents/ExperienceExampleCards/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 
 export default function ExperienceExampleCards({ results }) {
     return results.map(({ data, id }) => (
-        <section key={id} className="react-rainbow-experience-example-cards-item_example-container">
+        <section key={id} className="react-rainbow-experience-example-cards_example-container">
             <a
                 className="react-rainbow-experience-example-cards_example-link"
                 href={data['example-url'].url}
@@ -14,23 +14,23 @@ export default function ExperienceExampleCards({ results }) {
                 rel="noopener noreferrer">
 
                 <Card
-                    className="react-rainbow-experience-example-cards-item"
+                    className="react-rainbow-experience-example-cards"
                     footer={(
-                        <div className="react-rainbow-experience-example-cards-item_content">
-                            <p className="react-rainbow-experience-example-cards-item_example-header" >{data.title[0].text}</p>
-                            <p className="react-rainbow-experience-example-cards-item_description">{data.description[0].text}</p>
+                        <div className="react-rainbow-experience-example-cards_content">
+                            <p className="react-rainbow-experience-example-cards_example-header" >{data.title[0].text}</p>
+                            <p className="react-rainbow-experience-example-cards_description">{data.description[0].text}</p>
                         </div>
                     )}>
-                    <div className="react-rainbow-experience-example-cards-item_example-img-container">
+                    <div className="react-rainbow-experience-example-cards_example-img-container">
                         <img
-                            className="react-rainbow-experience-example-cards-item_example-img"
+                            className="react-rainbow-experience-example-cards_example-img"
                             src={data.image.url}
                             alt={data.title[0].text} />
                     </div>
                 </Card>
             </a>
             <a
-                className="react-rainbow-experience-example-cards-item_github-link"
+                className="react-rainbow-experience-example-cards_github-link"
                 href={data['github-url'].url}
                 target="_blank"
                 rel="noopener noreferrer">
