9629b2ada0d7b056ff55036d9617eb070265ed3d
Reinier Guerra <reiniergs@gmail.com>
2018-12-22 15:30:46 -0800

    doc: update column docs

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -7,14 +7,24 @@ export default function Column() {
 }
 
 Column.propTypes = {
-    /* The header of the column */
+    /** 
+    * The header of the column. it could just an String with text is going to be rendered or component 
+    * with a desired content.
+    */
     header: PropTypes.oneOfType([
         PropTypes.node,
         PropTypes.string,
     ]),
-    /** The component with wich to render the content of each cell */
+    /** 
+    * The component class or function that is going to be use to render the content of each cell on this column. 
+    * By default the cell is going to render the computed value(data[rowIndex][field]) for each cell.   
+    */
     component: PropTypes.func,
-    /** The field to display */
+    /** 
+    * The field value is used to compute/map the value is going to be render for each cell. it's the name of a property 
+    * in the data objects. e.g data = [{ name: 'Max', ... }, {...}]; field could be 'name' for a column that want to represent names 
+    * on a collection of people.  
+    */
     field: PropTypes.string.isRequired,
 };
 
