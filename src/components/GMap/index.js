eed623e1764e7b2acafa9c12ff6ea6a839fa6665
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2018-12-28 21:50:22 -0600

    Update index.js

--- src/components/GMap/index.js
+++ src/components/GMap/index.js
@@ -15,8 +15,8 @@ export default function GMap(props) {
 
 GMap.propTypes = {
     /** The application's API key. To use the Google Maps JavaScript API,
-     * you must get an apiKey. See https://console.cloud.google.com/google/maps-apis/overview
-     * to get an apiKey. */
+     * you must get an API Key. See https://console.cloud.google.com/google/maps-apis/overview
+     * to get an API Key. */
     apiKey: PropTypes.string.isRequired,
     /** The angular distance of a place north or south of the earth's equator. */
     latitude: PropTypes.number,
