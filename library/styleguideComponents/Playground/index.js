e5c28f3ba3045465dbb5feb071e894843e3021ef
LeandroTorresSicilia <jltorressicilia@gmail.com>
2019-01-08 16:36:52 -0500

    fix: fix playground component

--- library/styleguideComponents/Playground/index.js
+++ library/styleguideComponents/Playground/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 
+const pages = [
+    'GettingStarted',
+    'Experiences',
+    'Designs',
+];
+
+function isComponentsPage(name) {
+    return pages.some(page => page !== name);
+}
+
 export default function PlaygroundRenderer(props) {
     const {
         name,
@@ -13,22 +23,22 @@ export default function PlaygroundRenderer(props) {
     } = props;
     const { ...rest } = previewProps;
 
-    if (name === 'GettingStarted' || name === 'Experiences') {
+    if (isComponentsPage(name)) {
         return (
-            <div className="rainbow-position_relative">
-                {preview}
+            <div className="rainbow-position_relative rainbow-m-bottom_large">
+                {toolbar}
+                <article {...rest} className="react-rainbow-playground" data-preview={name}>
+                    {preview}
+                </article>
+                <div className="rainbow-flex rainbow-justify_end rainbow-p-vertical_xx-small">{tabButtons}</div>
+                <div>{tabBody}</div>
             </div>
         );
     }
 
     return (
-        <div className="rainbow-position_relative rainbow-m-bottom_large">
-            {toolbar}
-            <article {...rest} className="react-rainbow-playground" data-preview={name}>
-                {preview}
-            </article>
-            <div className="rainbow-flex rainbow-justify_end rainbow-p-vertical_xx-small">{tabButtons}</div>
-            <div>{tabBody}</div>
+        <div className="rainbow-position_relative">
+            {preview}
         </div>
     );
 }
