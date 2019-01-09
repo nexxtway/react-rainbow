bafb391da9e1806ec8e79fb19257d99272a65e6c
cmcartaya <cmcartaya1sm@gmail.com>
2019-01-08 22:55:37 -0500

    fix: fix resolveCurrentUrl in library sidebar

--- library/styleguideComponents/ComponentsList/index.js
+++ library/styleguideComponents/ComponentsList/index.js
@@ -5,8 +5,15 @@ import ExperiencesIcon from './icons/experiencesIcon';
 import PuzzleIcon from './icons/puzzleIcon';
 import StartupIcon from './icons/startupIcon';
 
+const url = window.location.href.split('#/')[1];
+
 function resolveCurrentUrl() {
-    return window.location.href.split('#/')[1] || 'GettingStarted';
+    if ((url === 'GettingStarted') || (url === 'Components') || (url === 'Experiences')) {
+        return url;
+    } else if (url && ((url !== 'GettingStarted') || (url !== 'Components') || (url !== 'Experiences'))) {
+        return 'Components';
+    }
+    return 'GettingStarted';
 }
 
 export default class ComponentsList extends Component {
