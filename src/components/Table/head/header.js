e244d01be440b33928b0094c22c8ae3b3bc110ac
ayanes <alejandro.yanes94@gmail.com>
2019-01-09 12:04:11 -0500

    fix: fixed lint errors

--- src/components/Table/head/header.js
+++ src/components/Table/head/header.js
@@ -100,7 +100,6 @@ export default class Header extends Component {
         const { width } = this.state;
         const {
             content,
-            isSelected,
             isResizable,
             sortDirection,
             minColumnWidth,
