ed0f5ab7b7aa78ba3e31a1827cf36acdb13372a3
Daren <daren@kmacho.net>
2019-01-11 21:22:23 -0500

    tab sidebarItem test

--- integration/specs/Sidebar/sidebar-1.spec.js
+++ integration/specs/Sidebar/sidebar-1.spec.js
@@ -1,4 +1,5 @@
 const PageSidebar = require('../../../src/components/Sidebar/pageObject');
+const { TAB_KEY, ENTER_KEY } = require('../../constants');
 
 const SIDEBAR = '#sidebar-1';
 
@@ -12,5 +13,14 @@ describe('Sidebar with multiple items', () => {
         const sidebarItem = sidebar.getItem(1);
         sidebarItem.click();
         expect(sidebarItem.isActive()).toBe(true);
+    });
+    it('should make the second item active when first item is active and press TAB and ENTER key', () => {
+        const sidebar = new PageSidebar(SIDEBAR);
+        const sidebarItemOne = sidebar.getItem(0);
+        const sidebarItemTwo = sidebar.getItem(1);
+        sidebarItemOne.click();
+        browser.keys(TAB_KEY);
+        browser.keys(ENTER_KEY);
+        expect(sidebarItemTwo.isActive()).toBe(true);
     });
 });
