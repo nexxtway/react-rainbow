0f3a1bc2583ef53eeb8d97d93e2b795bfd5ea42f
LeandroTorresSicilia <jltorressicilia@gmail.com>
2019-01-13 21:24:08 -0500

    fix: hide recaptcha key (#528)

--- library/setup.js
+++ library/setup.js
@@ -11,4 +11,6 @@ global.FeatureList = FeatureList;
 global.ExperienceExamplesPage = ExperienceExamplesPage;
 global.GettingStartedPage = GettingStartedPage;
 global.DesignsPage = DesignsPage;
+
 global.LIBRARY_GOOGLE_MAPS_APIKEY = process.env.REACT_APP_GOOGLE_MAPS_APIKEY;
+global.LIBRARY_RECAPTCHA_APIKEY = process.env.REACT_APP_RECAPTCHA_APIKEY;
