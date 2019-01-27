f59a9a5267558b140fe626fd74c53620491d6303
TahimiLeonBravo <tahimileon@gmail.com>
2019-01-26 23:17:24 -0600

    feat: add new designs

---
+++ library/pages/Designs/ComingSoon/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DesignDetails from './../../components/DesignDetails';
+
+export default function ComingSoon() {
+    return (
+        <DesignDetails
+            category="websites"
+            title="Rainbow Coming soon"
+            viewsAmount="20"
+            downloadsAmount="333"
+            imageSrc="/images/designsImages/coming-soon.png"
+            previewUrl="/images/designsImages/coming-soon.png"
+            sketchUrl=""
+            description="Minimalistic, modern and creative design for the Rainbow Coming soon page." />
+    );
+}
---
+++ library/pages/Designs/CreateProfile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DesignDetails from './../../components/DesignDetails';
+
+export default function CreateProfile() {
+    return (
+        <DesignDetails
+            category="websites"
+            title="Rainbow Create Profile"
+            viewsAmount="20"
+            downloadsAmount="333"
+            imageSrc="/images/designsImages/create-profile.png"
+            previewUrl="/images/designsImages/create-profile.png"
+            sketchUrl=""
+            description="The Rainbow Create Profile is one of the handiest elements that can be used inside a project. We highly recommend it not just as a design feature, but as a way of guiding your users through tough forms." />
+    );
+}
