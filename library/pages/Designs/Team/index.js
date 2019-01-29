78db0c494a306f14f8545c76f875dc1d96a8d68f
TahimiLeonBravo <tahimileon@gmail.com>
2019-01-29 17:49:09 -0600

    feat: add url download file

--- library/pages/Designs/Team/index.js
+++ library/pages/Designs/Team/index.js
@@ -10,7 +10,7 @@ export default function Team() {
             downloadsAmount="333"
             imageSrc="/images/designsImages/team.png"
             previewUrl="/images/designsImages/team.png"
-            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow-admin.appspot.com/o/rainbow-create-profile.sketch?alt=media&token=51d8bc8b-bfdc-4e95-9bfa-1f3e5a0bf3cb"
+            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow-admin.appspot.com/o/rainbow-team.sketch?alt=media&token=ce866b8b-52e0-407a-8944-ef9493304d12"
             description="Rainbow Team is one of the elements that can be used inside a project. We are curious to see how you want to use it and also improve it, so let us know any feedback you have." />
     );
 }
