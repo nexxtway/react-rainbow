56817b9070fb11c4558ff12b26b7be7f4dfb4685
TahimiLeonBravo <tahimileon@gmail.com>
2019-03-20 20:30:42 -0600

    style: implement UI calendar modal

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import RequiredAsterisk from '../RequiredAsterisk';
-import CalendarIcon from './calendarIcon';
+import CalendarIcon from './icons/calendarIcon';
+import RightIcon from './icons/rightArrow';
+import LeftIcon from './icons/leftArrow';
 import Modal from './../Modal';
+import ButtonIcon from './../ButtonIcon';
+import Select from './../Select';
 import './styles.css';
 
 /**
- * Text inputs are used for freeform data entry.
+ * A datepicker is a text input to capture a date.
  * @category Form
  */
-
-// eslint-disable-next-line react/prefer-stateless-function
 export default class DatePicker extends Component {
     constructor(props) {
         super(props);
@@ -50,6 +52,12 @@ export default class DatePicker extends Component {
             'rainbow-date-picker_label--hide': hideLabel,
         });
 
+        const options = [
+            { value: '2018', label: '2018' },
+            { value: '2019', label: '2019' },
+            { value: '2020', label: '2020' },
+        ];
+
         return (
             <div className={getContainerClassNames()} style={style}>
                 <label className={getLabelClassNames()} htmlFor={inputId} id={id}>
@@ -68,7 +76,172 @@ export default class DatePicker extends Component {
                     <CalendarIcon className="rainbow-date-picker_icon" onClick={() => this.openModal()} />
                 </div>
                 <Modal isOpen={isOpen} onRequestClose={() => this.closeModal()}>
-                    OK
+                    <section className="rainbow-date-picker_modal-container">
+                        <article className="rainbow-date-picker_calendar-container">
+                            <div className="rainbow-date-picker_calendar-month-container">
+                                <ButtonIcon size="medium" icon={<LeftIcon />} />
+                                <h3 className="rainbow-date-picker_calendar-month-text">
+                                    Month
+                                </h3>
+                                <ButtonIcon size="medium" icon={<RightIcon />} />
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Sunday">Sun</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Monday">Mon</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Tuesday">Tue</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Wednesday">Wed</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Thursday">Thu</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Friday">Fri</abbr>
+                                        </th>
+                                        <th className="rainbow-date-picker_calendar-header">
+                                            <abbr title="Saturday">Sat</abbr>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">1</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">2</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">3</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">4</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">5</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">6</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">7</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">8</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">9</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">10</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">11</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">12</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">13</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">14</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">15</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button--selected">16</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">17</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">18</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">19</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">20</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">21</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">22</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">23</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">24</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">25</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">26</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">27</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">28</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">29</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">30</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button">31</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button--disabled">1</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button--disabled">2</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button--disabled">3</button>
+                                        </td>
+                                        <td className="rainbow-date-picker_calendar-day">
+                                            <button className="rainbow-date-picker_calendar-day-button--disabled">4</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </article>
+                        <article className="rainbow-date-picker_calendar-details-container">
+                            <div className="rainbow-date-picker_calendar-details-text-container">
+                                <h1 className="rainbow-date-picker_calendar-details-day">
+                                    16
+                                </h1>
+                                <h2 className="rainbow-date-picker_calendar-details-week">
+                                    Monday
+                                </h2>
+                            </div>
+                            <Select className="rainbow-date-picker_calendar-select-year" options={options} />
+                        </article>
+                    </section>
                 </Modal>
             </div>
         );
