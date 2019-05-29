9b81d7efd6316810f09b3283bc87a8ec4262db92
Wilder González Díaz <wildergd@gmail.com>
2019-05-28 21:41:39 -0400

    test: add tests

---
+++ src/components/RadioButtonGroup/pageObject/radioButton.js
@@ -0,0 +1,47 @@
+/**
+ * Radio page object class.
+ * @class
+ */
+class PageRadioButtonItem {
+    /**
+     * Create a new RadioButton page object.
+     * @constructor
+     * @param {string} rootElement - The selector of the Radio root element.
+     */
+    constructor(rootElement) {
+        this.rootElement = rootElement;
+    }
+
+    /** Click the radio.
+     * @method
+     */
+    click() {
+        $(this.rootElement)
+            .$('label')
+            .click();
+    }
+
+    /**
+     * Returns true when the radio has the focus.
+     * @method
+     * @returns {bool}
+     */
+    hasFocus() {
+        return $(this.rootElement)
+            .$('input[type="radio"]')
+            .isFocused();
+    }
+
+    /**
+     * Returns true when the radio is checked.
+     * @method
+     * @returns {bool}
+     */
+    isChecked() {
+        return !!$(this.rootElement)
+            .$('input[type="radio"]')
+            .isSelected();
+    }
+}
+
+module.exports = PageRadioButtonItem;
