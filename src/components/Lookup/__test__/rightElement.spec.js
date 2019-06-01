fd89dad6af77e9983718fb5836fc14fe84d3685b
Wilder González Díaz <wildergd@gmail.com>
2019-06-01 12:42:26 -0400

    test: add icon and closeIcon props to RightElement tests

--- src/components/Lookup/__test__/rightElement.spec.js
+++ src/components/Lookup/__test__/rightElement.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import RightElement from '../rightElement';
+import SearchIcon from '../icons/searchIcon';
+import CloseIcon from '../icons/closeIcon';
 
 describe('<RightElement />', () => {
     it('should render an icon container', () => {
@@ -16,5 +18,13 @@ describe('<RightElement />', () => {
         const component = mount(<RightElement showCloseButton onClear={onClearMockFn} />);
         component.find('ButtonIcon').simulate('click');
         expect(onClearMockFn).toHaveBeenCalledTimes(1);
+    });
+    it('should render an icon container with custom icon', () => {
+        const component = mount(<RightElement searchIcon={<CloseIcon />} />);
+        expect(component.find('span.rainbow-lookup_input-icon').exists()).toBe(true);
+    });
+    it('should render an icon container with custom closeIcon', () => {
+        const component = mount(<RightElement closeIcon={<SearchIcon />} />);
+        expect(component.find('span.rainbow-lookup_input-icon').exists()).toBe(true);
     });
 });
