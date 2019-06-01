33aef4c8f488b82c56ef6d19eac9b5f5ed232622
Wilder González Díaz <wildergd@gmail.com>
2019-06-01 12:54:20 -0400

    test: remove icon and closeIcon tests

--- src/components/Lookup/__test__/rightElement.spec.js
+++ src/components/Lookup/__test__/rightElement.spec.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import RightElement from '../rightElement';
-import SearchIcon from '../icons/searchIcon';
-import CloseIcon from '../icons/closeIcon';
 
 describe('<RightElement />', () => {
     it('should render an icon container', () => {
@@ -18,13 +16,5 @@ describe('<RightElement />', () => {
         const component = mount(<RightElement showCloseButton onClear={onClearMockFn} />);
         component.find('ButtonIcon').simulate('click');
         expect(onClearMockFn).toHaveBeenCalledTimes(1);
-    });
-    it('should render an icon container with custom icon', () => {
-        const component = mount(<RightElement icon={<CloseIcon />} />);
-        expect(component.find('span.rainbow-lookup_input-icon').exists()).toBe(true);
-    });
-    it('should render an icon container with custom closeIcon', () => {
-        const component = mount(<RightElement closeIcon={<SearchIcon />} />);
-        expect(component.find('span.rainbow-lookup_input-icon').exists()).toBe(true);
     });
 });
