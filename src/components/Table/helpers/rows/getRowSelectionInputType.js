92c52c27332b745a3b41e8aa76e61504a40ee62c
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-06-08 12:22:22 -0400

    fix: use checkbox when only one row (#791)

--- src/components/Table/helpers/rows/getRowSelectionInputType.js
+++ src/components/Table/helpers/rows/getRowSelectionInputType.js
@@ -1,5 +1,5 @@
-export default function getRowSelectionInputType(maxRowSelection) {
-    if (maxRowSelection === 1) {
+export default function getRowSelectionInputType(maxRowSelection, rowsLength) {
+    if (maxRowSelection === 1 && rowsLength !== 1) {
         return 'radio';
     }
     return 'checkbox';
