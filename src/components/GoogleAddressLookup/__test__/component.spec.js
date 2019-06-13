27ad351167ac9986e4e32a06ad8cae499827c061
Wilder González Díaz <wildergd@gmail.com>
2019-06-13 12:22:54 -0400

    test: fis component tests

--- src/components/GoogleAddressLookup/__test__/component.spec.js
+++ src/components/GoogleAddressLookup/__test__/component.spec.js
@@ -2,20 +2,36 @@ import React from 'react';
 import { mount } from 'enzyme';
 import PlacesLookupComponent from '../component';
 
-global.google = {
-    maps: {
-        places: {
-            AutocompleteService: jest.fn(() => ({
-                getPlacePredictions: jest.fn(),
-            })),
+// global.google = {
+//     maps: {
+//         places: {
+//             AutocompleteService: jest.fn(() => ({
+//                 getPlacePredictions: jest.fn(),
+//             })),
+//         },
+//     },
+// };
+
+const setupGoogleMapsMock = () => {
+    const google = {
+        maps: {
+            places: {
+                AutocompleteService: jest.fn(),
+            },
         },
-    },
+    };
+
+    global.window.google = google;
 };
 
 const nextProps = {
     isScriptLoaded: true,
     isScriptLoadSucceed: true,
 };
+
+beforeAll(() => {
+    setupGoogleMapsMock();
+});
 
 describe('<PlacesLookupComponent/>', () => {
     it('should have the right class names', () => {
@@ -24,6 +40,7 @@ describe('<PlacesLookupComponent/>', () => {
             true,
         );
     });
+
     it('should not call google.maps.places.AutocompleteService when isScriptLoaded and isScriptLoadSucceed are false', () => {
         const component = mount(<PlacesLookupComponent className="some-class" />);
         component.setProps({
@@ -32,17 +49,34 @@ describe('<PlacesLookupComponent/>', () => {
         });
         expect(global.google.maps.places.AutocompleteService).not.toHaveBeenCalled();
     });
+
     it('should not call google.maps.places.AutocompleteService when isScriptLoaded is true and isScriptLoadSucceed is false', () => {
-        const component = mount(<PlacesLookupComponent className="some-class" />);
+        const component = mount(<PlacesLookupComponent />);
         component.setProps({
             isScriptLoaded: true,
             isScriptLoadSucceed: false,
         });
         expect(global.google.maps.places.AutocompleteService).not.toHaveBeenCalled();
     });
+
     it('should not call AutocompleteService when isScriptLoaded and isScriptLoadSucceed are true but previous isScriptLoaded was true', () => {
-        const component = mount(<PlacesLookupComponent className="some-class" isScriptLoaded />);
+        const component = mount(<PlacesLookupComponent isScriptLoaded />);
         component.setProps(nextProps);
         expect(global.google.maps.places.AutocompleteService).not.toHaveBeenCalled();
     });
+
+    it('should call google.maps.places.AutocompleteService', () => {
+        const component = mount(<PlacesLookupComponent />);
+        component.setProps(nextProps);
+        expect(global.google.maps.places.AutocompleteService).toHaveBeenCalled();
+    });
+
+    it('should be intialized after isScriptLoaded and isScriptLoadSucceed are set to true', () => {
+        const component = mount(<PlacesLookupComponent />);
+        expect(component.instance().fieldRef.current.initialized).toBe(false);
+        component.setProps(nextProps);
+        expect(component.instance().fieldRef.current.initialized).toBe(true);
+    });
+
+    it('should call onChange with the right data when select an option', () => {});
 });
