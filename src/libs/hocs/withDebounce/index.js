2db7c185266dcecb94b3521deb88e482d18eb13f
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-07-06 00:47:16 -0400

    feat: implement withDebounce hoc (#826)

---
+++ src/libs/hocs/withDebounce/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import debounce from '../../debounce';
+
+export default function withDebounce(WrappedComponent) {
+    return class extends Component {
+        constructor(props) {
+            super(props);
+            this.fieldRef = React.createRef();
+            this.handleChange = this.handleChange.bind(this);
+            this.debouncedChange = debounce(this.debouncedChange, props.debounceTimeout);
+        }
+
+        handleChange(event) {
+            event.persist();
+            this.debouncedChange(event);
+        }
+
+        debouncedChange(event) {
+            const { onChange } = this.props;
+            onChange(event);
+        }
+
+        focus() {
+            this.fieldRef.current.focus();
+        }
+
+        click() {
+            this.fieldRef.current.click();
+        }
+
+        blur() {
+            this.fieldRef.current.blur();
+        }
+
+        render() {
+            const { onChange, ...rest } = this.props;
+
+            return <WrappedComponent {...rest} onChange={this.handleChange} ref={this.fieldRef} />;
+        }
+    };
+}
