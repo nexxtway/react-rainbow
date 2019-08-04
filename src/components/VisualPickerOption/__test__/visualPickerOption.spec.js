d4ae08cfd64c96d3008bf9832b34eff885c50b71
Wilder González Díaz <wildergd@gmail.com>
2019-08-03 20:01:24 -0400

    test: add more tests

--- src/components/VisualPickerOption/__test__/visualPickerOption.spec.js
+++ src/components/VisualPickerOption/__test__/visualPickerOption.spec.js
@@ -16,14 +16,6 @@ describe('<VisualPickerOption/>', () => {
         const component = mount(<VisualPickerOption name="option1" groupName={groupName} />);
         expect(component.find('input').prop('name')).toBe(groupName);
     });
-    it('should mark the input element as checked', () => {
-        const value1 = 'option1';
-        const value2 = ['option1'];
-        const component1 = mount(<VisualPickerOption name="option1" value={value1} />);
-        const component2 = mount(<VisualPickerOption name="option1" multiple value={value2} />);
-        expect(component1.find('input').prop('checked')).toBe(true);
-        expect(component2.find('input').prop('checked')).toBe(true);
-    });
     it('should mark the input element as checked when is not multiple', () => {
         const value = 'option1';
         const component = mount(<VisualPickerOption name="option1" value={value} />);
