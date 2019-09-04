d79376b6cbf84e733962773d31457c7613f8a9dd
cmcartaya <cmcartaya1sm@gmail.com>
2019-09-04 15:10:53 -0400

    fix: fix some comments

--- src/components/TimePicker/__test__/ampmSelect.spec.js
+++ src/components/TimePicker/__test__/ampmSelect.spec.js
@@ -5,14 +5,15 @@ import AmPmSelect from './../ampmSelect';
 describe('<AmPmSelect/>', () => {
     it('should render the right markup initially when component is not focused', () => {
         const component = mount(<AmPmSelect tabIndex="-1" />);
-        component.setState({ isFocused: false });
         expect(
             component.find('input[className="rainbow-time-picker_time-select-value"]').exists(),
         ).toBe(true);
     });
     it('should render the right markup when component is focused', () => {
         const component = mount(<AmPmSelect />);
-        component.setState({ isFocused: true });
+        component
+            .find('input[className="rainbow-time-picker_time-select-value"]')
+            .simulate('focus');
         expect(
             component
                 .find(
@@ -26,26 +27,24 @@ describe('<AmPmSelect/>', () => {
     it('should fire the onChange event with "AM" when the component get focus and value and defaultValue are undefined', () => {
         const onChangeMckFn = jest.fn();
         const component = mount(<AmPmSelect onChange={onChangeMckFn} />);
-        component.setState({ isFocused: false });
         component
             .find('input[className="rainbow-time-picker_time-select-value"]')
             .simulate('focus');
         expect(component.prop('onChange')).toHaveBeenCalledWith('AM');
-        expect(component.state('isFocused')).toBe(true);
     });
     it('should fire the onChange event with the defaultValue passed when the component get focus and the prop value is undefined', () => {
         const onChangeMckFn = jest.fn();
         const component = mount(<AmPmSelect defaultValue="PM" onChange={onChangeMckFn} />);
-        component.setState({ isFocused: false });
         component
             .find('input[className="rainbow-time-picker_time-select-value"]')
             .simulate('focus');
         expect(component.prop('onChange')).toHaveBeenCalledWith('PM');
-        expect(component.state('isFocused')).toBe(true);
     });
     it('should render the initial input when component lost focus', () => {
         const component = mount(<AmPmSelect />);
-        component.setState({ isFocused: true });
+        component
+            .find('input[className="rainbow-time-picker_time-select-value"]')
+            .simulate('focus');
         component
             .find(
                 'fieldset[className="rainbow-time-picker_time-select-value rainbow-time-picker_select-ampm"]',
@@ -57,7 +56,9 @@ describe('<AmPmSelect/>', () => {
     });
     it('should focus the component when the focus method is called and the component is focused', () => {
         const component = mount(<AmPmSelect tabIndex="-1" />);
-        component.setState({ isFocused: true });
+        component
+            .find('input[className="rainbow-time-picker_time-select-value"]')
+            .simulate('focus');
         component.instance().fieldsetRef.current.focus();
         const focusedElementDataId = document.activeElement.getAttribute('data-id');
         const fieldsetDataId = component.find('fieldset').prop('data-id');
@@ -65,7 +66,6 @@ describe('<AmPmSelect/>', () => {
     });
     it('should focus the component when the focus method is called and the component is not focused', () => {
         const component = mount(<AmPmSelect tabIndex="-1" />);
-        component.setState({ isFocused: false });
         component.instance().fieldsetRef.current.focus();
         const focusedElementDataId = document.activeElement.getAttribute('data-id');
         const fieldsetDataId = component.find('input').prop('data-id');
@@ -74,7 +74,9 @@ describe('<AmPmSelect/>', () => {
     it('should call event.stopPropagation when component is focused and the input with value "AM" is blurred', () => {
         const stopPropagationMockFn = jest.fn();
         const component = mount(<AmPmSelect />);
-        component.setState({ isFocused: true });
+        component
+            .find('input[className="rainbow-time-picker_time-select-value"]')
+            .simulate('focus');
         component.find('input[value="AM"]').simulate('blur', {
             stopPropagation: stopPropagationMockFn,
         });
@@ -83,7 +85,9 @@ describe('<AmPmSelect/>', () => {
     it('should call event.stopPropagation when component is focused and the input with value "PM" is blurred', () => {
         const stopPropagationMockFn = jest.fn();
         const component = mount(<AmPmSelect />);
-        component.setState({ isFocused: true });
+        component
+            .find('input[className="rainbow-time-picker_time-select-value"]')
+            .simulate('focus');
         component.find('input[value="PM"]').simulate('blur', {
             stopPropagation: stopPropagationMockFn,
         });
