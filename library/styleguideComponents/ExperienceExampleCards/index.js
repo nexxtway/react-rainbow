5af6743ba5b25be1c90f8905b95f78f6b11a9a94
TahimiLeonBravo <tahimileon@gmail.com>
2019-09-12 11:26:30 -0500

    fix: experience example card

--- library/styleguideComponents/ExperienceExampleCards/index.js
+++ library/styleguideComponents/ExperienceExampleCards/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../../../src/components/Card';
+import RenderIf from '../../../src/components/RenderIf';
 import GithubIcon from './githubIcon';
 import './styles.css';
 
@@ -11,32 +12,40 @@ export default function ExperienceExampleCards({ results }) {
                 className="react-rainbow-experience-example-cards_example-link"
                 href={data['example-url'].url}
                 target="_blank"
-                rel="noopener noreferrer">
-
+                rel="noopener noreferrer"
+            >
                 <Card
                     className="react-rainbow-experience-example-cards"
-                    footer={(
+                    footer={
                         <div className="react-rainbow-experience-example-cards_content">
-                            <p className="react-rainbow-experience-example-cards_example-header" >{data.title[0].text}</p>
-                            <p className="react-rainbow-experience-example-cards_description">{data.description[0].text}</p>
+                            <p className="react-rainbow-experience-example-cards_example-header">
+                                {data.title[0].text}
+                            </p>
+                            <p className="react-rainbow-experience-example-cards_description">
+                                {data.description[0].text}
+                            </p>
                         </div>
-                    )}>
+                    }
+                >
                     <div className="react-rainbow-experience-example-cards_example-img-container">
                         <img
                             className="react-rainbow-experience-example-cards_example-img"
                             src={data.image.url}
-                            alt={data.title[0].text} />
+                            alt={data.title[0].text}
+                        />
                     </div>
                 </Card>
             </a>
-            <a
-                className="react-rainbow-experience-example-cards_github-link"
-                href={data['github-url'].url}
-                target="_blank"
-                rel="noopener noreferrer">
-
-                <GithubIcon />
-            </a>
+            <RenderIf isTrue={!!data['github-url'].url}>
+                <a
+                    className="react-rainbow-experience-example-cards_github-link"
+                    href={data['github-url'].url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <GithubIcon />
+                </a>
+            </RenderIf>
         </section>
     ));
 }
