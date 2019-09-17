691dbf385134e59ae0abf06a8a6f940ead918d84
Wilder González Díaz <wildergd@gmail.com>
2019-09-17 15:35:21 -0400

    feat: implement progress circular component (#936)

---
+++ src/components/ProgressCircular/__test__/progressCircular.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProgressCircular from './../index';
+
+describe('<ProgressCircular/>', () => {
+    it('should be accesible', () => {
+        const component = shallow(<ProgressCircular value={25} />);
+        expect(component.find('div[role="progressbar"]').exists()).toBe(true);
+        expect(component.find('div[aria-valuemax="100"]').exists()).toBe(true);
+        expect(component.find('div[aria-valuemin="0"]').exists()).toBe(true);
+        expect(component.find('div[aria-valuenow=25]').exists()).toBe(true);
+    });
+    it('should has set the value passed', () => {
+        const component = shallow(<ProgressCircular value={25} />);
+        expect(component.find('ProgressRing').prop('percent')).toBe(25);
+        expect(component.find('.rainbow-progress-circular_percent-text').text()).toBe('25%');
+    });
+    it('should have the right class names when varaint success', () => {
+        const component = mount(<ProgressCircular variant="success" />);
+        const container = component.find(
+            'div.rainbow-progress-circular.rainbow-progress-circular--success',
+        );
+        expect(container.exists()).toBe(true);
+    });
+});
