90f86f5c6c6ab23e89722e6bbe994792300ce5f8
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-09-21 04:28:24 -0400

    fix: remove StyledBadge tests

--- setupTests.js
+++ setupTests.js
@@ -2,7 +2,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { toHaveNoViolations } from 'jest-axe';
-import 'jest-styled-components';
 import toBeFocusable from './jestMatchers/toBeFocusable';
 
 configure({ adapter: new Adapter() });
