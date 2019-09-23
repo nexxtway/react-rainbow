cdbd5324f1c051bcd28003a13b699ab5dd941b1e
Reinier Guerra <reinier.guerra@salesforce.com>
2019-09-23 18:54:58 -0400

    chore: add typescript support first commit

--- src/components/Textarea/__test__/textarea.a11y.spec.js
+++ src/components/Textarea/__test__/textarea.a11y.spec.js
@@ -3,7 +3,7 @@ import ReactDOMServer from 'react-dom/server';
 import { axe } from 'jest-axe';
 import Textarea from './../';
 
-describe('<Textarea/>', () => {
+describe('Textarea', () => {
     it('should be accessible when label is passed', async () => {
         expect.assertions(1);
         const html = ReactDOMServer.renderToString(<Textarea label="Textarea Label" />);
