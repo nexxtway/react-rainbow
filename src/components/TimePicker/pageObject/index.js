036020de09c214db08f8f17aa37728e3544ca356
cmcartaya <cmcartaya1sm@gmail.com>
2019-09-22 21:39:46 -0400

    fix: fix a isOpen function on TimePicker pageObject

--- src/components/TimePicker/pageObject/index.js
+++ src/components/TimePicker/pageObject/index.js
@@ -84,29 +84,32 @@ class PageTimePicker {
      * @returns {bool}
      */
     isOpen() {
-        return (
-            $(timePickerModalId)
-                .$('button[id="time-picker_cancel-button"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('button[id="time-picker_ok-button"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('button[id="time-picker_up-button"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('button[id="time-picker_down-button"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('input[data-id="minutes"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('input[data-id="hour"]')
-                .isDisplayed() &&
-            $(timePickerModalId)
-                .$('input[data-id="input-element"]')
-                .isDisplayed()
-        );
+        if ($(timePickerModalId).isDisplayed()) {
+            return (
+                $(timePickerModalId)
+                    .$('button[id="time-picker_cancel-button"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('button[id="time-picker_ok-button"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('button[id="time-picker_up-button"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('button[id="time-picker_down-button"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('input[data-id="minutes"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('input[data-id="hour"]')
+                    .isDisplayed() &&
+                $(timePickerModalId)
+                    .$('input[data-id="input-element"]')
+                    .isDisplayed()
+            );
+        }
+        return false;
     }
 
     /**
