207b7f2a010d25ba971ab5894915b76b3d087319
Tahimi <tahimileon@gmail.com>
2019-09-24 18:18:59 -0500

    fix: scketch url (#1009)

--- library/pages/Designs/Authentication/index.js
+++ library/pages/Designs/Authentication/index.js
@@ -11,8 +11,9 @@ export default function Authentication() {
             imageSrc="/images/designsImages/authentication.png"
             previewUrl="/images/designsImages/authentication.png"
             liveUrl="https://react-rainbow-firebase-auth.firebaseapp.com/home/signin"
-            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow-admin.appspot.com/o/rainbow-authentication.sketch?alt=media&token=fff0b5e0-b5ac-4d02-b529-a1fa39bac3d5"
+            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow.appspot.com/o/sketch%20designs%2Frainbow-authentication.sketch?alt=media&token=9a144d78-df0e-4fbe-b48c-b1f0fa3ffeae"
             description="A minimum Rainbow example combines colors that are easy on the eye. Can be used to authenticate any website.
-            Super easy to custom everything in the layouts, you will be free to mixing the elements and components to create your own design with your Business.  " />
+            Super easy to custom everything in the layouts, you will be free to mixing the elements and components to create your own design with your Business.  "
+        />
     );
 }
--- library/pages/Designs/SocialIconsSet/index.js
+++ library/pages/Designs/SocialIconsSet/index.js
@@ -10,9 +10,9 @@ export default function SocialIconsSet() {
             viewsAmount="20"
             downloadsAmount="333"
             imageSrc="/images/designsImages/social.png"
-            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow-admin.appspot.com/o/rainbow-social-elements.sketch?alt=media&token=2a0e038a-c5c3-490c-8d66-1cd55cb5b774"
-            previewUrl="/images/designsImages/social.png">
-
+            sketchUrl="https://firebasestorage.googleapis.com/v0/b/react-rainbow.appspot.com/o/sketch%20designs%2Frainbow-social-elements.sketch?alt=media&token=81939c4c-a830-41c7-8de0-f4b3a4b0d234"
+            previewUrl="/images/designsImages/social.png"
+        >
             <h2 className="react-rainbow-rainbow-components_subtitle">
                 How to begin using this library?
             </h2>
@@ -21,15 +21,13 @@ export default function SocialIconsSet() {
             </p>
             <ol className="react-rainbow-rainbow-components_list">
                 <li className="react-rainbow-rainbow-components_list-element">
-                    Download the file, save in your local computer and follow the instructions in the file's Instructions section.
+                    Download the file, save in your local computer and follow the instructions in
+                    the file's Instructions section.
                 </li>
                 <li className="react-rainbow-rainbow-components_list-element">
                     Add the Sketch document from Sketch Cloud as a Shared Library.
                 </li>
-                <a
-                    href="https://sketch.cloud/s/mEdbz"
-                    rel="noopener noreferrer"
-                    target="_blank">
+                <a href="https://sketch.cloud/s/mEdbz" rel="noopener noreferrer" target="_blank">
                     https://sketch.cloud/s/mEdbz
                 </a>
             </ol>
