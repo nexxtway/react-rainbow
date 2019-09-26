43f993a8e3d12793d1d58a60e467239019dfc19b
TahimiLeonBravo <tahimileon@gmail.com>
2019-09-25 22:56:31 -0500

    feat: define application interface

---
+++ src/components/Application/index.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BaseProps } from './../types';
+
+export interface ApplicationProps extends BaseProps {}
+
+export default function(props: ApplicationProps): JSX.Element | null;
