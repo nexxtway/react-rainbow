9ca83943d5c18c77ace7e4c35eb393e112fec57d
TahimiLeonBravo <tahimileon@gmail.com>
2019-09-26 12:05:34 -0500

    feat: define ProgressCircular interface

---
+++ src/components/ProgressCircular/index.d.ts
@@ -0,0 +1,10 @@
+import { BaseProps } from './../types';
+
+export interface ProgressCircularProps extends BaseProps {
+    value?: number;
+    variant?: 'brand' | 'success' | 'warning' | 'error';
+    assistiveText?: string;
+    assistiveText?: string;
+}
+
+export default function(props: ProgressCircularProps): JSX.Element | null;
