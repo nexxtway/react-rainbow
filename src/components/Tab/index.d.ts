290207feae4567ce27d3156afd182325efeec663
TahimiLeonBravo <tahimileon@gmail.com>
2019-09-26 12:59:54 -0500

    feat: define Tab interface

---
+++ src/components/Tab/index.d.ts
@@ -0,0 +1,12 @@
+import { BaseProps } from './../types';
+
+export interface TabProps extends BaseProps {
+    label?: string | JSX.ElementChildrenAttribute;
+    name: string;
+    title?: string;
+    disabled?: boolean;
+    id?: string;
+    ariaControls?: string;
+}
+
+export default function(props: TabProps): JSX.Element | null;
