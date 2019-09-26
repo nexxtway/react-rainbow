cbc02f7f89afe75c98fa234ba7066b623744e18f
cmcartaya <cmcartaya1sm@gmail.com>
2019-09-26 18:10:42 -0400

    fix: move test to other file

--- src/components/TimePicker/__test__/timeSelect.a11y.spec.js
+++ src/components/TimePicker/__test__/timeSelect.a11y.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { axe } from 'jest-axe';
-import TimePicker from '..';
+import TimeSelect from '../timeSelect';
 
-describe('<TimePicker/>', () => {
+describe('<TimeSelect/>', () => {
     it('should be accessible', async () => {
         expect.assertions(1);
         const html = ReactDOMServer.renderToString(
-            <TimePicker label="Accessibility test on TimePicker" value="18:35" />,
+            <TimeSelect label="Accessibility test on TimeSelect" value="18:35" />,
         );
         const results = await axe(html);
         expect(results).toHaveNoViolations();
