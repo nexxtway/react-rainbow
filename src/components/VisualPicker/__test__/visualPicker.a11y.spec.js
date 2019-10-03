6f2784e0576762d902c96c6a38fa69768169d310
cmcartaya <cmcartaya1sm@gmail.com>
2019-10-03 01:01:32 -0400

    test: improve accesibility test on VisualPickerOption

--- src/components/VisualPicker/__test__/visualPicker.a11y.spec.js
+++ src/components/VisualPicker/__test__/visualPicker.a11y.spec.js
@@ -6,7 +6,7 @@ import VisualPickerOption from '../../VisualPickerOption';
 import VisualPickerOptionFooter from '../../VisualPickerOptionFooter';
 
 describe('<VisualPicker/>', () => {
-    it('should be accessible', async () => {
+    it('should be accessible when label is passed', async () => {
         expect.assertions(1);
         const html = ReactDOMServer.renderToString(
             <VisualPicker label="Our Rainbow Options" value="option-1">
@@ -29,6 +29,60 @@ describe('<VisualPicker/>', () => {
                             description="Everything you need to take support."
                         />
                     }
+                >
+                    <h3>user/month</h3>
+                </VisualPickerOption>
+            </VisualPicker>,
+        );
+        const results = await axe(html);
+        expect(results).toHaveNoViolations();
+    });
+    it('should be accessible when multiple option are selected', async () => {
+        expect.assertions(1);
+        const html = ReactDOMServer.renderToString(
+            <VisualPicker label="Our Rainbow Options" value={['option-1', 'option-2']}>
+                <VisualPickerOption
+                    name="option-1"
+                    footer={
+                        <VisualPickerOptionFooter
+                            label="Rainbow Basic"
+                            description="Complete service for teams of any size."
+                        />
+                    }
+                >
+                    <h3>user/month</h3>
+                </VisualPickerOption>
+                <VisualPickerOption
+                    name="option-2"
+                    footer={
+                        <VisualPickerOptionFooter
+                            label="Rainbow Basic"
+                            description="Everything you need to take support."
+                        />
+                    }
+                >
+                    <h3>user/month</h3>
+                </VisualPickerOption>
+            </VisualPicker>,
+        );
+        const results = await axe(html);
+        expect(results).toHaveNoViolations();
+    });
+    it('should be accessible when label or description is not passed on VisualPickerOptionFooter', async () => {
+        expect.assertions(1);
+        const html = ReactDOMServer.renderToString(
+            <VisualPicker label="Our Rainbow Options" value="option-1">
+                <VisualPickerOption
+                    name="option-1"
+                    footer={
+                        <VisualPickerOptionFooter description="Complete service for teams of any size." />
+                    }
+                >
+                    <h3>user/month</h3>
+                </VisualPickerOption>
+                <VisualPickerOption
+                    name="option-2"
+                    footer={<VisualPickerOptionFooter label="Rainbow Basic" />}
                 >
                     <h3>user/month</h3>
                 </VisualPickerOption>
