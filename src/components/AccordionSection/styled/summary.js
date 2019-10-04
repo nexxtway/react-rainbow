8026d9a620576aa335b782be62a089edb956c83b
Tahimi <tahimileon@gmail.com>
2019-10-04 04:22:36 -0500

    refactor: migrate AccordionSection to styled-components (#1046)
    * refactor: migrate AccordionSection to styled-components
    
    * fix: some fixes
    
    * fix: some test
    
    * fix: integration tests

---
+++ src/components/AccordionSection/styled/summary.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledSummary = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.875rem 1rem;
+`;
+
+export default StyledSummary;
