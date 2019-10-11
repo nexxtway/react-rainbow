9e2d0c55c72ec7567bad4ac19cc090cd7e582f9c
TahimiLeonBravo <tahimileon@gmail.com>
2019-10-11 18:10:01 -0500

    refactor: migrate MenuDivider styled-components

---
+++ src/components/MenuDivider/styled/divider.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { COLOR_GRAY_2 } from '../../../styles/colors';
+
+const StyledDivider = styled.li`
+    border-top: solid 1px ${COLOR_GRAY_2};
+    ${props =>
+        props.variant === 'space' &&
+        `
+            border-top: solid 1px ${COLOR_GRAY_2};
+            margin-top: 0.5rem;
+            padding-top: 0.5rem;
+        `};
+`;
+
+export default StyledDivider;
