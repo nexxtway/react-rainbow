336be9b0b43280843527c8c1596e3c0516583a9a
TahimiLeonBravo <tahimileon@gmail.com>
2019-10-10 23:20:01 -0500

    refactor: migrate GoogleAddressLookup to styled-components

---
+++ src/components/GoogleAddressLookup/styled/optionTyping.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledOptionTyping = styled.span`
+    color: #061c3f;
+    font-weight: 900;
+`;
+
+export default StyledOptionTyping;
---
+++ src/components/GoogleAddressLookup/styled/poweredByGoogleLogo.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import PoweredByGoogleLogo from '../icons/poweredByGoogle';
+
+const StyledPoweredByGoogleLogo = styled(PoweredByGoogleLogo)`
+    height: 0.725rem;
+`;
+
+export default StyledPoweredByGoogleLogo;
