d0dec5020334dc47b0de94ab71743b5c623331ea
TahimiLeonBravo <tahimileon@gmail.com>
2019-10-14 16:22:42 -0500

    test: fix some tests

--- src/components/RequiredAsterisk/__test__/requiredAsterisk.spec.js
+++ src/components/RequiredAsterisk/__test__/requiredAsterisk.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import RequiredAsterisk from './../index';
+import StyledAsterisk from '../StyledAsterisk';
 
 describe('<InputRequiredAsterisk/>', () => {
     it('should render the abbr element when is required', () => {
@@ -9,7 +10,7 @@ describe('<InputRequiredAsterisk/>', () => {
     });
     it('should set the title prop as required in the abbr element when is required', () => {
         const component = mount(<RequiredAsterisk required />);
-        const asterisk = component.find('abbr[className="rainbow-required-asterisk"]');
+        const asterisk = component.find(StyledAsterisk);
         expect(asterisk.prop('title')).toBe('required');
     });
 });
