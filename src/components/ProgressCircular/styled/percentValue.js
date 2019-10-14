b18122d235a0e7a8c1948ddb49f393464651916d
Tahimi <tahimileon@gmail.com>
2019-10-14 01:44:18 -0500

    refactor: migrate ProgressCircular to styled-components (#1086)
    * refactor: migrate ProgressCircular to styles-components
    
    * refactor: remove color
    
    * test: fix test

---
+++ src/components/ProgressCircular/styled/percentValue.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { COLOR_BRAND, COLOR_SUCCESS, COLOR_YELLOW_1, COLOR_ERROR } from '../../../styles/colors';
+
+const StyledPercentValue = styled.h1`
+    font-size: 2rem;
+    font-weight: 400;
+    color: ${COLOR_BRAND};
+    margin: 0;
+    padding: 0;
+    ${props =>
+        props.variant === 'success' &&
+        `
+            color: ${COLOR_SUCCESS};
+        `};
+    ${props =>
+        props.variant === 'warning' &&
+        `
+            color: ${COLOR_YELLOW_1};
+        `};
+    ${props =>
+        props.variant === 'error' &&
+        `
+            color: ${COLOR_ERROR};
+        `};
+`;
+
+export default StyledPercentValue;
