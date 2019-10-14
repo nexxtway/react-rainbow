14eadd1067556fd36a382f1267235c6476faf68a
Tahimi <tahimileon@gmail.com>
2019-10-14 01:34:16 -0500

    refactor: migrate Modal styled components (#1080)
    * refactor: migrate Modal to styled-components
    
    * test: fix some test
    
    * fix: className
    
    * fix: integration tests
    
    * fix: skip failing integration tests

---
+++ src/components/Modal/styled/closeButton.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import ButtonIcon from '../../ButtonIcon';
+
+const StyledCloseButton = styled(ButtonIcon)`
+    position: absolute;
+    top: 0.5rem;
+    right: 0.5rem;
+    z-index: 1000002;
+`;
+
+export default StyledCloseButton;
