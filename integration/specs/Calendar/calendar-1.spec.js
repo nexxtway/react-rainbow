eae3fbf83dde1f90ffacfae01bdc95cb6525065d
Wilder González Díaz <wildergd@gmail.com>
2019-10-22 22:23:49 -0400

    test: add more specs

--- integration/specs/Calendar/calendar-1.spec.js
+++ integration/specs/Calendar/calendar-1.spec.js
@@ -1,26 +1,6 @@
 const PageCalendar = require('../../../src/components/Calendar/pageObject');
-const { TAB_KEY } = require('../../constants');
 
-const CALENDAR = '.calendar-1';
-
-const getMonth = monthIndex => {
-    const indx = ((monthIndex % 12) + 12) % 12;
-    const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-    ];
-    return months[indx];
-};
+const CALENDAR = '#calendar-1';
 
 describe('Calendar', () => {
     beforeAll(() => {
@@ -33,21 +13,25 @@ describe('Calendar', () => {
     });
     it('should change selected month when clicked on left and right arrows', () => {
         const calendar = new PageCalendar(CALENDAR);
-        const prevMonth = getMonth(new Date().getMonth() - 1);
-        const currentMonth = getMonth(new Date().getMonth());
-        const nextMonth = getMonth(new Date().getMonth() + 1);
-        expect(calendar.getSelectedMonth()).toBe(currentMonth);
+        expect(calendar.getSelectedMonth()).toBe('December');
         calendar.clickPrevMonthButton();
-        expect(calendar.getSelectedMonth()).toBe(prevMonth);
+        expect(calendar.getSelectedMonth()).toBe('November');
         calendar.clickNextMonthButton();
         calendar.clickNextMonthButton();
-        expect(calendar.getSelectedMonth()).toBe(nextMonth);
+        expect(calendar.getSelectedMonth()).toBe('January');
     });
     it('should select the rigth day button element', () => {
         const calendar = new PageCalendar(CALENDAR);
-        const day = new Date().getDate();
-        expect(calendar.getSelectedDay()).toBe(`${day}`);
+        expect(calendar.getSelectedDay()).toBe('6');
         calendar.clickDay(4);
         expect(calendar.getSelectedDay()).toBe('4');
+    });
+    it('should change year when month is December and click next month button', () => {
+        const calendar = new PageCalendar(CALENDAR);
+        expect(calendar.getSelectedMonth()).toBe('December');
+        expect(calendar.getSelectedYear()).toBe('2019');
+        calendar.clickNextMonthButton();
+        expect(calendar.getSelectedMonth()).toBe('January');
+        expect(calendar.getSelectedYear()).toBe('2020');
     });
 });
