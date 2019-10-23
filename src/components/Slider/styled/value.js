dc13dae51c9a177a15af1cb36d9cca7c01e58609
Tahimi <tahimileon@gmail.com>
2019-10-23 01:22:00 -0500

    refactor: migrate VerticalNavigation to styled-components (#1109)
    * refactor: migrate VerticalNavigation to styled-components
    
    * test: fix some test
    
    * fix: remove className

--- src/components/Slider/styled/value.js
+++ src/components/Slider/styled/value.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import { COLOR_DARK_2 } from '../../../styles/colors';
+import { COLOR_GRAY_4 } from '../../../styles/colors';
 import { FONT_SIZE_TEXT_LARGE } from '../../../styles/fontSizes';
 
 const StyledValue = styled.span`
     padding: 0 0.5rem;
-    color: ${COLOR_DARK_2};
+    color: ${COLOR_GRAY_4};
     font-size: ${FONT_SIZE_TEXT_LARGE};
 `;
 
