155bf4b6f95fc5e819df8654f26f01811d703397
Carlos Miguel <33812799+cmcartaya@users.noreply.github.com>
2019-11-01 01:24:45 -0400

    review: remove javascript:void(0) on Breadcrumbs component (#1119)

--- src/components/Breadcrumb/styled/button.js
+++ src/components/Breadcrumb/styled/button.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 import { COLOR_BRAND, COLOR_GRAY_4 } from '../../../styles/colors';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
 
-const StyledAnchor = styled.a`
+const StyledButton = styled.button`
+    background: none;
+    border: none;
+    padding: 0;
+    outline: inherit;
+    cursor: pointer;
     letter-spacing: 0.3px;
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     color: ${COLOR_GRAY_4};
@@ -19,4 +24,4 @@ const StyledAnchor = styled.a`
     `};
 `;
 
-export default StyledAnchor;
+export default StyledButton;
