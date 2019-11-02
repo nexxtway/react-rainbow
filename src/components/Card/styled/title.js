e4926a1ce2c71386992f54567c363651e77fc51b
cmcartaya <cmcartaya1sm@gmail.com>
2019-11-02 15:27:53 -0400

    review: remove javascript:void(0) on Card component

--- src/components/Card/styled/title.js
+++ src/components/Card/styled/title.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { FONT_SIZE_HEADING_MEDIUM } from '../../../styles/fontSizes';
 import { COLOR_GRAY_4 } from '../../../styles/colors';
 
-const StyledTitle = styled.span`
+const StyledTitle = styled.h2`
     font-size: ${FONT_SIZE_HEADING_MEDIUM};
     font-weight: 500;
     color: ${COLOR_GRAY_4};
