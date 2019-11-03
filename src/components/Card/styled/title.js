6019edcecabaf933280fcbfc8fede38a0083d20a
cmcartaya <cmcartaya1sm@gmail.com>
2019-11-03 14:20:57 -0500

    fix: fix some comments

--- src/components/Card/styled/title.js
+++ src/components/Card/styled/title.js
@@ -6,6 +6,9 @@ const StyledTitle = styled.h2`
     font-size: ${FONT_SIZE_HEADING_MEDIUM};
     font-weight: 500;
     color: ${COLOR_GRAY_4};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 `;
 
 export default StyledTitle;
