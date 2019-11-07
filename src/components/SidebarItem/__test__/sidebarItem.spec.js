82893131f6b1f9dacaa160c94966b042bd047ec1
Carlos Miguel <33812799+cmcartaya@users.noreply.github.com>
2019-11-06 21:38:04 -0500

    review: remove javascript:void(0) on Sidebar component (#1126)
    * review: remove javascript:void(0) on Sidebar component
    
    * fix: fix some comments
    
    * fix: fix some comments

--- src/components/SidebarItem/__test__/sidebarItem.spec.js
+++ src/components/SidebarItem/__test__/sidebarItem.spec.js
@@ -5,11 +5,12 @@ import { faClock } from '@fortawesome/free-solid-svg-icons';
 import Sidebar from '../../Sidebar';
 import SidebarItem from '../index';
 
-describe('<SidebarItem />', () => {
+describe('<SidebarItem when href is passed />', () => {
     it('should have aria-current of page if isSelected', () => {
         const component = mount(
             <Sidebar selectedItem="sidebaritem-test-2">
                 <SidebarItem
+                    href="/#/Components"
                     icon={<FontAwesomeIcon icon={faClock} />}
                     name="sidebaritem-test-2"
                     label="sidebaritem-test-2"
@@ -23,6 +24,7 @@ describe('<SidebarItem />', () => {
         const component = mount(
             <Sidebar selectedItem="sidebaritem-test-3">
                 <SidebarItem
+                    href="/#/Components"
                     icon={<FontAwesomeIcon icon={faClock} />}
                     name="sidebaritem-test-3"
                     label="sidebaritem-test-3"
@@ -31,6 +33,37 @@ describe('<SidebarItem />', () => {
             </Sidebar>,
         );
         const sidebarLink = component.find('a');
+        sidebarLink.simulate('click');
+        expect(handleOnClickMockFn).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('<SidebarItem when href is not passed />', () => {
+    it('should have aria-current of page if isSelected', () => {
+        const component = mount(
+            <Sidebar selectedItem="sidebaritem-test-2">
+                <SidebarItem
+                    icon={<FontAwesomeIcon icon={faClock} />}
+                    name="sidebaritem-test-2"
+                    label="sidebaritem-test-2"
+                />
+            </Sidebar>,
+        );
+        expect(component.find('button').prop('aria-current')).toBe('page');
+    });
+    it('should fire an event when click in SidebarItem', () => {
+        const handleOnClickMockFn = jest.fn();
+        const component = mount(
+            <Sidebar selectedItem="sidebaritem-test-3">
+                <SidebarItem
+                    icon={<FontAwesomeIcon icon={faClock} />}
+                    name="sidebaritem-test-3"
+                    label="sidebaritem-test-3"
+                    onClick={handleOnClickMockFn}
+                />
+            </Sidebar>,
+        );
+        const sidebarLink = component.find('button');
         sidebarLink.simulate('click');
         expect(handleOnClickMockFn).toHaveBeenCalledTimes(1);
     });
