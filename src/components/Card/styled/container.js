50e963e6313bba8bdeb970ba00ee14748cc125a0
cmcartaya <cmcartaya1sm@gmail.com>
2019-11-11 18:08:37 -0500

    style: add style on Card component deleted in before commit by error

--- src/components/Card/styled/container.js
+++ src/components/Card/styled/container.js
@@ -18,6 +18,10 @@ const StyledContainer = styled.article`
         transition: all 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
     }
 
+    .rainbow-carousel-image {
+        box-shadow: none;
+    }
+
     & > .rainbow-google-map {
         .rainbow-google-map_map-container {
             border: none;
