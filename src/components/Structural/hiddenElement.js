5d7be01a963a24f3cb433aaa3f7006dcb589b39d
TahimiLeonBravo <tahimileon@gmail.com>
2019-11-17 15:49:48 -0600

    refactor: global styles in CheckboxToggle

--- src/components/Structural/hiddenElement.js
+++ src/components/Structural/hiddenElement.js
@@ -11,6 +11,11 @@ const HiddenElement = styled.span`
     clip: rect(0 0 0 0) !important;
     text-transform: none !important;
     white-space: nowrap !important;
+    ${props =>
+        props.as === 'input' &&
+        `
+            box-sizing: border-box;
+        `};
 `;
 
 export default HiddenElement;
