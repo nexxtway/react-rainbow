da64aea4700bb65c964db6f96aed50fccdcdf4eb
TahimiLeonBravo <tahimileon@gmail.com>
2019-11-17 21:15:48 -0600

    refactor: global styles in Modal

---
+++ src/components/Modal/styled/title.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { COLOR_GRAY_4 } from '../../../styles/colors';
+
+const StyledTitle = styled.h1`
+    font-family: 'Lato Light', Arial, sans-serif;
+    padding: 0;
+    text-align: center;
+    font-size: 1.5rem;
+    color: ${COLOR_GRAY_4};
+    margin: 0;
+    font-weight: inherit;
+`;
+
+export default StyledTitle;
