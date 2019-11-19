6272c720cd7e9241377a069e58289459b8a7f931
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-11-18 19:17:24 -0600

    fix: remove all scss and css files

--- library/styleguideComponents/Wrapper/index.js
+++ library/styleguideComponents/Wrapper/index.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 import { createStore, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { Provider } from 'react-redux';
-import Application from './../../../src/components/Application';
-import './styles.css';
 
 const rootReducer = combineReducers({
     form: formReducer,
@@ -14,20 +12,11 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function Wrapper({ children }) {
-    return (
-        <Provider store={store}>
-            <Application className="react-rainbow-styleguide-wrapper-container">
-                {children}
-            </Application>
-        </Provider>
-    );
+    return <Provider store={store}>{children}</Provider>;
 }
 
 Wrapper.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.object,
-    ]),
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.object]),
 };
 
 Wrapper.defaultProps = {
