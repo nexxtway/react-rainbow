4ddce3f9f981f36871825f72c1c52464dfb0608a
cmcartaya <cmcartaya1sm@gmail.com>
2019-11-25 12:14:06 -0500

    test: add test when readOnly is passed

--- integration/specs/DatePicker/datepicker-13.spec.js
+++ integration/specs/DatePicker/datepicker-13.spec.js
@@ -1,14 +1,32 @@
 const PageDatePicker = require('../../../src/components/DatePicker/pageObject');
+const { ENTER_KEY, SPACE_KEY } = require('../../constants');
 
 const DATEPICKER = '#datePicker-13';
 
 describe('DatePicker readOnly', () => {
-    it('should not open the DatePicker when readOnly is passed and click on the input element', () => {
-        const component = $(DATEPICKER);
-        const datePicker = new PageDatePicker(DATEPICKER);
+    beforeAll(() => {
         browser.url('/#!/DatePicker/13');
+    });
+    beforeEach(() => {
+        browser.refresh();
+        const component = $(DATEPICKER);
         component.waitForExist();
+    });
+    it('should not open the DatePicker when readOnly is passed and click on the input element', () => {
+        const datePicker = new PageDatePicker(DATEPICKER);
         datePicker.click();
+        expect(datePicker.isOpen()).toBe(false);
+    });
+    it('should not open the DatePicker when readOnly is passed and enter key is pressed while the input element is focused', () => {
+        const datePicker = new PageDatePicker(DATEPICKER);
+        datePicker.click();
+        browser.keys(ENTER_KEY);
+        expect(datePicker.isOpen()).toBe(false);
+    });
+    it('should not open the DatePicker when readOnly is passed and space key is pressed while the input element is focused', () => {
+        const datePicker = new PageDatePicker(DATEPICKER);
+        datePicker.click();
+        browser.keys(SPACE_KEY);
         expect(datePicker.isOpen()).toBe(false);
     });
 });
