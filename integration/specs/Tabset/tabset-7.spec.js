9daa873bf91bb49723163e4f0f90110f9bc3b0c7
Yuri Victor Munayev <yvmunayev@gmail.com>
2019-12-06 18:13:12 -0500

    add test-case for when unregister child in tabset

--- integration/specs/Tabset/tabset-7.spec.js
+++ integration/specs/Tabset/tabset-7.spec.js
@@ -6,10 +6,16 @@ const TABSET = '#tabset-7';
 const addRecents = () => $('#button-icon_add-recents').click();
 
 describe('Tabset with Tabs changed dynamically', () => {
-    it('should select the new option with keyboard after it is added dynamically', () => {
+    beforeAll(() => {
         browser.url('/#!/Tabset/7');
+    });
+    beforeEach(() => {
+        browser.refresh();
         const component = $(TABSET);
         component.waitForExist();
+    });
+
+    it('should select the new option with keyboard after it is added dynamically', () => {
         const tabset = new PageTabset(TABSET);
         const firstTab = tabset.getItem(0);
         firstTab.click();
@@ -23,5 +29,20 @@ describe('Tabset with Tabs changed dynamically', () => {
         browser.keys(ARROW_RIGHT_KEY);
         expect(secondTab.isSelected()).toBe(true);
         expect(secondTab.getLabelText()).toBe('RECENTS');
+    });
+    it('should select the second option with keyboard after it is added and remove dynamically a new element', () => {
+        const tabset = new PageTabset(TABSET);
+        addRecents();
+        const firstTab = tabset.getItem(0);
+        firstTab.click();
+        browser.keys(ARROW_RIGHT_KEY);
+        const secondTab = tabset.getItem(1);
+        expect(secondTab.isSelected()).toBe(true);
+        expect(secondTab.getLabelText()).toBe('RECENTS');
+        addRecents();
+        firstTab.click();
+        browser.keys(ARROW_RIGHT_KEY);
+        expect(secondTab.isSelected()).toBe(true);
+        expect(secondTab.getLabelText()).toBe('SHARED');
     });
 });
