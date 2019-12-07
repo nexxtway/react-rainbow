32281cc6f2b14d9691f24e3471b546a2ccdcf87d
yvmunayev <yvmunayev@gmail.com>
2019-12-06 22:09:56 -0500

    test: add test-case for when unregister child in Accordion (#1184)
    * add test-case for when unregister child in accordion

--- integration/specs/Accordion/accordion-9.spec.js
+++ integration/specs/Accordion/accordion-9.spec.js
@@ -6,10 +6,16 @@ const ACCORDION = '#accordion-9';
 const addAdvancedSettings = () => $('#button-icon_add-new-advanced-settings').click();
 
 describe('Accordion with AccordionOption changed dynamically', () => {
-    it('should select the new option with keyboard after it is added dynamically', () => {
+    beforeAll(() => {
         browser.url('/#!/Accordion/9');
+    });
+    beforeEach(() => {
+        browser.refresh();
         const component = $(ACCORDION);
         component.waitForExist();
+    });
+
+    it('should select the new option with keyboard after it is added dynamically', () => {
         const accordion = new PageAccordion(ACCORDION);
         const firstSection = accordion.getItem(0);
         firstSection.clickButtonIcon();
@@ -25,5 +31,22 @@ describe('Accordion with AccordionOption changed dynamically', () => {
         browser.keys(ENTER_KEY);
         expect(secondSection.isExpanded()).toBe(true);
         expect(secondSection.getLabel()).toBe('Advanced Settings');
+    });
+    it('should select the second option with keyboard after it is added and removed dynamically a new element', () => {
+        const accordion = new PageAccordion(ACCORDION);
+        addAdvancedSettings();
+        const firstSection = accordion.getItem(0);
+        firstSection.clickButtonIcon();
+        browser.keys(ARROW_DOWN_KEY);
+        browser.keys(ENTER_KEY);
+        const secondSection = accordion.getItem(1);
+        expect(secondSection.isExpanded()).toBe(true);
+        expect(secondSection.getLabel()).toBe('Advanced Settings');
+        addAdvancedSettings();
+        firstSection.clickButtonIcon();
+        browser.keys(ARROW_DOWN_KEY);
+        browser.keys(ENTER_KEY);
+        expect(secondSection.isExpanded()).toBe(true);
+        expect(secondSection.getLabel()).toBe('Personal Profile');
     });
 });
