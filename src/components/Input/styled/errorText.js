112566426abb83afa5e668a17550dc689f3f8fe8
Yuri Victor Munayev <yvmunayev@gmail.com>
2019-12-08 18:50:50 -0500

    add support theme for input component

--- src/components/Input/styled/errorText.js
+++ src/components/Input/styled/errorText.js
@@ -1,17 +1,20 @@
 import styled from 'styled-components';
 import { MARGIN_X_SMALL } from '../../../styles/margins';
-import { COLOR_ERROR } from '../../../styles/colors';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
+import getTheme from '../../../styles/helpers/getTheme';
 
 function getAlignSelf(props) {
     return props.alignSelf || 'center';
 }
 
-const ErrorText = styled.div`
+const ErrorText = styled.div.attrs(props => {
+    const errorMainColor = getTheme(props).palette.error.main;
+    return { errorMainColor: errorMainColor };
+})`
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     margin-top: ${MARGIN_X_SMALL};
     align-self: ${getAlignSelf};
-    color: ${COLOR_ERROR};
+    color: ${props => props.errorMainColor};
 `;
 
 export default ErrorText;
