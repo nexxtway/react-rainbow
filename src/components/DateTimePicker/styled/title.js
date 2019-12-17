0f9b98415ac8da696b51cacf9a7a978cddb679af
Yuri Victor Munayev <yvmunayev@gmail.com>
2019-12-17 18:14:02 -0500

    add theme support for datepicker

--- src/components/DateTimePicker/styled/title.js
+++ src/components/DateTimePicker/styled/title.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import getTheme from '../../../styles/helpers/getTheme';
 
 const StyledTitle = styled.h2.attrs(props => {
-    const brandDarkColor = getTheme(props).palette.brand.dark;
-    return { brandDarkColor };
+    const brandMainColor = getTheme(props).palette.brand.main;
+    return { brandMainColor };
 })`
-    color: ${props => props.brandDarkColor};
+    color: ${props => props.brandMainColor};
     font-size: 1.5rem;
     font-weight: 500;
     text-transform: capitalize;
