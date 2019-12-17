e3f4d760360f4f74c4b607a7eb17c129134ed52c
RafaelMG <rafielmg@gmail.com>
2019-12-17 00:11:53 -0500

    fix: dots style

--- src/components/TimePicker/styled/dots.js
+++ src/components/TimePicker/styled/dots.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 import { PADDING_XX_SMALL } from '../../../styles/paddings';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledDots = styled.span.attrs(props => props.theme.rainbow.palette)`
+const StyledDots = styled.span.attrs(props => {
+    const theme = getTheme(props);
+    const { brand } = theme.palette;
+    const { main: brandMainColor } = brand;
+
+    return {
+        brandMainColor,
+    };
+})`
     font-size: 32px;
     font-weight: 200;
     height: 100%;
-    color: ${props => props.brand.main};
+    color: ${props => props.brandMainColor};
     margin: auto ${PADDING_XX_SMALL};
     display: flex;
     align-items: center;
