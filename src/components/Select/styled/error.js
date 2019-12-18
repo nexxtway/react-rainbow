00bf1295c32df39fdc1f991b91f24f92b5e08380
Yuri Victor Munayev <yvmunayev@gmail.com>
2019-12-18 00:13:59 -0500

    add themes support for select component

--- src/components/Select/styled/error.js
+++ src/components/Select/styled/error.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
 import { MARGIN_X_SMALL } from '../../../styles/margins';
-import { COLOR_ERROR } from '../../../styles/colors';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledError = styled.div`
+const StyledError = styled.div.attrs(props => {
+    const errorMainColor = getTheme(props).palette.error.main;
+    return { errorMainColor };
+})`
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     margin-top: ${MARGIN_X_SMALL};
     align-self: center;
-    color: ${COLOR_ERROR};
+    color: ${props => props.error};
 `;
 
 export default StyledError;
