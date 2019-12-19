14f97f6b6900e671ff26173709d06ea56471a8e1
yvmunayev <yvmunayev@gmail.com>
2019-12-19 01:13:37 -0500

    feat: add support theme for Calendar, Select, DateTimePicker and DatePicker (#1217)

--- src/components/DatePicker/styled/headerTitle.js
+++ src/components/DatePicker/styled/headerTitle.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
-import { COLOR_BRAND } from '../../../styles/colors';
 import { FONT_SIZE_HEADING_LARGE } from '../../../styles/fontSizes';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledHeaderTitle = styled.h1`
-    color: ${COLOR_BRAND};
+const StyledHeaderTitle = styled.h1.attrs(props => {
+    const brandMainColor = getTheme(props).palette.brand.main;
+    return { brandMainColor };
+})`
+    color: ${props => props.brandMainColor};
     font-size: ${FONT_SIZE_HEADING_LARGE};
     font-weight: 500;
     text-transform: capitalize;
