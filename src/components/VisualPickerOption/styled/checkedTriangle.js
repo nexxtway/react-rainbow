8701a161f9d0dd7b10383344d04d4376d261c6fb
Rafael <39649678+rafaelmg83@users.noreply.github.com>
2019-12-19 01:31:00 -0500

    feat: add support theme to visualpicker and visualpickeroption components (#1218)

--- src/components/VisualPickerOption/styled/checkedTriangle.js
+++ src/components/VisualPickerOption/styled/checkedTriangle.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
-import { COLOR_BRAND } from '../../../styles/colors';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledCheckedTriangle = styled.span`
+const StyledCheckedTriangle = styled.span.attrs(props => {
+    const theme = getTheme(props);
+    const { brand } = theme.palette;
+    const { main: brandMainColor } = brand;
+
+    return {
+        brandMainColor,
+    };
+})`
     position: absolute;
     top: -1px;
     right: -1px;
     border: 1.5rem solid transparent;
     border-radius: 0 20px 0 0;
-    border-right-color: ${COLOR_BRAND};
-    border-top-color: ${COLOR_BRAND};
+    border-right-color: ${props => props.brandMainColor};
+    border-top-color: ${props => props.brandMainColor};
     margin: 0;
     padding: 0;
 `;
--- src/components/VisualPickerOption/styled/input.js
+++ src/components/VisualPickerOption/styled/input.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 import HiddenElement from '../../Structural/hiddenElement';
-import { COLOR_BRAND, COLOR_GRAY_2, COLOR_GRAY_1 } from '../../../styles/colors';
-import { SHADOW_OUTLINE } from '../../../styles/shadows';
+import { COLOR_GRAY_2, COLOR_GRAY_1 } from '../../../styles/colors';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledInput = styled(HiddenElement)`
+const StyledInput = styled(HiddenElement).attrs(props => {
+    const theme = getTheme(props);
+    const { brand } = theme.palette;
+    const { light: brandLightColor } = brand;
+    const { main: brandMainColor } = brand;
+
+    return {
+        brandMainColor,
+        brandLightColor,
+        // TODO: move up to defaultTheme or normalizeTheme
+        brandShadow: `0 0 2px ${brandMainColor}`,
+    };
+})`
     color: inherit;
     font: inherit;
     margin: 0;
@@ -11,17 +23,17 @@ const StyledInput = styled(HiddenElement)`
 
     :hover ~ label > [data-id='visual-picker_option'] {
         cursor: pointer;
-        border: solid 1.5px ${COLOR_BRAND};
+        border: solid 1.5px ${props => props.brandMainColor};
         box-shadow: 0 2px 4px 0 rgba(136, 152, 170, 0.5);
     }
 
     :focus ~ label > [data-id='visual-picker_option'] {
-        border: solid 0.5px #67d3f9;
-        box-shadow: 0 1px 2px 0 rgba(136, 152, 170, 0.5), ${SHADOW_OUTLINE};
+        border: solid 1px ${props => props.brandLightColor};
+        box-shadow: 0 1px 2px 0 rgba(136, 152, 170, 0.5), ${props => props.brandShadow};
     }
 
     :checked ~ label > [data-id='visual-picker_option'] {
-        border: solid 2px ${COLOR_BRAND};
+        border: solid 2px ${props => props.brandMainColor};
         position: relative;
     }
 
