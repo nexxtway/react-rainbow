0fe4079be6d75a9d49f3bea62b421b4fc3f35458
RafaelMG <rafielmg@gmail.com>
2019-12-31 13:01:02 -0500

    fix: moved fonts to assets

--- styleguide.config.js
+++ styleguide.config.js
@@ -162,7 +162,7 @@ module.exports = {
                     test: /\.(ttf|eot|woff|woff2)$/,
                     loader: 'file-loader',
                     options: {
-                        name: 'fonts/[name].[ext]',
+                        name: 'fonts/[name]/[name].[ext]',
                     },
                 },
                 {
