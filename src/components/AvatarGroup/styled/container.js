1e4897b55990a7449752190894bb4fb750a119f2
RafaelMG <rafielmg@gmail.com>
2020-01-05 02:28:13 -0500

    fix: lint error

--- src/components/AvatarGroup/styled/container.js
+++ src/components/AvatarGroup/styled/container.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
@@ -8,22 +9,22 @@ const StyledContainer = styled.div`
             props.size === 'large' &&
             `
             margin-left: -1.6rem;
-        `};
+            `}
         ${props =>
             props.size === 'medium' &&
             `
             margin-left: -1.25rem;
-        `};
+            `}
         ${props =>
             props.size === 'small' &&
             `
             margin-left: -0.75rem;
-        `};
+            `}
         ${props =>
             props.size === 'x-small' &&
             `
             margin-left: -0.625rem;
-        `};
+            `}
     }
 `;
 
