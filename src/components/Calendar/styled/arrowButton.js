b800e9bae7373635f1cd0ef0b4c97f0d4afc16c0
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-01-09 17:02:38 -0500

    add themes support to calendar

--- src/components/Calendar/styled/arrowButton.js
+++ src/components/Calendar/styled/arrowButton.js
@@ -4,14 +4,19 @@ import { COLOR_GRAY_2 } from '../../../styles/colors';
 import getTheme from '../../../styles/helpers/getTheme';
 
 const StyledArrowButton = styled(ButtonIcon).attrs(props => {
-    const brandMainColor = getTheme(props).palette.brand.main;
-    return { brandMainColor };
+    const palette = getTheme(props).palette;
+    const brandMainColor = palette.brand.main;
+    const textDisabled = palette.text.disabled;
+    return {
+        brandMainColor,
+        textDisabled,
+    };
 })`
     color: ${props => props.brandMainColor};
     ${props =>
         props.disabled &&
         `
-            color: ${COLOR_GRAY_2};
+            color: ${props.textDisabled};
         `};
 `;
 
--- src/components/Calendar/styled/day.js
+++ src/components/Calendar/styled/day.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 import { COLOR_DARK_1 } from '../../../styles/colors';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
+import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledDay = styled.td`
+const StyledDay = styled.td.attrs(props => {
+    const textPrimary = getTheme(props).palette.text.primary;
+    return { textPrimary };
+})`
     text-align: center;
-    color: ${COLOR_DARK_1};
+    color: ${props => props.textPrimary};
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     font-weight: 400;
     border-radius: 100%;
