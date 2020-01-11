62a0df14b1c0e512f5cc9d0123a2f3fdbb3ab7fa
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-01-11 01:54:07 -0500

    add background.disabled to normalizeTheme

--- src/styles/helpers/normalizeTheme.js
+++ src/styles/helpers/normalizeTheme.js
@@ -40,12 +40,13 @@ function normalizeBackground(background) {
             background: {
                 main: background,
                 secondary: dark ? lighten(background, 0.2) : darken(background, 0.2),
+                disabled: dark ? lighten(background, 0.1) : darken(background, 0.1),
             },
-            divider: dark ? lighten(background, 0.12) : darken(background, 0.12),
+            divider: dark ? lighten(background, 0.3) : darken(background, 0.12),
             text: {
                 main: mainText,
                 secondary: dark ? darken(mainText, 0.3) : lighten(mainText, 0.3),
-                disabled: dark ? darken(mainText, 0.5) : lighten(mainText, 0.8),
+                disabled: dark ? darken(mainText, 0.5) : lighten(mainText, 0.5),
             },
             action: {
                 hover: dark ? lighten(background, 0.1) : darken(background, 0.1),
