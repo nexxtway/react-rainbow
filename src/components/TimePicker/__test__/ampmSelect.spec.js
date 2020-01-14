416e686da9570852b4b6e17f555952c2ba922e03
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-01-13 19:13:17 -0500

    test: fix tests for ampmselect

--- src/components/TimePicker/__test__/ampmSelect.spec.js
+++ src/components/TimePicker/__test__/ampmSelect.spec.js
@@ -3,13 +3,12 @@ import { mount } from 'enzyme';
 import AmPmSelect from './../ampmSelect';
 
 describe('<AmPmSelect/>', () => {
-    it('should render the right markup initially when component is not focused', () => {
+    it('should render the right markup when isFocused prop is false', () => {
         const component = mount(<AmPmSelect tabIndex="-1" />);
         expect(component.find('input[aria-label="am-pm selector"]').exists()).toBe(true);
     });
-    it('should render the right markup when component is focused', () => {
-        const component = mount(<AmPmSelect />);
-        component.find('input[aria-label="am-pm selector"]').simulate('focus');
+    it('should render the right markup when isFocused prop is true', () => {
+        const component = mount(<AmPmSelect isFocused />);
         expect(component.find('fieldset[role="presentation"]').exists()).toBe(true);
         expect(component.find('input[value="AM"]').exists()).toBe(true);
         expect(component.find('input[value="PM"]').exists()).toBe(true);
@@ -26,19 +25,10 @@ describe('<AmPmSelect/>', () => {
         component.find('input[aria-label="am-pm selector"]').simulate('focus');
         expect(component.prop('onChange')).toHaveBeenCalledWith('PM');
     });
-    it.skip('should render the initial input when component lost focus', () => {
-        const component = mount(<AmPmSelect />);
-        component.find('input[aria-label="am-pm selector"]').simulate('focus');
-        component.find('fieldset[role="presentation"]').simulate('blur');
+    it('should render the initial input when component lost focus', () => {
+        const component = mount(<AmPmSelect isFocused />);
+        component.setProps({ isFocused: false });
         expect(component.find('input[aria-label="am-pm selector"]').exists()).toBe(true);
-    });
-    it('should focus the component when the focus method is called and the component is focused', () => {
-        const component = mount(<AmPmSelect tabIndex="-1" />);
-        component.find('input[aria-label="am-pm selector"]').simulate('focus');
-        component.instance().fieldsetRef.current.focus();
-        const focusedElementDataId = document.activeElement.getAttribute('data-id');
-        const fieldsetDataId = component.find('fieldset').prop('data-id');
-        expect(focusedElementDataId).toBe(fieldsetDataId);
     });
     it('should focus the component when the focus method is called and the component is not focused', () => {
         const component = mount(<AmPmSelect tabIndex="-1" />);
@@ -49,8 +39,7 @@ describe('<AmPmSelect/>', () => {
     });
     it('should call event.stopPropagation when component is focused and the input with value "AM" is blurred', () => {
         const stopPropagationMockFn = jest.fn();
-        const component = mount(<AmPmSelect />);
-        component.find('input[aria-label="am-pm selector"]').simulate('focus');
+        const component = mount(<AmPmSelect isFocused />);
         component.find('input[value="AM"]').simulate('blur', {
             stopPropagation: stopPropagationMockFn,
         });
@@ -58,8 +47,7 @@ describe('<AmPmSelect/>', () => {
     });
     it('should call event.stopPropagation when component is focused and the input with value "PM" is blurred', () => {
         const stopPropagationMockFn = jest.fn();
-        const component = mount(<AmPmSelect />);
-        component.find('input[aria-label="am-pm selector"]').simulate('focus');
+        const component = mount(<AmPmSelect isFocused />);
         component.find('input[value="PM"]').simulate('blur', {
             stopPropagation: stopPropagationMockFn,
         });
