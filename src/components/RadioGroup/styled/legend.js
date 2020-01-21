aafd8201f6070dafb8e87e3a64c4113ca2300d99
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-01-21 08:31:31 -0500

    feat: add theme support to ReadioGroup component

--- src/components/RadioGroup/styled/legend.js
+++ src/components/RadioGroup/styled/legend.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
-import { COLOR_GRAY_4 } from '../../../styles/colors';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttr';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
 import { MARGIN_SMALL, MARGIN_XX_SMALL } from '../../../styles/margins';
 
-const StyledLegend = styled.legend`
+const StyledLegend = attachThemeAttrs(styled.legend)`
     border: 0;
     padding: 0;
     box-sizing: border-box;
     display: inline-block;
-    color: ${COLOR_GRAY_4};
+    color: ${props => props.palette.text.label};
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     margin-right: ${MARGIN_SMALL};
     margin-bottom: ${MARGIN_XX_SMALL};
