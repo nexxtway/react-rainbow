ec29e1c3786bf45daa75c3c63412cfa473e1625d
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-01-20 22:37:40 -0500

    feat: add theme support to SidebarItem component

--- src/components/SidebarItem/styled/label.js
+++ src/components/SidebarItem/styled/label.js
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
-import { COLOR_GRAY_3 } from '../../../styles/colors';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttr';
 import { FONT_SIZE_TEXT_X_SMALL } from '../../../styles/fontSizes';
 
-const StyledLabel = styled.span`
+const StyledLabel = attachThemeAttrs(styled.span)`
     font-family: 'Lato Regular', Arial, sans-serif;
     font-size: ${FONT_SIZE_TEXT_X_SMALL};
     letter-spacing: -0.2px;
     text-align: center;
-    color: ${COLOR_GRAY_3};
+    color: ${props => props.palette.text.label};
+
+    ${props =>
+        props.isSelected &&
+        `
+        color: ${props.palette.text.main};
+    `};
 `;
 
 export default StyledLabel;
