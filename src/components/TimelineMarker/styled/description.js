5f89dabaaee65f7c930e4c50cfdd9941ac2ed43b
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-01-30 22:21:05 -0500

    feat: add theme support to TimelineMarker component

--- src/components/TimelineMarker/styled/description.js
+++ src/components/TimelineMarker/styled/description.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttr';
 import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
-import { COLOR_GRAY_4 } from '../../../styles/colors';
 import { PADDING_X_SMALL } from '../../../styles/paddings';
 
-const StyledDescription = styled.p`
+const StyledDescription = attachThemeAttrs(styled.p)`
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
-    color: ${COLOR_GRAY_4};
+    color: ${props => props.palette.text.label};
     padding: ${PADDING_X_SMALL} 0;
     margin: 0;
     box-sizing: border-box;
