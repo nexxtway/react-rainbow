9b0abafd10bfdb4b81bb57c8413a14e973513bbc
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-02-07 08:52:32 -0500

    style: fix lookup style when it has value

--- src/components/Lookup/styled/selectedInput.js
+++ src/components/Lookup/styled/selectedInput.js
@@ -4,16 +4,19 @@ import { FONT_SIZE_TEXT_MEDIUM } from '../../../styles/fontSizes';
 import Input from '../../Input/styled/input';
 
 const StyledSelectedInput = attachThemeAttrs(styled(Input))`
-    box-shadow: inset 0 0 0 3px #fff, inset 0 0 0 4px #dddbda;
-    caret-color: transparent;
+caret-color: transparent;
     color: ${props => props.palette.text.label};
     font-size: ${FONT_SIZE_TEXT_MEDIUM};
     padding-right: 2.35rem;
     text-overflow: ellipsis;
+    box-shadow: inset 0 0 0 3px ${props => props.palette.background.main},
+                inset 0 0 0 4px ${props => props.palette.border.main};
 
     :focus,
     :active {
-        box-shadow: inset 0 0 0 2px #fff, inset 0 0 0 3px #dddbda, ${props => props.shadows.brand};
+        box-shadow: inset 0 0 0 2px ${props => props.palette.background.main},
+                    inset 0 0 0 3px ${props => props.palette.border.main},
+                    ${props => props.shadows.brand};
         padding-right: 2.35rem;
         text-overflow: ellipsis;
     }
