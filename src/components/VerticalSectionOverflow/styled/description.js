4e0fd1eab8deccced0c5a640a5c539ccb5e9bca7
Wilder González Díaz <wildergd@gmail.com>
2020-02-08 11:47:39 -0500

    feat: add theme support for VerticalItem, VerticalNavigation, VerticalSection, VerticalSectionOverflow (#1300)
    * feat: add theme support to VerticalItem component
    
    * feat: add theme support to VerticalNavigation component
    
    * feat: add theme support to VerticalSection component
    
    * feat: add theme support to VerticalSectionOverflow component
    
    * styles: fix VerticalNavigation
    
    Co-authored-by: Tahimi <tahimi@nexxtway.com>
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- src/components/VerticalSectionOverflow/styled/description.js
+++ src/components/VerticalSectionOverflow/styled/description.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttr';
 import TruncatedText from '../../Structural/truncatedText';
 import { FONT_SIZE_TEXT_X_SMALL } from '../../../styles/fontSizes';
-import { COLOR_GRAY_3 } from '../../../styles/colors';
 
-const StyledDescription = styled(TruncatedText)`
+const StyledDescription = attachThemeAttrs(styled(TruncatedText))`
     font-size: ${FONT_SIZE_TEXT_X_SMALL};
     line-height: 1rem;
-    color: ${COLOR_GRAY_3};
+    color: ${props => props.palette.text.label};
     text-align: left;
     max-width: 100%;
     transition: all 0.15s cubic-bezier(0.23, 1, 0.32, 1) 0ms;
