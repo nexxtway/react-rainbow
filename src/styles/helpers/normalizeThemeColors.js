d37cf9ad6831b0e8bc078896952afb6f46cc96ae
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-02-16 10:28:07 -0500

    fix: fix some issues

--- src/styles/helpers/normalizeThemeColors.js
+++ src/styles/helpers/normalizeThemeColors.js
@@ -1,21 +1,21 @@
 import { colorToRgba } from './color';
 
-export default function normalizeThemeColors(theme) {
-    const normalizePaletteColors = palette => {
-        const normalizedPalette = {};
-        Object.keys(palette).forEach(key => {
-            const value = palette[key];
-            if (typeof value === 'string') {
-                normalizedPalette[key] = colorToRgba(value);
-            } else if (typeof value === 'object') {
-                normalizedPalette[key] = normalizePaletteColors(palette[key]);
-            } else {
-                normalizedPalette[key] = palette[key];
-            }
-        });
-        return normalizedPalette;
-    };
+function normalizePaletteColors(palette) {
+    const normalizedPalette = {};
+    Object.keys(palette).forEach(key => {
+        const value = palette[key];
+        if (typeof value === 'string') {
+            normalizedPalette[key] = colorToRgba(value);
+        } else if (typeof value === 'object') {
+            normalizedPalette[key] = normalizePaletteColors(palette[key]);
+        } else {
+            normalizedPalette[key] = palette[key];
+        }
+    });
+    return normalizedPalette;
+}
 
+export default function normalizeThemeColors(theme) {
     return {
         ...theme,
         palette: {
