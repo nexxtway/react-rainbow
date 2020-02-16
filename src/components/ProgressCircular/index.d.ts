cc20b09212d3f21c4467cda8614d64290d530147
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-02-16 15:04:14 -0500

    remove color to ProgressCircularProps

--- src/components/ProgressCircular/index.d.ts
+++ src/components/ProgressCircular/index.d.ts
@@ -3,7 +3,6 @@ import { BaseProps } from '../types';
 export interface ProgressCircularProps extends BaseProps {
     value?: number;
     variant?: 'brand' | 'success' | 'warning' | 'error';
-    color?: string;
     assistiveText?: string;
 }
 
