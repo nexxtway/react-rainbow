7c33763e4a06e229febc88a75a79d119a88700f6
Wilder González Díaz <wildergd@gmail.com>
2020-02-22 20:02:54 -0500

    feat: add theme support to ProgressStep, RadioButtonGroup, RadioGroup and Rating components (#1290)
    * feat: add theme support to PicklistOption component
    
    * feat: add theme support to Picklist component
    
    * feat: add theme support to ProgressCircular component
    
    * feat: add theme support to ProgressIndicator component
    
    * feat: add theme support to ProgressStep component
    
    * feat: add theme support to ReadioGroup component
    
    * feat: add theme support to RadioButtonGroup component
    
    * feat: add theme support to Rating component
    
    * fix: fix ProgressStep example
    
    * fix: fix RadioButtonGroup
    
    * styles: fix stepButton color
    
    * fix: fix RadioButtonGroup
    
    * fix: errortext and label styles
    
    * fix: fix RadioButtonGroup component
    
    * fix: fix RadioButtonGroup marker shadow
    
    * fix: fix styles with alpha channels
    
    * styles: fix RadioButtonGroup
    
    * fix: import
    
    Co-authored-by: Tahimi <tahimi@nexxtway.com>
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- src/components/RadioButtonGroup/styled/marker.js
+++ src/components/RadioButtonGroup/styled/marker.js
@@ -1,47 +1,36 @@
 /* stylelint-disable max-line-length */
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
 import { BORDER_RADIUS_2 } from '../../../styles/borderRadius';
-import { COLOR_WHITE } from '../../../styles/colors';
-import getTheme from '../../../styles/helpers/getTheme';
 
-const StyledMarker = styled.span.attrs(props => {
-    const theme = getTheme(props);
-    const { brand } = theme.palette;
-    const { main: brandMainColor, dark: brandDarkColor } = brand;
-
-    return {
-        brandMainColor,
-        brandDarkColor,
-    };
-})`
+const StyledMarker = attachThemeAttrs(styled.span)`
     position: absolute;
-    background: ${COLOR_WHITE};
+    background: ${props => props.palette.background.main};
     opacity: 0;
     top: 0;
     bottom: 0;
     margin: auto;
     height: 2.6rem;
     margin-top: -0.05rem;
-    border-color: white;
+    border-color: ${props => props.palette.border.main};
     border-radius: ${BORDER_RADIUS_2};
     border: solid 1px transparent;
-    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: ${props => props.shadows.shadow_1};
     transition: transform 600ms cubic-bezier(0.02, 0.94, 0.09, 0.97),
         all 300ms cubic-bezier(0.17, 0.67, 0.14, 1.03);
     transform: translate3d(0, 0, 0);
     ${props =>
         props.variant === 'inverse' &&
         `
-            box-shadow: 0 0 4px 0 ${props.brandMainColor};
-            background-color: ${props.brandMainColor};
-            border: solid 1px ${props.brandMainColor};
+        background-color: ${props.palette.brand.main};
+        border: solid 1px ${props.palette.brand.main};
+        box-shadow: 0 0 4px 0 ${props.palette.brand.main};
         `};
     ${props =>
         props.variant === 'brand' &&
         `
-            background-color: ${props.brandMainColor};
-            border-color: ${props.brandDarkColor};
-            box-shadow: 0 0 3px 0 rgba(1, 38, 96, 0.4);
+            background-color: ${props.palette.brand.main};
+            border-color: ${props.palette.brand.dark};
         `};
 `;
 
