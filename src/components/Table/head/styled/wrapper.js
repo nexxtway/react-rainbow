ad4b4c60a228e207dd0436348cc7570a7e1821a0
Wilder González Díaz <wildergd@gmail.com>
2020-02-23 13:20:29 -0500

    feat: add theme support for Spinner, Table and TableWithBrowserPagination components (#1291)
    * feat: add theme support to Spinner component
    
    * feat: add theme support to Table component
    
    * feat: add theme support to TableWithBrowserPagination component
    
    * styles: fix spinner neutral
    
    * styles: header title
    
    * fix: fix Table styles
    
    * fix: fix styles
    
    * fix: table styles
    
    * styles: TableWithBrowserPagination
    
    Co-authored-by: Tahimi <tahimi@nexxtway.com>
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- src/components/Table/head/styled/wrapper.js
+++ src/components/Table/head/styled/wrapper.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import { COLOR_GRAY_1 } from '../../../../styles/colors';
+import attachThemeAttrs from '../../../../styles/helpers/attachThemeAttrs';
 
-const StyledWrapper = styled.div`
+const StyledWrapper = attachThemeAttrs(styled.div)`
     outline: none;
     position: absolute;
     top: 0;
     height: 44px;
-    background-color: ${COLOR_GRAY_1};
+    background-color: ${props => props.palette.background.highlight};
 `;
 
 export default StyledWrapper;
