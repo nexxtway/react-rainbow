d523f7031cf7ea0240988082ba34dd2e482d2d47
Tahimi <tahimileon@gmail.com>
2020-02-24 14:54:35 -0600

    docs: add rainbow algolia serach example (#1332)
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

---
+++ library/pages/Designs/FindBook/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import DesignDetails from './../../components/DesignDetails';
+
+export default function FindBook() {
+    return (
+        <DesignDetails
+            category="websites"
+            title="Rainbow Algolia Search"
+            viewsAmount="20"
+            downloadsAmount="333"
+            liveUrl="https://rainbow-algolia-search.web.app"
+            imageSrc="/images/designsImages/find-book.png"
+            previewUrl="/images/designsImages/find-book.png"
+            description="The Rainbow Algolia Search is one of the handiest elements that can be used inside a project. We highly recommend it not just as a design feature."
+        />
+    );
+}
