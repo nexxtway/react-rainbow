2540c3417949e697d11b3a797fb690799e32b2d5
José Faro <blakmetall@gmail.com>
2020-02-26 11:59:26 -0600

    fix: from em to rem

--- src/components/Rating/styled/fieldset.js
+++ src/components/Rating/styled/fieldset.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledFieldset = styled.fieldset`
-    line-height: 1.5em;
+    line-height: 1.5rem;
     height: 1.5rem;
     margin: 0;
     padding: 0;
