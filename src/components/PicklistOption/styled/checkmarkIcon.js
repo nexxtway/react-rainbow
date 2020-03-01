e475b5416b9eb68919a9586a0d138af24e9e4998
HellWolf93 <drodriguezfeito@gmail.com>
2020-03-01 00:34:34 -0500

    fix: make checkmark smaller

--- src/components/PicklistOption/styled/checkmarkIcon.js
+++ src/components/PicklistOption/styled/checkmarkIcon.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
 
 const StyledCheckmarkIcon = attachThemeAttrs(styled.svg)`
-    width: 1rem;
-    height: 1rem;
+    width: 0.8rem;
+    height: 0.8rem;
     line-height: 1;
     margin-left: 0.75rem;
     flex-shrink: 0;
