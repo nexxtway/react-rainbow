d5a831a5090bf4697db1b4932fe046210fa08f1e
JoseCarloBastos <josecarlobastos@gmail.com>
2020-03-10 13:08:05 -0400

    fix: add theme support for ImportRecorFlow

--- src/components/ImportRecordsFlow/stepTwo/styled/fileCard.js
+++ src/components/ImportRecordsFlow/stepTwo/styled/fileCard.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../../styles/helpers/attachThemeAttrs';
 
-const StyledFileCard = styled.div`
+const StyledFileCard = attachThemeAttrs(styled.div)`
     border: solid 1px #f4f6f9;
-    background-color: #fafbfc;
+    background-color: ${props => props.palette.background.main};
     padding: 20px 8px 20px 16px;
     display: flex;
     margin: 0 16px 32px 16px;
