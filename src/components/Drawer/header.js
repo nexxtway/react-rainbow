4cfe7fccf4d410ea58c2e1013947e47400d2dcca
Wilder González Díaz <wildergd@gmail.com>
2020-03-11 15:07:05 -0400

    feat: implement Drawer component (#1357)
    * feat: implement Drawer component
    
    * feat: add useUniqueIdentifier hook
    
    * fix: fix some issues
    
    * fix: fix css animations
    
    * fix: fix slide-in and slide-out animations
    
    * fix: minor issue
    
    * fix: fix more issues
    
    * fix: fix wrong modified file
    
    * fix: fix drawer implementation
    
    * test: commit for debug purposes
    
    * docs: fix examples
    
    * fix: fix drawer in example 2
    
    * docs: fix examples
    
    * fix: move counterManager, manageTab and scrollController to libs
    
    * fix: fix minor issues
    
    * fix: fix minor issues
    
    * test: move tests from src to lib
    
    * fix: width
    
    * fix: some details
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>
    Co-authored-by: TahimiLeonBravo <tahimi@nexxtway.com>
    Co-authored-by: Tahimi <tahimileon@gmail.com>

---
+++ src/components/Drawer/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StyledTitle from './styled/title';
+import StyledDivider from './styled/divider';
+
+export default function Header({ content, id }) {
+    if (typeof content === 'string') {
+        return (
+            <>
+                <StyledTitle id={id}>{content}</StyledTitle>
+                <StyledDivider />
+            </>
+        );
+    }
+
+    return (
+        <>
+            {content}
+            <StyledDivider />
+        </>
+    );
+}
+
+Header.propTypes = {
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+Header.defaultProps = {
+    content: undefined,
+};
