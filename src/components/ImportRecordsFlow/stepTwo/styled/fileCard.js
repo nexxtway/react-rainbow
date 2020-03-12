d3c2310b8c45fecd365134622ba1599d8ddf8dcb
TahimiLeonBravo <tahimi@nexxtway.com>
2020-03-12 11:15:35 -0600

    fix: styles on step two

--- src/components/ImportRecordsFlow/stepTwo/styled/fileCard.js
+++ src/components/ImportRecordsFlow/stepTwo/styled/fileCard.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import attachThemeAttrs from '../../../../styles/helpers/attachThemeAttrs';
 
 const StyledFileCard = attachThemeAttrs(styled.div)`
-    border: solid 1px ${props => props.palette.border.divider};
-    background-color: ${props => props.palette.background.main};
+    border: solid 1px ${props => props.palette.background.highlight};
+    background-color: ${props => props.palette.background.secondary};
     padding: 20px 8px 20px 16px;
     display: flex;
     margin: 0 16px 32px 16px;
