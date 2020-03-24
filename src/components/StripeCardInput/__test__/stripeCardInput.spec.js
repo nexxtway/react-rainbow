cf652b82c48595333594bcac85c203e23089838a
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-03-24 17:53:13 -0400

    test: StripeCardInput spec

---
+++ src/components/StripeCardInput/__test__/stripeCardInput.spec.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import StripeCardInput from '..';
+
+jest.mock('@stripe/stripe-js', () => {
+    return {
+        loadStripe: jest.fn(() => Promise.resolve()),
+    };
+});
+
+jest.mock('@stripe/react-stripe-js', () => {
+    return {
+        // eslint-disable-next-line react/prop-types
+        Elements: props => <div>{props.children}</div>,
+        // eslint-disable-next-line react/prop-types
+        CardElement: ({ onChange }) => <input onChange={onChange} />,
+        useElements: jest.fn().mockReturnValue({
+            getElement: jest.fn().mockReturnValue({}),
+        }),
+        useStripe: jest.fn().mockReturnValue({}),
+    };
+});
+
+const error = {
+    code: 'invalid_number',
+    type: 'validation_error',
+    message: 'Your card number is invalid.',
+};
+const event = {
+    error,
+    empty: false,
+    complete: false,
+    brand: 'unknown',
+};
+
+describe('<StripeCardInput>', () => {
+    it('should fire onChange with especific event', () => {
+        const onChangeMockFn = jest.fn();
+        const component = mount(
+            <StripeCardInput apiKey="STRIPE_API_KEY" onChange={onChangeMockFn} />,
+        );
+        component.find('input').simulate('change', event);
+        expect(onChangeMockFn).toHaveBeenCalledWith({
+            brand: 'unknown',
+            iEmpty: false,
+            isComplete: false,
+            error,
+            stripe: {},
+            element: {},
+        });
+    });
+});
