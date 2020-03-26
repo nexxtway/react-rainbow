f5af30c01232f84c5e6e1ef2f39a63721d38f2d3
TahimiLeonBravo <tahimi@nexxtway.com>
2020-03-25 23:49:40 -0600

    fix: error

--- src/components/StripeCardInput/helpers/getErrror.js
+++ src/components/StripeCardInput/helpers/getErrror.js
@@ -1,11 +1,10 @@
 export default function getError(error) {
     if (error) {
         return {
-              code: error.code,
-              type: error.type,
-              message: error.message,
-          };
+            code: error.code,
+            type: error.type,
+            message: error.message,
+        };
     }
     return undefined;
-      
 }
