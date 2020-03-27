0125e36947156a1161b45e6cd68620267dda3b4c
Jose Faro <blakmetall@gmail.com>
2020-03-27 16:43:31 -0600

    fix: footer element was not being found

--- src/components/ImportRecordsFlow/__test__/importRecordsFlow.spec.js
+++ src/components/ImportRecordsFlow/__test__/importRecordsFlow.spec.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import ImportRecordsFlow from '../';
+import Footer from '../footer';
 
 describe('<ImportRecordsFlow />', () => {
     it('should set isBackButtonDisabled to true initially in Footer component', () => {
         const component = mount(<ImportRecordsFlow isOpen />);
-        expect(component.find('Footer').prop('isBackButtonDisabled')).toBe(true);
+        expect(component.find(Footer).prop('isBackButtonDisabled')).toBe(true);
     });
     it('should set isNextButtonDisabled to true initially in Footer component', () => {
         const component = mount(<ImportRecordsFlow isOpen />);
-        expect(component.find('Footer').prop('isNextButtonDisabled')).toBe(true);
+        expect(component.find(Footer).prop('isNextButtonDisabled')).toBe(true);
     });
     it('should set the right modal title initially', () => {
         const component = mount(<ImportRecordsFlow isOpen />);
