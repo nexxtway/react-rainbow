0a26e7c9eaed981848ccfccbdd430a76fb0d02e6
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-03-29 18:51:24 -0400

    fix: fix index.ts

--- src/components/MonthCalendar/index.d.ts
+++ src/components/MonthCalendar/index.d.ts
@@ -1,8 +1,15 @@
-import { ReactNode, ComponentType } from 'react';
+import { ReactNode } from 'react';
 import { BaseProps } from '../types';
 
 export interface MonthCalendarProps extends BaseProps {
+    selectedDate?: string | Date;
     id?: string;
+    maxDate?: Date;
+    minDate?: Date;
+    locale?: string;
+    dateComponent: (date: Date) => ReactNode;
+    onSelectDate?: (date: Date) => void;
+    onMonthChanged: (date: Date) => void;
 }
 
 export default function(props: MonthCalendarProps): JSX.Element | null;
