3cb6e7e746115d9051ff661c074e1c8e9799ebb2
TahimiLeonBravo <tahimi@nexxtway.com>
2020-04-04 23:06:27 -0600

    styles: some details

--- src/components/MonthlyCalendar/styled/month.js
+++ src/components/MonthlyCalendar/styled/month.js
@@ -3,12 +3,13 @@ import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
 
 const StyledMonth = attachThemeAttrs(styled.h3)`
     color: ${props => props.palette.brand.main};
-    font-size: 2.5rem;
+    font-size: 2rem;
     line-height: 2.5rem;
     text-align: center;
     margin: 0 1.5rem;
-    min-width: 250px;
+    min-width: 220px;
     text-transform: capitalize;
+    font-weight: 300;
 `;
 
 export default StyledMonth;
