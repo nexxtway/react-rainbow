b449d21b731e97e04818f2f704e843f972699882
RafaelMG <rafielmg@gmail.com>
2020-04-07 10:05:22 -0400

    fix: stylelint error

--- src/components/TimeRangePicker/styled/line.js
+++ src/components/TimeRangePicker/styled/line.js
@@ -4,7 +4,7 @@ const StyledLine = styled.div`
     height: 1px;
     width: 15px;
     border: 1.5px solid rgb(164, 167, 179);
-    margin: 0px 5px;
+    margin: 0 5px;
     border-radius: 5px;
 `;
 
