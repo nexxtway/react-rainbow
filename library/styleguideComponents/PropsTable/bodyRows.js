5b594b4be87763dffdf46ea0a8616134cf810f91
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-04-08 20:09:53 -0400

    fix: fix shape proptypes rendering im library docs

--- library/styleguideComponents/PropsTable/bodyRows.js
+++ library/styleguideComponents/PropsTable/bodyRows.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import propTypeShapeRender from './renderers/propTypeShape';
 
 function getTitleValue(value) {
     if (!value) {
@@ -11,32 +12,42 @@ function getTitleValue(value) {
     return value;
 }
 
-function renderValue(value) {
+function renderValue(value, caption, row) {
+    const { description, type } = row;
+
     if (value === 'undefined' || value === 'null') {
         return '';
     }
+
+    if (caption === 'Description' && type && type.name === 'shape') {
+        return (
+            <div>
+                {description}
+                {propTypeShapeRender(type.value)}
+            </div>
+        );
+    }
+
     return value;
 }
 
 export default function BodyRows({ rows, columns, getRowKey }) {
-    return (
-        rows.map(row => (
-            <tr key={getRowKey(row)}>
-                {columns.map(({ render, caption }, index) => {
-                    const key = `td-${index}`;
-                    const value = render(row) ? render(row).props.children : null;
-                    const titleValue = getTitleValue(value);
-                    return (
-                        <td key={key} data-label={caption}>
-                            <div className="react-rainbow-truncate" title={titleValue}>
-                                {renderValue(value)}
-                            </div>
-                        </td>
-                    );
-                })}
-            </tr>
-        ))
-    );
+    return rows.map(row => (
+        <tr key={getRowKey(row)}>
+            {columns.map(({ render, caption }, index) => {
+                const key = `td-${index}`;
+                const value = render(row) ? render(row).props.children : null;
+                const titleValue = getTitleValue(value);
+                return (
+                    <td key={key} data-label={caption}>
+                        <div className="react-rainbow-truncate" title={titleValue}>
+                            {renderValue(value, caption, row)}
+                        </div>
+                    </td>
+                );
+            })}
+        </tr>
+    ));
 }
 
 BodyRows.propTypes = {
