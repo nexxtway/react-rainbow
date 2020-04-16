3d2ed9f43ee31eeac523a24e8f354caa90801591
Ramón Guillermo Álvarez Hernández <62291608+rgah2107@users.noreply.github.com>
2020-04-16 00:23:38 -0400

    feat: implement the minus icon when some children are selected on the… (#1459)
    * feat: implement the minus icon when some children are selected on the Tree component
    
    #1429
    
    * fix: modify InputCheckbox component and its use in higher order components
    
    * fix: modify the Primitivecheckbox component and its use according to the specifications
    
    * fix: modify tests according to new changes
    
    * fix: modify components according to the last comments
    
    * fix: yarn lint problems
    
    * fix: modify example according to the comments
    
    * fix: modify example to make it work in any case
    
    * fix: modify example according to the last comments
    
    * fix: discard modification in yarn.lock file
    
    * fix: yarn.lock problems
    
    * fix: undo changes to the yarn file
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- src/components/Tree/helpers/getChild.js
+++
@@ -1,9 +0,0 @@
-const getChild = (tree, childPath) => {
-    const firstNode = tree[childPath[0]];
-    if (childPath.length === 1) {
-        return firstNode;
-    }
-    return getChild(firstNode.children, childPath.slice(1, childPath.length));
-};
-
-export default getChild;
---
+++ src/components/Tree/styled/inputCheckbox.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import PrimitiveCheckbox from '../../PrimitiveCheckbox';
+
+const InputCheckbox = styled(PrimitiveCheckbox)`
+    margin-right: 8px;
+`;
+
+export default InputCheckbox;
