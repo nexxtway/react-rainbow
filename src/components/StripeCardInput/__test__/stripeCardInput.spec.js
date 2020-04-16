4ea5ab13084eaaad7b6ce8925c9bc38faab30d27
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2020-04-15 23:19:43 -0400

    test: add test to StripeCardInput component (#1479)
    fix: #1450
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- src/components/StripeCardInput/__test__/stripeCardInput.spec.js
+++ src/components/StripeCardInput/__test__/stripeCardInput.spec.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Component as StripeCardInput } from '..';
+import StyledCardInput from '../styled/cardInput';
+import Label from '../../Input/label';
+import ErrorText from '../../Input/styled/errorText';
+import HelpText from '../../Input/styled/helpText';
 
 const error = {
     code: 'invalid_number',
@@ -13,11 +17,23 @@ const event = {
     complete: false,
     brand: 'unknown',
 };
+const focusEvent = {
+    type: 'focus',
+};
+const blurEvent = {
+    type: 'blur',
+};
 const elementMock = {
     mount: jest.fn(),
     on: jest.fn((eventType, callback) => {
         if (eventType === 'change') {
             callback(event);
+        }
+        if (eventType === 'focus') {
+            callback(focusEvent);
+        }
+        if (eventType === 'blur') {
+            callback(blurEvent);
         }
     }),
 };
@@ -50,5 +66,79 @@ describe('<StripeCardInput>', () => {
                 onChange={onChangeMockFn}
             />,
         );
+    });
+
+    it('should fire onFocus', doneFocus => {
+        const onFocusMockFn = e => {
+            expect(e).toEqual({
+                type: 'focus',
+            });
+            doneFocus();
+        };
+
+        mount(
+            <StripeCardInput
+                apiKey="STRIPE_API_KEY"
+                isScriptLoaded
+                isScriptLoadSucceed
+                onFocus={onFocusMockFn}
+            />,
+        );
+    });
+
+    it('should fire onBlur', doneBlur => {
+        const onBlurMockFn = e => {
+            expect(e).toEqual({
+                type: 'blur',
+            });
+            doneBlur();
+        };
+
+        mount(
+            <StripeCardInput
+                apiKey="STRIPE_API_KEY"
+                isScriptLoaded
+                isScriptLoadSucceed
+                onBlur={onBlurMockFn}
+            />,
+        );
+    });
+
+    it('should render bottom help text when bottomHelpText prop is passed', () => {
+        const component = mount(
+            <StripeCardInput
+                apiKey="STRIPE_API_KEY"
+                isScriptLoaded
+                isScriptLoadSucceed
+                bottomHelpText="Stripe card input"
+            />,
+        );
+        expect(component.find(HelpText).exists()).toBe(true);
+    });
+
+    it('should render an error text when error prop is passed', () => {
+        const component = mount(
+            <StripeCardInput
+                apiKey="STRIPE_API_KEY"
+                isScriptLoaded
+                isScriptLoadSucceed
+                error="An error occurred"
+            />,
+        );
+        expect(component.find(ErrorText).exists()).toBe(true);
+    });
+
+    it('should pass the same value to the id of StyledCardInput and the inputId prop of Label component', () => {
+        const component = mount(
+            <StripeCardInput
+                apiKey="STRIPE_API_KEY"
+                isScriptLoaded
+                isScriptLoadSucceed
+                label="Stripe card input"
+            />,
+        );
+        const inputIdProp = component.find(Label).prop('inputId');
+        const inputId = component.find(StyledCardInput).prop('id');
+        expect(inputIdProp).toBe(inputId);
     });
 });
