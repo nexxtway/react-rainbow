caacb1709ad16f635ca25780a36bec8c290b917c
Jose Leandro Torres <jtorressicilia@gmail.com>
2020-04-17 10:18:13 -0500

    fix: remove commented code

--- library/styleguideComponents/PropsTable/renderers/propTypeUnion.js
+++ library/styleguideComponents/PropsTable/renderers/propTypeUnion.js
@@ -2,6 +2,5 @@ import React from 'react';
 import buildUnionTypeList from './builders/unionTypeList';
 
 export default function propTypeShapeRender(value) {
-    // return <div>{JSON.stringify(value)}</div>;
     return <ul className="react-rainbow-proptype-union-list">{buildUnionTypeList(value)}</ul>;
 }
