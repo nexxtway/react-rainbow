cb4f47399535fde2c837d241206d2f1c5e1c12f9
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-04-22 00:07:07 -0400

    styles: change checkmark icon in all components (#1486)
    * styles: change checkmark icon in all components
    
    * styles: change checkmark icon in ProgressStep

--- src/components/VisualPickerOption/styled/checkmarkIcon.js
+++ src/components/VisualPickerOption/styled/checkmarkIcon.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
+import CheckmarkIcon from '../checkmark';
 
-const StyledCheckmarkIcon = attachThemeAttrs(styled.svg)`
+const StyledCheckmarkIcon = attachThemeAttrs(styled(CheckmarkIcon))`
     position: absolute;
     top: 8px;
     right: 8px;
-    fill: ${props => props.palette.getContrastText(props.palette.brand.main)};
+    width: 16px;
+    height:10px;
+    color: ${props => props.palette.getContrastText(props.palette.brand.main)};
 `;
 
 export default StyledCheckmarkIcon;
