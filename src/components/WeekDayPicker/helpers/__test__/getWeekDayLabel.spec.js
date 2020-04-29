e5e3200803135ccdecdbe512b60bac43b02ccd13
LeandroTorresSicilia <jtorressicilia@gmail.com>
2020-04-29 09:13:45 -0500

    fix: locale unit test

--- src/components/WeekDayPicker/helpers/__test__/getWeekDayLabel.spec.js
+++ src/components/WeekDayPicker/helpers/__test__/getWeekDayLabel.spec.js
@@ -1,9 +1,20 @@
 import getWeekDayLabel from '../getWeekDayLabel';
 
+global.Intl = {
+    DateTimeFormat: jest.fn(locale => ({
+        format: jest.fn(() => {
+            if (locale === 'es-MX') {
+                return 'L';
+            }
+            return 'M';
+        }),
+    })),
+};
+
 describe('getWeekDayLabel', () => {
     it('should return day label in english when locale is not manually set', () => {
-        const label = getWeekDayLabel('sunday');
-        expect(label).toBe('S');
+        const label = getWeekDayLabel('monday');
+        expect(label).toBe('M');
     });
     it('should return empty string when no day is sent', () => {
         const label = getWeekDayLabel();
