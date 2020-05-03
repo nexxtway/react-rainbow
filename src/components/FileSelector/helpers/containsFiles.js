6b07038da9c17fbde2c47b50f64c78cd527e7273
HellWolf93 <drodriguezfeito@gmail.com>
2020-05-03 19:24:16 -0400

    fix: add edge case to containsFiles when array is empty or undefined

--- src/components/FileSelector/helpers/containsFiles.js
+++ src/components/FileSelector/helpers/containsFiles.js
@@ -1,4 +1,7 @@
 export default function containsFiles(event) {
+    if (!Array.isArray(event.dataTransfer.types) || event.dataTransfer.types.length === 0) {
+        return false;
+    }
     if (event.dataTransfer.types) {
         return !event.dataTransfer.types.some(type => type !== 'Files');
     }
