5ae0d2ccc75322be073b319f384ebb38e7bf0c6c
guille <ramon.alvarez@desoft.cu>
2020-05-14 16:22:14 -0400

    feat: add the variant prop to the Picklist component
    fix: #1542

--- library/exampleComponents/GlobalHeader/styled.js
+++ library/exampleComponents/GlobalHeader/styled.js
@@ -5,6 +5,13 @@ import attachThemeAttrs from '../../../src/styles/helpers/attachThemeAttrs';
 export const StyledGlobalHeader = attachThemeAttrs(styled.header)`
     background-color: ${props => props.palette.background.main};
     border-bottom: 1px solid ${props => props.palette.border.divider};
+
+    ${props =>
+        props.variant === 'shaded' &&
+        `
+            background-color: ${props.palette.background.secondary};
+            border-bottom: none;
+        `};
 `;
 
 export const StyledLogo = styled.img`
