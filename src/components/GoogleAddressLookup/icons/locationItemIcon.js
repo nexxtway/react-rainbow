ba650f7359a18cd6ee32ccc63d4ad5e1218804f1
Grethel Talles <gtallesb@gmail.com>
2020-05-19 13:55:54 -0400

    fix: icons color on GoogleAddressLookup component

--- src/components/GoogleAddressLookup/icons/locationItemIcon.js
+++ src/components/GoogleAddressLookup/icons/locationItemIcon.js
@@ -14,16 +14,16 @@ const LocationItemIcon = props => {
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
         >
-           <IconCircleColor>
+            <IconCircleColor>
                 <circle cx="14" cy="14" r="14" />
                 <IconPinColor>
-                 <path
-                    transform="translate(8 6)"
-                    fillRule="nonzero"
-                    d="M5.383 15.677C.843 9.095 0 8.42 0 6a6 6 0 1 1 12 0c0 2.42-.843 3.095-5.383 9.677a.75.75 0 0 1-1.234 0zM6 8.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
-                 />
+                    <path
+                        transform="translate(8 6)"
+                        fillRule="nonzero"
+                        d="M5.383 15.677C.843 9.095 0 8.42 0 6a6 6 0 1 1 12 0c0 2.42-.843 3.095-5.383 9.677a.75.75 0 0 1-1.234 0zM6 8.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                    />
                 </IconPinColor>
-           </IconCircleColor>
+            </IconCircleColor>
         </svg>
     );
 };
