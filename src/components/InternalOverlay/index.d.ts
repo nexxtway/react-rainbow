99c15ae01d0af8c7b492cca0ad87e6e80a8a00a1
HellWolf93 <drodriguezfeito@gmail.com>
2020-05-23 15:09:03 -0400

    fix: add positionResolver parameter type

--- src/components/InternalOverlay/index.d.ts
+++ src/components/InternalOverlay/index.d.ts
@@ -7,11 +7,34 @@ interface Position {
     left: number;
 }
 
+interface Coordinates {
+    x: number;
+    y: number;
+}
+
+interface Size {
+    width: number;
+    height: number;
+}
+
+interface TriggerMeta {
+    leftUpAnchor: Coordinates;
+    leftBottomAnchor: Coordinates;
+    rightUpAnchor: Coordinates;
+    rightBottomAnchor: Coordinates;
+}
+
+interface PositionResolverOpts {
+    trigger: TriggerMeta;
+    viewport: Size;
+    content: Size;
+}
+
 export interface InternalOverlayProps {
     render: ComponentClass | FunctionComponent;
     isVisible?: boolean;
     triggerElementRef: RefObject | TriggerElementRefFunction;
-    positionResolver: () => Position;
+    positionResolver: (opts: PositionResolverOpts) => Position;
 }
 
 export default function(props: InternalOverlayProps): JSX.Element | null;
