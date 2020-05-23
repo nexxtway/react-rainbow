c7e7f2df479748ceb1870b76fc521b7142d9cc37
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-05-23 07:03:56 -0400

    fix: change the selected country

--- src/components/PhoneInput/countriesDropdown/styled/item.js
+++ src/components/PhoneInput/countriesDropdown/styled/item.js
@@ -22,12 +22,6 @@ const StyledItem = attachThemeAttrs(styled.li)`
     }
 
     ${props =>
-        props.isSelected &&
-        `
-            background-color: ${props.palette.action.active};
-        `};
-
-    ${props =>
         !props.isSelected &&
         `
             padding-right: 37px;
