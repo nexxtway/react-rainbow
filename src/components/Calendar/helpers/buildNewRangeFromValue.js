23a15346cee95f235db3b1c79d7e98c15a0bc865
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-05-26 12:21:10 -0400

    fix: mutation issue in buildNewRangeFromValue helper

--- src/components/Calendar/helpers/buildNewRangeFromValue.js
+++ src/components/Calendar/helpers/buildNewRangeFromValue.js
@@ -4,27 +4,29 @@ import compareDates from './compareDates';
 export default function buildNewRangeFromValue(value, currentRange, currentUpdatePosition = 0) {
     if (currentRange && currentRange.length > 0) {
         const [rangeStart, rangeEnd] = currentRange;
-        rangeStart.setHours(0, 0, 0, 0);
+        const newRangeStart = new Date(rangeStart);
+        newRangeStart.setHours(0, 0, 0, 0);
 
         if (rangeEnd && isDateWithinRange(value, currentRange)) {
             if (currentUpdatePosition === 0) {
                 value.setHours(0, 0, 0, 0);
-                rangeEnd.setHours(23, 59, 59, 999);
+                const newRangeEnd = new Date(rangeEnd);
+                newRangeEnd.setHours(23, 59, 59, 999);
                 return {
-                    range: [value, rangeEnd],
+                    range: [value, newRangeEnd],
                     nextUpdatePosition: 1,
                 };
             }
 
             value.setHours(23, 59, 59, 999);
             return {
-                range: [rangeStart, value],
+                range: [newRangeStart, value],
                 nextUpdatePosition: 0,
             };
-        } else if (!rangeEnd && compareDates(value, rangeStart) >= 0) {
+        } else if (!rangeEnd && compareDates(value, newRangeStart) >= 0) {
             value.setHours(23, 59, 59, 999);
             return {
-                range: [rangeStart, value],
+                range: [newRangeStart, value],
             };
         }
     }
