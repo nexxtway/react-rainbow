111cf30101f8cba63f70646d577a56bb4cf3d168
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-05-29 13:06:58 -0400

    fix: some minor fixes

--- src/components/Calendar/doubleCalendar/hooks/useNormalizedValue.js
+++ src/components/Calendar/doubleCalendar/hooks/useNormalizedValue.js
@@ -4,9 +4,8 @@ import { normalizeDate, isSameDay } from '../../helpers';
 export default function useNormalizedValue(value) {
     const normalizedValue = useRef(null);
     return useMemo(() => {
-        if (!isSameDay(value, normalizedValue.current)) {
-            normalizedValue.current = normalizeDate(value);
-        }
+        if (isSameDay(value, normalizedValue.current)) return normalizedValue.current;
+        normalizedValue.current = normalizeDate(value);
         return normalizedValue.current;
     }, [value]);
 }
