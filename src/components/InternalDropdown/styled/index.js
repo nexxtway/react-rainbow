3e100a79799f0907d34a2edcb7a52a7c926e8f2f
LeandroTorresSicilia <jtorressicilia@gmail.com>
2020-05-29 23:30:40 -0500

    fix: lint error

--- src/components/InternalDropdown/styled/index.js
+++ src/components/InternalDropdown/styled/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
-import { FONT_SIZE_TEXT_LARGE } from '../../../../styles/fontSizes';
+import { FONT_SIZE_TEXT_LARGE } from '../../../styles/fontSizes';
 
 export const Dropdown = attachThemeAttrs(styled.div)`
     position: relative;
