3933f1d197fdd4f73bb84a150d04378f4f10685f
Jose Leandro Torres <jtorressicilia@gmail.com>
2020-06-20 22:04:28 -0500

    fix: lint error

--- src/components/MultiSelect/helpers/__test__/hasChips.spec.js
+++ src/components/MultiSelect/helpers/__test__/hasChips.spec.js
@@ -3,15 +3,15 @@ import hasChips from '../hasChips';
 describe('hasChips', () => {
     it('should return true', () => {
         const values = [{ label: 'Label', name: 'Name' }, [{ label: 'Label', name: 'Name' }]];
-        values.forEach(val => {
-            expect(hasChips(val)).toBe(true);
+        values.forEach(value => {
+            expect(hasChips(value)).toBe(true);
         });
     });
 
     it('should return false', () => {
         const values = [undefined, null, 0, false, true, []];
-        values.forEach((val, index) => {
-            expect(hasChips(val)).toBe(false);
+        values.forEach(value => {
+            expect(hasChips(value)).toBe(false);
         });
     });
 });
