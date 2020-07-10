630ad532a67ec88b04fb21568f5788ad8662961a
hportales2000 <pablo@nexxtway.com>
2020-07-10 16:17:07 -0400

    fix: removing unnecesary files

--- src/components/MarkdownOutput/index.d 2.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import { BaseProps } from '../types';
-
-export interface MarkdownOutputProps extends BaseProps {
-    id?: string;
-    value?: string;
-}
-
-export default function(props: MarkdownOutputProps): JSX.Element | null;
