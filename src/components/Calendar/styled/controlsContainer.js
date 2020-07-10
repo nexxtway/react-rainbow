50b79ed607bd0c603e2da3d686332292761fe13a
hportales2000 <58480174+hportales2000@users.noreply.github.com>
2020-07-10 16:40:14 -0400

    fix: responsive calendar (#1691)
    * fix: calendar view for mobile
    
    * fix: calendar view for mobile
    
    * fix: cleaning code
    
    * skipping test 'should set range initial and end dates when using keyboard navigation' until until further keboard featuare development
    
    * fix: removing unnecesary files
    
    * fix: error
    
    Co-authored-by: Pablo Alberto Hernandez Portales <pabloalbertohernandezportales@MacBook-Pro.attlocal.net>
    Co-authored-by: TahimiLeonBravo <tahimi@nexxtway.com>

--- src/components/Calendar/styled/controlsContainer.js
+++ src/components/Calendar/styled/controlsContainer.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const StyledControlsContainer = styled.div`
     display: flex;
-    align-items: center;
+    flex-grow: 1;
+    align-content: start;
     justify-content: space-between;
     padding: 0 0 8px 0;
 `;
