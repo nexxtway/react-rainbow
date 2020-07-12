a86075b5b732398d8d7e161e2ef51954170affc7
Maxx Greene <maxx@nexxtway.com>
2020-07-12 02:47:36 -0400

    refactor: functions

--- src/components/Chart/chart.js
+++ src/components/Chart/chart.js
@@ -10,38 +10,44 @@ const getRadius = (height, width) => {
     return maxRadius;
 };
 
-Chart.elements.Rectangle.prototype.draw = function Draw() {
+function drawRectVertical(context, view) {
+    const { x, y, base, width } = view;
+    const left = x - width / 2;
+    const height = base - y;
+    const radius = getRadius(height, width);
+    context.moveTo(left + radius, y);
+    context.lineTo(left + width - radius, y);
+    context.quadraticCurveTo(left + width, y, left + width, y + radius);
+    context.lineTo(left + width, base);
+    context.lineTo(left, base);
+    context.lineTo(left, y + radius);
+    context.quadraticCurveTo(left, y, left + radius, y);
+}
+
+function drawRectHorizontal(context, view) {
+    const { x, y, base, height } = view;
+    const width = x;
+    const top = y - height / 2;
+    const radius = getRadius(height, width);
+    context.moveTo(x - radius, top);
+    context.quadraticCurveTo(x, top, x, top + radius);
+    context.lineTo(x, top + height - radius);
+    context.quadraticCurveTo(x, top + height, x - radius, top + height);
+    context.lineTo(base, top + height);
+    context.lineTo(base, top);
+    context.lineTo(top - radius, top);
+}
+
+Chart.elements.Rectangle.prototype.draw = function drawRect() {
     const context = this._chart.ctx;
     const view = this._view;
-    const { x, y, base } = view;
     context.beginPath();
     context.fillStyle = view.backgroundColor;
     context.strokeStyle = view.borderColor;
-
     if (view.horizontal) {
-        const { height } = view;
-        const width = x;
-        const top = y - height / 2;
-        const radius = getRadius(height, width);
-        context.moveTo(x - radius, top);
-        context.quadraticCurveTo(x, top, x, top + radius);
-        context.lineTo(x, top + height - radius);
-        context.quadraticCurveTo(x, top + height, x - radius, top + height);
-        context.lineTo(base, top + height);
-        context.lineTo(base, top);
-        context.lineTo(top - radius, top);
+        drawRectHorizontal(context, view);
     } else {
-        const { width } = view;
-        const left = x - width / 2;
-        const height = base - y;
-        const radius = getRadius(height, width);
-        context.moveTo(left + radius, y);
-        context.lineTo(left + width - radius, y);
-        context.quadraticCurveTo(left + width, y, left + width, y + radius);
-        context.lineTo(left + width, base);
-        context.lineTo(left, base);
-        context.lineTo(left, y + radius);
-        context.quadraticCurveTo(left, y, left + radius, y);
+        drawRectVertical(context, view);
     }
     context.fill();
 };
