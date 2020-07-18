8043f0e14903e14afcff2037ac17c465c27ed4bc
Jose Leandro Torres <jtorressicilia@gmail.com>
2020-07-18 10:57:19 -0500

    test: tree component (#1727)
    * fix: accessibility issues on Tree component, adjusting examples
    
    * replacing yarn.lock with master
    
    * cleaning code
    
    * cleaning code
    
    * fix: tree accesibility issues
    
    * fix: tree accesibility issues
    
    * fix: impriving code
    
    * fix: test
    
    * chore: fix yarn.lock
    
    * test: first commit
    
    * fix: adding tests
    
    * fix: adding tests
    
    Co-authored-by: hportales2000 <pablo@nexxtway.com>
    Co-authored-by: Tahimi <tahimileon@gmail.com>

--- src/components/Tree/__test__/tree.spec.js
+++ src/components/Tree/__test__/tree.spec.js
@@ -41,13 +41,13 @@ const data = [
 describe('<Tree/>', () => {
     it('should call onNodeExpand with the right parameters when the button is clicked', () => {
         const nodePath = [2, 1];
-        const onNodeExandMock = jest.fn();
-        const component = mount(<Tree data={data} onNodeExpand={onNodeExandMock} />);
+        const onNodeExpandMock = jest.fn();
+        const component = mount(<Tree data={data} onNodeExpand={onNodeExpandMock} />);
         component
             .find('ButtonIcon')
             .at(1)
             .simulate('click');
-        expect(onNodeExandMock).toHaveBeenCalledWith({ nodePath });
+        expect(onNodeExpandMock).toHaveBeenCalledWith({ nodePath });
     });
     it('should call onNodeCheck with the right parameters when the node is selected', () => {
         const nodePath = [2];
@@ -60,8 +60,95 @@ describe('<Tree/>', () => {
             .simulate('change');
         expect(onNodeCheckMock).toHaveBeenCalledWith({ nodePath });
     });
+    it('should call onNodeSelect with the right data', () => {
+        const name = 'node-2';
+        const nodePath = [1];
+        const onNodeSelectMock = jest.fn();
+        const component = mount(<Tree data={data} onNodeSelect={onNodeSelectMock} />);
+        component
+            .find('li')
+            .at(1)
+            .simulate('click');
+        expect(onNodeSelectMock).toHaveBeenCalledWith({ name, nodePath });
+    });
     it('should render the correct number of children', () => {
         const component = mount(<Tree data={data} />);
         expect(component.find('Child').length).toBe(10);
+    });
+    it('should have the container ul element a role tree', () => {
+        const component = mount(<Tree data={data} />);
+        const container = component.find('ul').at(0);
+        expect(container.prop('role')).toBe('tree');
+    });
+    it('should have the role group in the child ul element', () => {
+        const component = mount(<Tree data={data} />);
+        const container = component.find('ul').at(1);
+        expect(container.prop('role')).toBe('group');
+    });
+    it('should set the first li element with tabIndex 0', () => {
+        const component = mount(<Tree data={data} />);
+        const firstLi = component.find('li').at(0);
+        expect(firstLi.prop('tabIndex')).toBe(0);
+    });
+    it('should all li element with tabIndex -1 except the first one', () => {
+        const component = mount(<Tree data={data} />);
+        const secondLi = component.find('li').at(1);
+        expect(secondLi.prop('tabIndex')).toBe(-1);
+        const thirdLi = component.find('li').at(2);
+        expect(thirdLi.prop('tabIndex')).toBe(-1);
+    });
+    it('should tabIndex 0 to the selected node', () => {
+        const component = mount(<Tree data={data} selectedNode="node-3.2" />);
+        const li = component.find('li').at(4);
+        expect(li.prop('tabIndex')).toBe(0);
+    });
+    it('should generate the right node names', () => {
+        const component = mount(<Tree data={data} />);
+        const firstLi = component.find('li').at(0);
+        expect(firstLi.prop('id')).toBe('node-1');
+        const secondLi = component.find('li').at(1);
+        expect(secondLi.prop('id')).toBe('node-2');
+        const fourthLi = component.find('li').at(3);
+        expect(fourthLi.prop('id')).toBe('node-3.1');
+        const fifthLi = component.find('li').at(4);
+        expect(fifthLi.prop('id')).toBe('node-3.2');
+    });
+    it('should set tabIndex to -1 in expand collapse button', () => {
+        const component = mount(<Tree data={data} />);
+        const firstExpandButton = component.find('ButtonIcon').at(0);
+        expect(firstExpandButton.prop('tabIndex')).toBe(-1);
+        const fourthExpandButton = component.find('ButtonIcon').at(2);
+        expect(fourthExpandButton.prop('tabIndex')).toBe(-1);
+    });
+    it('should pass right aria-level number', () => {
+        const component = mount(<Tree data={data} />);
+        const firstLi = component.find('li').at(0);
+        expect(firstLi.prop('aria-level')).toBe(1);
+        const fourthLi = component.find('li').at(3);
+        expect(fourthLi.prop('aria-level')).toBe(2);
+        const sixthLi = component.find('li').at(5);
+        expect(sixthLi.prop('aria-level')).toBe(3);
+    });
+    it('should pass the right aria-label', () => {
+        const ariaLabel = 'labelSample';
+        const component = mount(<Tree data={data} ariaLabel={ariaLabel} />);
+        const container = component.find('ul').at(0);
+        expect(container.prop('aria-label')).toBe(ariaLabel);
+    });
+    it('should pass the right aria-labelledby', () => {
+        const ariaLabelledBy = 'labelledBySample';
+        const component = mount(<Tree data={data} ariaLabelledBy={ariaLabelledBy} />);
+        const container = component.find('ul').at(0);
+        expect(container.prop('aria-labelledby')).toBe(ariaLabelledBy);
+    });
+    it('should not set aria-expanded when the node does not have children', () => {
+        const component = mount(<Tree data={data} />);
+        const firstLi = component.find('li').at(1);
+        expect(firstLi.prop('aria-expanded')).toBeUndefined();
+    });
+    it('should set the aria-expanded value of the node when it has children', () => {
+        const component = mount(<Tree data={data} />);
+        const node = component.find('li').at(2);
+        expect(node.prop('aria-expanded')).toBe(true);
     });
 });
