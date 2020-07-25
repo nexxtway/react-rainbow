b92933bdd4db57b836251a95fc6f4b314bb7e15f
Ramon Chancay <ramon.chancay@gmail.com>
2020-07-25 12:50:59 -0500

    change tests

--- src/components/PhoneInput/__tests__/phoneinput.spec.js
+++ src/components/PhoneInput/__tests__/phoneinput.spec.js
@@ -2,26 +2,21 @@ import React from 'react';
 import { mount } from 'enzyme';
 import PhoneInput from '../';
 
-const Wrapper = () => {
-    const [value, setValue] = React.useState('');
-    return (
-        <PhoneInput
-            label="Phone Number"
-            value={value}
-            onChange={setValue}
-            placeholder="Enter your phone number"
-        />
-    );
-};
-
 describe('<PhoneInput />', () => {
     it('Should accept only numbers.', () => {
-        const wrapper = mount(<Wrapper />);
+        const onChangeMockFn = jest.fn();
+        const wrapper = mount(<PhoneInput label="Phone Number" onChange={onChangeMockFn} />);
+
         wrapper
             .find('input[type="tel"]')
             .first()
             .simulate('change', { target: { value: 'Your phone number is 0987256983' } });
         wrapper.update();
-        expect(wrapper.find('input[type="tel"]').prop('value')).toBe('0987256983');
+
+        expect(onChangeMockFn).toHaveBeenCalledWith(
+            expect.objectContaining({
+                phone: '0987256983',
+            }),
+        );
     });
 });
