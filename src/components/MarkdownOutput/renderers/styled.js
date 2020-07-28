5c3fad51785fd3059c7c12b9524474259368fd02
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-07-28 14:19:50 -0400

    fix: remove some style of StyleGuide

--- src/components/MarkdownOutput/renderers/styled.js
+++ src/components/MarkdownOutput/renderers/styled.js
@@ -54,8 +54,71 @@ export const StyledCode = attachThemeAttrs(styled.code)`
         margin-right: 4px;
     }
 
-    .hljs-emphasis {
-        font-style: italic;
+    
+    .hljs-comment,
+    .hljs-meta {
+        color: #969896;
+    }
+
+    .hljs-variable,
+    .hljs-template-variable,
+    .hljs-strong,
+    .hljs-emphasis,
+    .hljs-quote {
+        color: #df5000;
+    }
+
+    .hljs-keyword,
+    .hljs-selector-tag,
+    .hljs-type {
+        color: #d73a49;
+    }
+
+    .hljs-literal,
+    .hljs-symbol,
+    .hljs-bullet,
+    .hljs-attribute {
+        color: #0086b3;
+    }
+
+    .hljs-section,
+    .hljs-name {
+        color: #63a35c;
+    }
+
+    .hljs-tag {
+    color: #333333;
+    }
+
+    .hljs-title,
+    .hljs-attr,
+    .hljs-selector-id,
+    .hljs-selector-class,
+    .hljs-selector-attr,
+    .hljs-selector-pseudo {
+        color: #6f42c1;
+    }
+
+    .hljs-addition {
+        color: #55a532;
+        background-color: #eaffea;
+    }
+
+    .hljs-deletion {
+        color: #bd2c00;
+        background-color: #ffecec;
+    }
+
+    .hljs-link {
+        text-decoration: underline;
+    }
+
+    .hljs-number {
+        color: #005cc5;
+    }
+
+    .hljs-string {
+        color: #032f62;
     }
 `;
 
