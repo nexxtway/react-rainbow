e4ef87e11dea6248ee400dee41800871a0e6714e
HellWolf93 <drodriguezfeito@gmail.com>
2020-08-02 22:59:26 -0400

    fix: refactor getAllValues to make it more functional

--- src/components/InternalDropdown/helpers/getAllValues.js
+++ src/components/InternalDropdown/helpers/getAllValues.js
@@ -4,16 +4,16 @@ export default function getAllValues(children) {
     if (!Array.isArray(children)) {
         return [];
     }
-    const values = [];
-    children.forEach(child => {
+    const values = children.reduce((accumulator, child) => {
         const { label: childLabel, name, value: childValue, variant: childVariant } = child;
         if (childVariant === 'default') {
-            values.push({
+            accumulator.push({
                 label: childLabel,
                 name,
                 value: childValue,
             });
         }
-    });
+        return accumulator;
+    }, []);
     return normalizeValue(values);
 }
