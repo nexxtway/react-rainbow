f3477f6df08a3a1aad83bdae576a3f6642ebfc9b
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2020-08-05 12:06:41 -0400

    feat: implement `showCheckbox` prop on the `MultiSelect` component (#1689)
    * feat: implement `showCheckbox` prop on the `MultiSelect` component
    
    fix: #1655
    
    * fix: improve MultiSelect component
    
    * fix: outside click not working
    
    * fix: failing test cases
    
    * fix: Option component test case failing
    
    * fix: remove stopPropagation usages
    
    * fix: remove unnecessary component in MultiSelect
    
    * fix: refactor getAllValues to make it more functional
    
    * fix: remove unused import
    
    * fix: remove unnecessary preventDefault
    
    * fix: move normalizeValue back to MultiSelect and fix tests
    
    * fix: update normalizeValue import
    
    * fix: remove unnecessary timeouts
    
    * fix: getAllValues missing icon and tests
    
    Co-authored-by: Tahimi <tahimileon@gmail.com>
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

---
+++ src/components/Option/styled/button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Button from '../../Button';
+
+const StyledButton = styled(Button)`
+    height: 1.7rem;
+    line-height: 1.7rem;
+    font-size: 0.8rem;
+    padding: 0 0.7rem;
+`;
+
+export default StyledButton;
