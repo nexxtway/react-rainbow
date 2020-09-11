e5082595055e8a4f8f109ae3d3276c45e2ec558f
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-09-11 00:18:33 -0400

    test: fix integration tests for AvatarMenu component

--- integration/specs/AvatarMenu/avatarMenu-1.spec.js
+++ integration/specs/AvatarMenu/avatarMenu-1.spec.js
@@ -1,10 +1,5 @@
 const PageAvatarMenu = require('../../../src/components/AvatarMenu/pageObject');
-const {
-    ESCAPE_KEY,
-    ARROW_DOWN_KEY,
-    ARROW_UP_KEY,
-    TAB_KEY,
-} = require('../../constants');
+const { ESCAPE_KEY, ARROW_DOWN_KEY, ARROW_UP_KEY, TAB_KEY } = require('../../constants');
 
 const AVATAR_MENU = '#avatar-menu';
 const REACT_LOGO = 'img[alt="rainbow logo"]';
@@ -17,6 +12,7 @@ describe('AvatarMenu base example', () => {
         browser.refresh();
         const component = $(AVATAR_MENU);
         component.waitForExist();
+        $('button=Show Code').moveTo();
     });
 
     it('should open the menu when click on the AvatarMenu', () => {
@@ -39,8 +35,9 @@ describe('AvatarMenu base example', () => {
     });
     it('should close the menu when the menu is opened and click an element', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
-        const menuItem = avatarMenu.getItem(1);
         avatarMenu.click();
+        expect(avatarMenu.isOpen()).toBe(true);
+        const menuItem = avatarMenu.getItem(1);
         menuItem.click();
         expect(avatarMenu.isOpen()).toBe(false);
     });
@@ -52,39 +49,39 @@ describe('AvatarMenu base example', () => {
     });
     it('should set the focus to the first menu item when open the menu', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
-        const menuItem = avatarMenu.getItem(0);
         avatarMenu.click();
+        const menuItem = avatarMenu.getItem(0);
         expect(menuItem.hasFocus()).toBe(true);
     });
     it('should move to the next item when press arrow down', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
-        const menuItem = avatarMenu.getItem(1);
         avatarMenu.click();
+        const menuItem = avatarMenu.getItem(1);
         browser.keys(ARROW_DOWN_KEY);
         expect(menuItem.hasFocus()).toBe(true);
     });
     it('should move to the previous item when press arrow up', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
+        avatarMenu.click();
         const menuItemOne = avatarMenu.getItem(0);
         const menuItemTwo = avatarMenu.getItem(1);
-        avatarMenu.click();
         menuItemTwo.hover();
         browser.keys(ARROW_UP_KEY);
         expect(menuItemOne.hasFocus()).toBe(true);
     });
     it('should move to the first item when last item is active and press arrow down', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
+        avatarMenu.click();
         const firstMenuItem = avatarMenu.getItem(0);
         const lastMenuItem = avatarMenu.getItem(1);
-        avatarMenu.click();
         lastMenuItem.hover();
         browser.keys(ARROW_DOWN_KEY);
         expect(firstMenuItem.hasFocus()).toBe(true);
     });
     it('should move to the last item when first item is active and press arrow up', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
-        const lastMenuItem = avatarMenu.getItem(1);
         avatarMenu.click();
+        const lastMenuItem = avatarMenu.getItem(1);
         browser.keys(ARROW_UP_KEY);
         expect(lastMenuItem.hasFocus()).toBe(true);
     });
@@ -96,8 +93,8 @@ describe('AvatarMenu base example', () => {
     });
     it('should move focus to the item hovered', () => {
         const avatarMenu = new PageAvatarMenu(AVATAR_MENU);
-        const menuItem = avatarMenu.getItem(1);
         avatarMenu.click();
+        const menuItem = avatarMenu.getItem(1);
         menuItem.hover();
         expect(menuItem.hasFocus()).toBe(true);
     });
