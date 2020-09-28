258c417b8fd479e8dda906469762089d0ef20d62
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2020-09-28 11:27:31 -0500

    docs: example of dynamic row action in table component (#1852)
    * docs: example of dynamic row action in table component
    
    * docs: added exapledata.js
    
    * docs: moved data examples to library/data
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

---
+++ library/data/dataTableExamples.js
@@ -0,0 +1,249 @@
+export const DataTable = [
+    {
+        name: 'Leandro Torres',
+        company: 'nexxtway',
+        email: 'leandro@gmail.com',
+        status: 'verified',
+        id: '1234qwerty',
+    },
+    {
+        name: 'Jose Torres',
+        company: 'Google',
+        email: 'jose@gmail.com',
+        status: 'verified',
+        id: '1234asdfgh',
+    },
+    {
+        name: 'Reinier',
+        company: 'Nexxtway',
+        email: 'reinier@gmail.com',
+        status: 'verified',
+        id: '1234zxcvbn',
+    },
+    {
+        name: 'Sara',
+        company: 'Nexxtway',
+        email: 'sara@gmail.com',
+        status: 'verified',
+        id: '5678qwerty',
+    },
+    {
+        name: 'Tahimi',
+        company: 'nexxtway',
+        email: 'tahimi@gmail.com',
+        status: 'verified',
+        id: '5678asdfgh',
+    },
+    {
+        name: 'Carlos',
+        company: 'Oracle',
+        email: 'carlos@gmail.com',
+        status: 'verified',
+        id: '5678zxcvbn',
+    },
+    {
+        name: 'Andy',
+        company: 'Oracle',
+        email: 'andy@gmail.com',
+        status: 'verified',
+        id: '9012qwerty',
+    },
+    {
+        name: 'Luis',
+        company: 'Google',
+        email: 'luis@gmail.com',
+        status: 'verified',
+        id: '9012asdfgh',
+    },
+];
+
+export const DynamicDataTable = [
+    {
+        name: 'Leandro Torres',
+        company: 'nexxtway',
+        email: 'leandro@gmail.com',
+        status: 'verified',
+        id: '1234qwerty',
+        link: '#',
+    },
+    {
+        name: 'Jose Torres',
+        company: 'Google',
+        email: 'jose@gmail.com',
+        status: 'pending',
+        id: '1234asdfgh',
+        link: '#',
+    },
+    {
+        name: 'Reinier',
+        company: 'nexxtway',
+        email: 'reinier@gmail.com',
+        status: 'verified',
+        id: '1234zxcvbn',
+        link: '#',
+    },
+    {
+        name: 'Sara',
+        company: 'nexxtway',
+        email: 'sara@gmail.com',
+        status: 'pending',
+        id: '5678qwerty',
+        link: '#',
+    },
+    {
+        name: 'Tahimi',
+        company: 'nexxtway',
+        email: 'tahimi@gmail.com',
+        status: 'verified',
+        id: '5678asdfgh',
+        link: '#',
+    },
+    {
+        name: 'Leo',
+        company: 'nexxtway',
+        email: 'leo@gmail.com',
+        status: 'verified',
+        id: '5678zxcvbn',
+        link: '#',
+    },
+    {
+        name: 'Tahimi Leon',
+        company: 'nexxtway',
+        email: 'leon@nexxtway.com',
+        status: 'verified',
+        id: '9012qwerty',
+        link: '#',
+    },
+    {
+        name: 'Alejandro',
+        company: 'Google',
+        email: 'alejandro@gmail.com',
+        status: 'pending',
+        id: '5678zdfgdf',
+        link: '#',
+    },
+    {
+        name: 'Carlos',
+        company: 'Oracle',
+        email: 'carlos@gmail.com',
+        status: 'verified',
+        id: '3434fgfgdf',
+        link: '#',
+    },
+    {
+        name: 'Luis',
+        company: 'Google',
+        email: 'luis@gmail.com',
+        status: 'verified',
+        id: '9012asdfgh',
+        link: '#',
+    },
+];
+
+export const ActionsDataTable = [
+    {
+        name: 'Leandro Torres',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '1234qwerty',
+    },
+    {
+        name: 'Jose Torres',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '1234asdfgh',
+    },
+    {
+        name: 'Reinier',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '1234zxcvbn',
+    },
+    {
+        name: 'Sara',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '5678qwerty',
+    },
+    {
+        name: 'Tahimi',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '5678asdfgh',
+    },
+    {
+        name: 'Leo',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '5678zxcvbn',
+    },
+    {
+        name: 'Tahimi Leon',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '9012qwerty',
+    },
+    {
+        name: 'Alejandro',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '5678zdfgdf',
+    },
+    {
+        name: 'Carlos',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '3434fgfgdf',
+    },
+    {
+        name: 'Luis',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+        id: '9012asdfgh',
+    },
+];
+
+export const ListviewDataTable = [
+    {
+        task: 'fix: keyboard navigation on Tree.',
+        coins: 2,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 2,
+        id: '1234qwerty',
+    },
+    {
+        task: 'feat: implement Notification Manager',
+        coins: 4,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 2,
+        id: '1234asdfgh',
+    },
+    {
+        task: 'test: InternalDropdown.',
+        coins: 2,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 2,
+        id: '1234zxcvbn',
+    },
+    {
+        task: 'feat: implement MultiSelect.',
+        coins: 8,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 2,
+        id: '5678qwerty',
+    },
+    {
+        task: 'fix: position resolver on InternalOverlay',
+        coins: 8,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 1,
+        id: '5678asdfgh',
+    },
+    {
+        task: 'refactor: ButtonMenu component.',
+        coins: 8,
+        constributor: 'yvmunayev@gmail.com',
+        priority: 0,
+        id: '5278aswegh',
+    },
+];
