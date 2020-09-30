e5dca785eec6578a36e6ab20b0b15dac1b38df79
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-09-30 16:29:59 -0400

    fix: change stars badge on react-rainbow

--- library/styleguideComponents/RibbonRenderer/index.js
+++ library/styleguideComponents/RibbonRenderer/index.js
@@ -1,55 +1,48 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import Badge from '../../../src/components/Badge';
+import React from 'react';
+import GitHubButton from 'react-github-btn';
 import githublogo from './image/github.svg';
-import fetchGithubStars from './fetchStars';
-import './styles.css';
+import {
+    StyledContainer,
+    StyledGitHubContianer,
+    StyledGithubIcon,
+    StyledGithubLink,
+    StyledHeading,
+} from './styled';
 
-export default class RibbonRenderer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { starsCount: '0' };
-    }
-
-    componentDidMount() {
-        fetchGithubStars().then(starsCount => this.setState({ starsCount }));
-    }
-
-    render() {
-        const { starsCount } = this.state;
-
-        return (
-            <header className="react-rainbow-heading-container rainbow-flex rainbow-justify_end rainbow-align_center rainbow-position_fixed">
-                <div className="rainbow-align-content_center react-rainbow-github-badge-container">
-                    <div className="react-rainbow-github-stars-container">
-                        <Badge
-                            className="react-rainbow-github-badge rainbow-color_dark-1 rainbow-m-right_small"
-                            variant="lightest"
-                        >
-                            <FontAwesomeIcon
-                                className="react-rainbow-github-badge_icon"
-                                color="#061c3f"
-                                icon={faStar}
-                            />
-                            {starsCount}
-                        </Badge>
-                    </div>
-                    <a
-                        className="react-rainbow-heading_github-link"
+export default function RibbonRenderer() {
+    return (
+        <StyledHeading>
+            <StyledContainer>
+                <StyledGitHubContianer>
+                    <GitHubButton
                         href="https://github.com/nexxtway/react-rainbow"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        data-icon="octicon-star"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Star nexxtway/react-rainbow on GitHub"
                     >
-                        <img
-                            src={githublogo}
-                            alt="github logo"
-                            className="react-rainbow-heading_github-icon"
-                        />
-                    </a>
-                </div>
-            </header>
-        );
-    }
+                        Star
+                    </GitHubButton>
+                </StyledGitHubContianer>
+                <StyledGitHubContianer>
+                    <GitHubButton
+                        href="https://github.com/nexxtway"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Follow @nexxtway on GitHub"
+                    >
+                        Follow
+                    </GitHubButton>
+                </StyledGitHubContianer>
+                <StyledGithubLink
+                    href="https://github.com/nexxtway/react-rainbow"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <StyledGithubIcon src={githublogo} alt="github logo" />
+                </StyledGithubLink>
+            </StyledContainer>
+        </StyledHeading>
+    );
 }
