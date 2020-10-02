d1f77fe6f3d830ddba934de25914f97ed5182cd1
Grethel Talles <gtallesb@gmail.com>
2020-10-02 10:18:30 -0400

    test: add tests for array with empty string and invalid country code

--- src/components/PhoneInput/hooks/useCountries.js
+++ src/components/PhoneInput/hooks/useCountries.js
@@ -9,7 +9,7 @@ export default function useCountries(countries) {
         if (allCountries.filter(country => countries.includes(country.isoCode)).length === 0) {
             console.warn('You have entered an invalid country code or empty input');
             return allCountries;
-        } 
+        }
         return allCountries.filter(country => countries.includes(country.isoCode));
     }, [countries]);
 }
