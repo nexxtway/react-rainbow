a7f26a32a8bfd5614bcd89c9b058a7f37cdbc69c
Grethel Talles <gtallesb@gmail.com>
2020-10-02 09:41:39 -0400

    test: add tests for array with empty string and invalid country code

--- src/components/PhoneInput/hooks/useCountries.js
+++ src/components/PhoneInput/hooks/useCountries.js
@@ -3,10 +3,10 @@ import allCountries from '../countries';
 
 export default function useCountries(countries) {
     return useMemo(() => {
-        if ((countries.length === 0)) {
+        if (countries.length === 0) {
             return allCountries;
         }
-        if ((allCountries.filter(country => countries.includes(country.isoCode))).length === 0) {
+        if (allCountries.filter(country => countries.includes(country.isoCode)).length === 0) {
             console.warn('You have entered an invalid country code or empty input');
             return allCountries;
         } 
