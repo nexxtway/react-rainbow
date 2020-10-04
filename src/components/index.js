497ff9700da1506bd7fc5a5345fd25c894d400a9
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2020-10-04 15:18:06 -0400

    feat: implement 'LoadingShape' component (#1866)
    * feat: implement 'LoadingShape' component
    
    fix: #1846
    
    * fix: change rect and rouded rect min-height
    
    * fix: add LoadingShape component description
    
    * fix: change interactive example input type
    
    * fix: make SCG icons fill currentColor
    
    * fix: some changes in styles
    
    * docs: update icon
    
    * fix: make text color change with the theme
    
    * docs: add real use case example
    
    * fix: icon
    
    Co-authored-by: Tahimi <tahimileon@gmail.com>
    Co-authored-by: TahimiLeonBravo <tahimi@nexxtway.com>

--- src/components/index.js
+++ src/components/index.js
@@ -37,6 +37,7 @@ export { default as GoogleAddressLookup } from './GoogleAddressLookup';
 export { default as HelpText } from './HelpText';
 export { default as ImportRecordsFlow } from './ImportRecordsFlow';
 export { default as Input } from './Input';
+export { default as LoadingShape } from './LoadingShape';
 export { default as Lookup } from './Lookup';
 export { default as MapMarker } from './MapMarker';
 export { default as MarkdownOutput } from './MarkdownOutput';
