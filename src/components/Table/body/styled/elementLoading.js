cb4e84275e0a13bbe0530e54147c6d59dd30b700
Wilder Gonzalez Diaz <wildergd@gmail.com>
2020-10-08 22:58:02 -0400

    style: fix style for elementLoading sub-component

--- src/components/Table/body/styled/elementLoading.js
+++ src/components/Table/body/styled/elementLoading.js
@@ -1,22 +1,23 @@
 import styled from 'styled-components';
 import attachThemeAttrs from '../../../../styles/helpers/attachThemeAttrs';
-import { replaceAlpha } from '../../../../styles/helpers/color';
+import { replaceAlpha, darken, getContrastRatio } from '../../../../styles/helpers/color';
 
 const StyledElementLoading = attachThemeAttrs(styled.div)`
     height: 10px;
     object-fit: contain;
     border-radius: 24px;
     z-index: 1;
-    background: ${props => props.palette.background.secondary}
+    background: ${props => replaceAlpha(props.palette.background.secondary, 0.5)}
         linear-gradient(
             90deg,
             ${props => replaceAlpha(props.palette.background.highlight, 0.01)} 0%,
-            ${props => replaceAlpha(props.palette.background.highlight, 0.65)} 50%,
+            ${props => replaceAlpha(props.palette.background.highlight, 0.7)} 50%,
             ${props => replaceAlpha(props.palette.background.highlight, 0.01)} 100%
         );
     background-size: 400% 400%;
     animation: gradient 2.5s ease-in-out infinite;
-
+    filter: invert(3%); 
+    
     @keyframes gradient {
         0% {
             background-position: 14% 0;
