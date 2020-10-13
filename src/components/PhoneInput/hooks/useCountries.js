a1ffdc7fb1e88b1c7c4a2b9ab139acb9eb661794
LeandroTorresSicilia <jtorressicilia@gmail.com>
2020-10-13 09:48:52 -0500

    fix: lint error

--- src/components/PhoneInput/hooks/useCountries.js
+++ src/components/PhoneInput/hooks/useCountries.js
@@ -1,16 +1,15 @@
 import { useMemo } from 'react';
 import allCountries from '../countries';
-import customWarning from './../customWarning';
+import customWarning from '../customWarning';
 
 export default function useCountries(countries) {
     return useMemo(() => {
-        const filteredCountries = allCountries.filter(country =>
-            countries.includes(country.isoCode),
-        );
-
         if (countries.length === 0) {
             return allCountries;
         }
+        const filteredCountries = allCountries.filter(country =>
+            countries.includes(country.isoCode),
+        );
         if (filteredCountries.length === 0) {
             customWarning();
             return allCountries;
