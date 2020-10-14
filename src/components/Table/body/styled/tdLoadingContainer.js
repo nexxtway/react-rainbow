0902637c688583e8e667f50edd2226e8ef30c239
Wilder González Díaz <wildergd@gmail.com>
2020-10-14 14:34:55 -0400

    fix: isLoading on the Table component when the variant is `listView`  (#1896)
    * style: fix style for elementLoading sub-component
    
    * Update elementLoading.js
    
    * style: fix table loading styles when variant is listview
    
    * fix: fix amount of loading indicators in Table component
    
    Co-authored-by: Tahimi <tahimileon@gmail.com>

--- src/components/Table/body/styled/tdLoadingContainer.js
+++ src/components/Table/body/styled/tdLoadingContainer.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../../styles/helpers/attachThemeAttrs';
 
-const StyledTdLoadingContainer = styled.td`
+const StyledTdLoadingContainer = attachThemeAttrs(styled.td)`
     padding: 0;
     text-align: left;
     box-sizing: border-box;
@@ -12,6 +13,42 @@ const StyledTdLoadingContainer = styled.td`
     :last-of-type > div {
         padding: 0 12px;
     }
+
+    ${props =>
+        props.theme.variant === 'listview' &&
+        `
+        background-color: ${props.palette.background.main};
+        border: none;
+        text-align: center;
+        border-top: 1px solid ${props.palette.border.divider};
+        border-right: 1px solid ${props.palette.border.divider};
+
+        :first-child {
+            border-left: 1px solid ${props.palette.border.divider};
+            border-left-style: solid;
+            border-top-left-radius: 13px; 
+            border-bottom-left-radius: 13px;
+        }
+        
+        :last-child {
+            border-right-style: solid;
+            border-bottom-right-radius: 13px; 
+            border-top-right-radius: 13px; 
+        }
+
+        :last-child > div {
+            border-bottom-right-radius: 12px; 
+            border-top-right-radius: 12px;
+            overflow: hidden;
+        }
+
+        :first-child > div {
+            border-top-left-radius: 12px; 
+            border-bottom-left-radius: 12px;
+            overflow: hidden;
+            padding-left: 0.75rem;
+        }
+    `}
 `;
 
 export default StyledTdLoadingContainer;
