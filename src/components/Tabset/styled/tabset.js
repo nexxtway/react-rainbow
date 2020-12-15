bc092ebbd73cf631fea349bd832e20f175cb4e48
Yuri Victor Munayev <yvmunayev@gmail.com>
2020-12-15 12:25:25 -0500

    fix: Tabset some times show the navigate button (#2048)
    Co-authored-by: Tahimi <tahimileon@gmail.com>

--- src/components/Tabset/styled/tabset.js
+++ src/components/Tabset/styled/tabset.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { PADDING_SMALL } from '../../../styles/paddings';
 
 const StyledTabset = styled.div`
     display: flex;
@@ -8,6 +9,17 @@ const StyledTabset = styled.div`
     justify-content: flex-start;
     width: 100%;
     box-sizing: border-box;
+    padding: 0 ${PADDING_SMALL};
+
+    @media (max-width: 600px) {
+        padding: 0;
+    }
+
+    ${props =>
+        props.variant === 'line' &&
+        `
+            padding: 0;
+        `};
 `;
 
 export default StyledTabset;
