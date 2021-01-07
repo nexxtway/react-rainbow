39d4d21191bdd1e3a6cbd19f83a8469aed89782c
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2021-01-06 21:11:28 -0500

    fix: fix chart legend never showing (#2082)
    fix: #2052

--- src/components/Chart/helpers/unregisterGlobalPlugins.js
+++ src/components/Chart/helpers/unregisterGlobalPlugins.js
@@ -1,3 +1,6 @@
+import { filler, legend, title } from 'chart.js/src/plugins';
+
 export default function unregisterGlobalPlugins(ChartJS) {
     ChartJS.plugins.clear();
+    [filler, legend, title].forEach(plugin => ChartJS.plugins.register(plugin));
 }
