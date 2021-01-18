82d4891f98cfb40a44d93de1de3c6cfb10d885de
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-01-18 15:08:54 -0500

    fix: issues in comments

--- src/components/FileSelector/types.js
+++ src/components/FileSelector/types.js
@@ -1,12 +1,4 @@
-const isBrowser = !!(
-    typeof window !== 'undefined' &&
-    window.document &&
-    window.document.createElement
-);
-
-const isNative = typeof navigator !== 'undefined' && navigator.product === 'ReactNative';
-
-const isServer = !(isBrowser || isNative);
+import { isServer } from '../../libs/utils';
 
 // eslint-disable-next-line import/prefer-default-export
 export const FileListType = isServer ? Object : window.FileList;
--- src/libs/utils/getBrowserLocale.js
+++ src/libs/utils/getBrowserLocale.js
@@ -1,7 +1,8 @@
+import isServer from './isServer';
+
 export default function getBrowserLocale() {
-    if (typeof window !== 'undefined') {
-        if (navigator.languages && navigator.languages.length > 0) return navigator.languages[0];
-        if (navigator.language) return navigator.language;
-    }
+    if (isServer) return 'en-US';
+    if (navigator.languages && navigator.languages.length > 0) return navigator.languages[0];
+    if (navigator.language) return navigator.language;
     return 'en-US';
 }
