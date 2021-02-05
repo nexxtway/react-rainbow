c45660bf5e4d5f4362976e669cd4ef27aa76a0fe
TahimiLeonBravo <tahimi@nexxtway.com>
2021-02-05 11:19:44 -0600

    fix: example

--- src/components/Tab/styled/button.js
+++ src/components/Tab/styled/button.js
@@ -83,6 +83,7 @@ const StyledAnchor = attachThemeAttrs(styled.button)`
             font-size: 16px;
             padding: ${PADDING_X_SMALL} ${PADDING_SMALL};
             letter-spacing: 1.5px;
+            line-height: 1.5; 
         `};
 
     @media (max-width: 600px) {
