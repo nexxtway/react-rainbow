efaaa328d96e7c2eedab7d6704838c1028264519
HellWolf93 <drodriguezfeito@gmail.com>
2021-03-05 22:40:17 -0500

    fix: improve useReduxForm type declaration

--- src/libs/hooks/useReduxForm.d.ts
+++ src/libs/hooks/useReduxForm.d.ts
@@ -1,2 +1,14 @@
-declare function useReduxForm<T>(props: T): Record<string, unknown>;
+import { SyntheticEvent, FocusEvent } from 'react';
+
+type FormProps = {
+    name?: string;
+    onBlur?: (event: SyntheticEvent<FocusEvent>) => void;
+    onChange?: (event: SyntheticEvent<InputEvent>) => void;
+    onFocus?: (event: SyntheticEvent<FocusEvent>) => void;
+    value?: any;
+    error?: any;
+    dirty?: boolean;
+};
+
+declare function useReduxForm<T>(props: T): FormProps & T;
 export default useReduxForm;
