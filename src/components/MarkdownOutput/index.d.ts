18c959f6444d1afed78dd93579e72388a9b9892a
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2021-04-03 09:29:16 -0400

    fix: add variant to type declaration in MarkdownOutput (#2212)

--- src/components/MarkdownOutput/index.d.ts
+++ src/components/MarkdownOutput/index.d.ts
@@ -4,6 +4,7 @@ import { BaseProps } from '../types';
 export interface MarkdownOutputProps extends BaseProps {
     id?: string;
     value?: string;
+    variant?: 'default' | 'inline';
 }
 
 export default function(props: MarkdownOutputProps): JSX.Element | null;
