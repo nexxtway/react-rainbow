45ffeab8798771f105095de6fc70835774a12265
HellWolf93 <drodriguezfeito@gmail.com>
2021-04-05 14:37:59 -0400

    fix: lift some CSS to MarkdownOutput container

--- src/components/MarkdownOutput/styled/index.js
+++ src/components/MarkdownOutput/styled/index.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
+import { FONT_SIZE_TEXT_LARGE } from '../renderers/fontSizes';
 
-const StyledContainer = styled.div`
+const StyledContainer = attachThemeAttrs(styled.div)`
+    font-size: ${FONT_SIZE_TEXT_LARGE};
+    color: ${props => props.palette.text.main};
+    line-height: 1.5;
+
     .task-list-item {
         list-style: none;
         margin-left: 0;
