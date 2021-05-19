15b98f575cc8cd05a6d3da4fa51d86ca77541476
David Rodriguez <drodriguezfeito@gmail.com>
2021-05-19 13:04:47 -0400

    fix: declare props on index.d.ts

--- src/components/ZoomableImage/index.d.ts
+++ src/components/ZoomableImage/index.d.ts
@@ -1,4 +1,13 @@
-interface ZoomableImageProps {}
+interface ZoomableImageProps {
+    /** The image URL. */
+    src: string;
+    /** Defines an alternative text description of the image. */
+    alt: string;
+    /** The intrinsic width of the image in pixels. Must be an integer without a unit. */
+    width: number;
+    /** The intrinsic height of the image, in pixels. Must be an integer without a unit. */
+    height: number;
+}
 
 declare const ZoomableImage: React.ComponentType<ZoomableImageProps>;
 export default ZoomableImage;
