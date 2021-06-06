ac8f498b5b2e4bf27b9850e9fe0d0147f28b7552
David Rodriguez <drodriguezfeito@gmail.com>
2021-06-06 14:09:34 -0400

    fix: fix margin of Tree component

--- src/components/Tree/styled/treeContainerUl.js
+++ src/components/Tree/styled/treeContainerUl.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const TreeContainerUl = styled.ul`
     list-style: none;
-    margin: 0;
+    margin: 0 0 0 16px;
     padding: 0;
     box-sizing: border-box;
 `;
