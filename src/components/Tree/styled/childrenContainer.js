2632a9b315d0d9c5e4a84129044fa750b8339a6e
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2021-06-07 09:39:09 -0400

    fix: add left margin to Tree component (#2246)
    * fix: fix margin of Tree component
    
    * fix: use padding instead of margin

--- src/components/Tree/styled/childrenContainer.js
+++ src/components/Tree/styled/childrenContainer.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import attachThemeAttrs from '../../../styles/helpers/attachThemeAttrs';
 
 const ChildrenContainerUl = attachThemeAttrs(styled.ul)`
-    margin-left: 10px;
+    margin-left: 26px;
     border-left: 1px solid ${props => props.palette.border.divider};
     padding-left: 10px;
+
+    ${props => props.ariaLevelValue > 1 && `margin-left: 10px;`}
 `;
 
 export default ChildrenContainerUl;
