cc11cb9fe1e1a6415bdd24731c30c52d388fc4d7
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-06-09 09:36:29 -0400

    fix: fix tree label to accept jsx

--- src/components/Tree/hooks/useTreeNodesAsPlainList.js
+++ src/components/Tree/hooks/useTreeNodesAsPlainList.js
@@ -4,6 +4,7 @@ import buildPlainListFromTree from '../helpers/buildPlainListFromTree';
 export default function useTreeNodesAsPlainList(data) {
     const dataStr = JSON.stringify(data, ['label', 'isExpanded', 'children']);
     return useMemo(() => {
-        return buildPlainListFromTree(JSON.parse(dataStr));
+        return buildPlainListFromTree(data);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [dataStr]);
 }
