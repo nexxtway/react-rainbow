a4d74af105bd8e16c6cbe34931e6bf4afac79b76
David Rodriguez Feitó <drodriguezfeito@gmail.com>
2021-09-28 15:50:02 -0400

    feat: add description prop to options on RadioGroup and CheckboxGroup components (#2290)
    * feat: add description prop to options on RadioGroup and CheckboxGroup components
    
    * fix: add margin beetween items
    
    * test: fix integration test for radio and checkboxgroup
    
    * Update checkboxGroup-1.spec.js
    
    Co-authored-by: Tahimi <tahimileon@gmail.com>

--- src/components/CheckboxGroup/checkboxList.js
+++ src/components/CheckboxGroup/checkboxList.js
@@ -2,23 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import isOptionSelected from './isOptionSelected';
 import Checkbox from '../Input/inputCheckbox/checkbox';
+import StyledItemContainer from '../RadioGroup/styled/itemContainer';
+import StyledItemDescription from '../RadioGroup/styled/itemDescription';
+import RenderIf from '../RenderIf';
 
 export default function CheckboxList(props) {
     const { values, options, onChange, describedBy, name, error } = props;
     return options.map((option, index) => {
         const key = `checkbox-${index}`;
+        const { description, ...rest } = option;
 
         return (
-            <Checkbox
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...option}
-                checked={isOptionSelected(values, option)}
-                onChange={onChange}
-                ariaDescribedBy={describedBy}
-                key={key}
-                name={name}
-                error={error}
-            />
+            <StyledItemContainer data-id="input-checkboxgroup_container">
+                <Checkbox
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...rest}
+                    checked={isOptionSelected(values, option)}
+                    onChange={onChange}
+                    ariaDescribedBy={describedBy}
+                    key={key}
+                    name={name}
+                    error={error}
+                />
+                <RenderIf isTrue={description}>
+                    <StyledItemDescription>{description}</StyledItemDescription>
+                </RenderIf>
+            </StyledItemContainer>
         );
     });
 }
---
+++ src/components/RadioGroup/styled/itemContainer.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledItemContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export default StyledItemContainer;
