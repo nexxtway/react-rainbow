8b7ea8f8caa740e829c02876a417b008fe5ad779
David Rodriguez <drodriguezfeito@gmail.com>
2022-06-28 18:02:24 -0300

    fix: fix lint errors

--- src/components/Button/buttonContent.js
+++ src/components/Button/buttonContent.js
@@ -1,3 +1,15 @@
+import PropTypes from 'prop-types';
+
 export default function ButtonContent({ label, children }) {
     return children || label || null;
 }
+
+ButtonContent.propTypes = {
+    label: PropTypes.string,
+    children: PropTypes.node,
+};
+
+ButtonContent.defaultProps = {
+    label: undefined,
+    children: undefined,
+};
