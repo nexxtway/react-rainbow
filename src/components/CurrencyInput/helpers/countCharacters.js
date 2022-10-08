c18120acd2c5e4a3f8fdbff0e5e1d5766fc71d84
Yuri Victor Munayev <yvmunayev@gmail.com>
2022-10-08 12:52:13 -0400

    feat: add currency input component (#2468)
    * feat: add currency input component
    
    * feat: add currency input prop size

---
+++ src/components/CurrencyInput/helpers/countCharacters.js
@@ -0,0 +1,3 @@
+export default function countCharacters(str, char) {
+    return str.split(char).length - 1;
+}
---
+++ src/components/CurrencyInput/hooks/index.js
@@ -0,0 +1,3 @@
+export { default as useOptions } from './useOptions';
+export { default as useDecimalSeparator } from './useDecimalSeparator';
+export { default as useGroupSeparator } from './useGroupSeparator';
