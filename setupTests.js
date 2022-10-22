cb321d962bb35907b50ab2532eed844b80058b23
Yuri Victor Munayev <yvmunayev@gmail.com>
2022-10-22 12:16:45 -0400

    tests: add unit tests to currency input (#2488)
    * tests: add unit test to currency input
    
    * fix: some lint error
    
    * fix: change name of isValidNumberValue function
    
    Co-authored-by: Jose Leandro Torres <jtorressicilia@gmail.com>

--- setupTests.js
+++ setupTests.js
@@ -3,6 +3,7 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { toHaveNoViolations } from 'jest-axe';
 import toBeFocusable from './jestMatchers/toBeFocusable';
+import '@testing-library/jest-dom';
 
 configure({ adapter: new Adapter() });
 
