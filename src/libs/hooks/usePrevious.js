4465af0529608551426f771ea6e059b05a7977bb
Yuri Victor Munayev <yvmunayev@gmail.com>
2022-11-11 21:24:25 -0500

    feat: add usePrevios

---
+++ src/libs/hooks/usePrevious.js
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from 'react';
+
+const usePrevious = value => {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+};
+
+export default usePrevious;
