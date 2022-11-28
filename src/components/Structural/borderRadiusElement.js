a3e1a3117db8c562fb2833cacab6a2d3320b85b6
Yuri Victor Munayev <yvmunayev@gmail.com>
2022-11-27 19:04:00 -0500

    feat: add border radius prop to modal

--- src/components/Structural/borderRadiusElement.js
+++ src/components/Structural/borderRadiusElement.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import {
+    BORDER_RADIUS_1,
     BORDER_RADIUS_SQUARE,
     BORDER_RADIUS_SEMI_SQUARE,
     BORDER_RADIUS_SEMI_ROUNDED,
 } from '../../styles/borderRadius';
 
 const BorderRadiusElement = styled.div`
-    border-radius: 0.875rem;
+    border-radius: ${BORDER_RADIUS_1};
     ${props =>
         props.borderRadius === 'square' &&
         `
