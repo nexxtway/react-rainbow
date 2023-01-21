7b07e9734dc18306645d00b823b2e3ab38ea1250
Yuri Victor Munayev <yvmunayev@gmail.com>
2023-01-21 17:15:43 -0500

    test: change stepThree unit test

--- src/components/ImportRecordsFlow/stepThree/__test__/stepThree.spec.js
+++ src/components/ImportRecordsFlow/stepThree/__test__/stepThree.spec.js
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import { mount } from 'enzyme';
 import StepThree from '..';
 import getAssignFieldsData from '../../helpers/getAssignFieldsData';
-import StyledTable from '../../styled/table';
 
 jest.mock('../../helpers/getAssignFieldsData', () =>
     jest.fn(() => [
@@ -27,8 +27,8 @@ const baseParams = {
 
 describe('<StepThree />', () => {
     it('should have the assign field modal closed by default', () => {
-        const component = mount(<StepThree {...baseParams} />);
-        expect(component.find('AssignFieldModal').prop('isAssignFieldModalOpen')).toBe(false);
+        const { queryByRole } = render(<StepThree {...baseParams} />);
+        expect(queryByRole('dialog')).toBeNull();
     });
     it('should pass params to modal', () => {
         const component = mount(<StepThree {...baseParams} />);
@@ -76,39 +76,30 @@ describe('<StepThree />', () => {
         ]);
     });
     it('should pass the right data to the Table component', () => {
-        const component = mount(<StepThree {...baseParams} />);
-        expect(component.find(StyledTable).prop('data')).toEqual([
-            { required: true, fileField: undefined, databaseField: 'name' },
-            { required: false, fileField: undefined, databaseField: 'age' },
-        ]);
+        const { getByRole } = render(<StepThree {...baseParams} />);
+
+        expect(getByRole('row', { name: 'required name Not assigned yet' })).toBeVisible();
+        expect(getByRole('row', { name: 'age Not assigned yet' })).toBeVisible();
     });
     it('should open the modal when a column cell is clicked', () => {
-        const component = mount(<StepThree {...baseParams} />);
-        component
-            .find('ModifyCell')
-            .at(0)
-            .simulate('click');
-        expect(component.find('AssignFieldModal').prop('isAssignFieldModalOpen')).toBe(true);
+        const { getByRole, getAllByRole } = render(<StepThree {...baseParams} />);
+
+        fireEvent.click(getAllByRole('button')[0]);
+        expect(getByRole('dialog')).toBeVisible();
     });
     it('should close AssignFieldModal when onRequestClose prop is called', () => {
-        const component = mount(<StepThree {...baseParams} />);
-        component
-            .find('ModifyCell')
-            .at(0)
-            .simulate('click');
-        component
-            .find('AssignFieldModalFooter')
-            .find('Button')
-            .at(0)
-            .simulate('click');
-        expect(component.find('AssignFieldModal').prop('isAssignFieldModalOpen')).toBe(false);
+        const { getByRole, getAllByRole, queryByRole } = render(<StepThree {...baseParams} />);
+
+        fireEvent.click(getAllByRole('button')[0]);
+        expect(getByRole('dialog')).toBeVisible();
+        fireEvent.click(getByRole('button', { name: 'Cancel' }));
+        expect(queryByRole('dialog')).toBeNull();
     });
     it('should pass the right databaseFieldToAssign prop to the modal when a column cell is clicked', () => {
-        const component = mount(<StepThree {...baseParams} />);
-        component
-            .find('ModifyCell')
-            .at(1)
-            .simulate('click');
-        expect(component.find('AssignFieldModal').prop('databaseFieldToAssign')).toBe('age');
+        const { getByRole, getAllByRole } = render(<StepThree {...baseParams} />);
+
+        fireEvent.click(getAllByRole('button')[1]);
+        expect(getByRole('dialog')).toBeVisible();
+        expect(getByRole('heading', { name: 'Assign to the field: age' })).toBeVisible();
     });
 });
