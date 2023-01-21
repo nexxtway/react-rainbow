308e49fac5da2453154e5892e83a64940411747c
Yuri Victor Munayev <yvmunayev@gmail.com>
2023-01-21 15:10:20 -0500

    test: change csvCell unit test

--- src/components/ImportRecordsFlow/stepThree/__test__/csvCell.spec.js
+++ src/components/ImportRecordsFlow/stepThree/__test__/csvCell.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import CSVCell from '../csvCell';
-import StyledAssignButton from '../styled/assignButton';
+import ButtonIcon from '../../../ButtonIcon';
 
 describe('<CSVCell />', () => {
     it('should return the same value if value is sent as param', () => {
-        const returnValue = CSVCell({ value: 'test-value' });
-        expect(returnValue).toBe('test-value');
+        const returnValue = mount(<CSVCell value="test-value" />);
+        expect(returnValue.text()).toBe('test-value');
     });
     it('should have assign field content when no value is sent as param', () => {
         const component = mount(<CSVCell />);
@@ -16,7 +16,7 @@ describe('<CSVCell />', () => {
         const onClickFn = jest.fn();
         const row = { databaseField: 'name' };
         const component = mount(<CSVCell onClick={onClickFn} row={row} />);
-        component.find(StyledAssignButton).simulate('click');
+        component.find(ButtonIcon).simulate('click');
         expect(onClickFn).toHaveBeenCalledWith(row.databaseField);
     });
 });
