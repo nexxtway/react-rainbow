624b5e5ce6b95c06ccc925c62bb7b90a86005034
Adrian Estevez <adrianestevez@Adrians-MacBook-Pro.local>
2023-02-10 13:10:56 -0500

    fix: test

--- src/components/ImportRecordsFlow/stepFour/__test__/stepFour.spec.js
+++ src/components/ImportRecordsFlow/stepFour/__test__/stepFour.spec.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import getPreviewDataToImport from '../../helpers/getPreviewDataToImport';
+import { shallow } from 'enzyme';
 import StepFour from '..';
 import Column from '../../../Column';
 
