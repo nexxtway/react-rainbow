80659a80e27a86c83675faea96608e9b70c7cee7
Wilder González Díaz <wildergd@gmail.com>
2023-07-28 11:06:25 -0600

    style: fix Lookup styles

--- src/components/Lookup/styled/chipContainer.js
+++ src/components/Lookup/styled/chipContainer.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const StyledChipContainer = styled.span`
     display: flex;
     align-items: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
 `;
 
 export default StyledChipContainer;
